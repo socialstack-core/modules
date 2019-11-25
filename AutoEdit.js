1b2f40bb93592e7ba11c6972ceb6f4ea826211b1
Luke Briggs <luke@kulestar.com>
2019-11-25 23:57:57 +0000

    Initial commit

---
+++ AutoEdit.js
@@ -0,0 +1,25 @@
+import Tile from 'Admin/Tile';
+import Loop from 'UI/Loop';
+import AutoForm from 'Admin/AutoForm';
+import Default from 'Admin/Pages/Default';
+
+
+export default class AutoEdit extends React.Component {
+	
+	constructor(props){
+		super(props);
+	}
+	
+	render(){
+		console.log(this.props);
+		return (
+			<Default>
+				<Tile>
+					<AutoForm endpoint={this.props.endpoint} id={this.props.id} />
+				</Tile>
+				{this.props.children}
+			</Default>	
+		);
+	}
+	
+}
