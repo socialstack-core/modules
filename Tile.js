1d2caebd700fdc179142ac132f9ab83040debbe6
Luke Briggs <luke@kulestar.com>
2019-11-26 00:08:09 +0000

    Initial commit

---
+++ Tile.js
@@ -0,0 +1,26 @@
+/**
+ * A tile in the admin area.
+ */
+export default class Tile extends React.Component {
+	
+	render() {
+
+        const className = (12 / (this.props.row || 1));
+		
+        return (
+            <div className={'col-md-' + className}>
+                <div className={this.props.empty ? "" : "component-tile"}>
+                    {!this.props.empty && (
+                        <header>
+                            {this.props.title}
+                        </header>
+                    )}
+                    <article>
+                        {this.props.children}
+                    </article>
+                </div>
+            </div>
+        );
+    }
+	
+}
