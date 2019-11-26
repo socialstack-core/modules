6c7108e3622d176eae0b9c207939f2c6173a7efc
Luke Briggs <luke@kulestar.com>
2019-11-26 00:04:35 +0000

    Initial commit

---
+++ List.js
@@ -0,0 +1,31 @@
+import Tile from 'Admin/Tile';
+import AutoList from 'Admin/AutoList';
+import Loop from 'UI/Loop';
+import Default from 'Admin/Pages/Default';
+
+
+export default class List extends React.Component {
+	
+	constructor(props){
+		super(props);
+	}
+	
+	render(){
+		var path = document.location.pathname;
+		if(path[path.length-1] != '/'){
+			path += '/';
+		}
+		
+		return (
+			<Default>
+				<Tile>
+					<a href={path + 'add'}>
+						<div className="btn btn-primary">Create</div>
+					</a>
+				</Tile>
+				<AutoList endpoint={this.props.endpoint} path={path} {...this.props} />
+			</Default>	
+		);
+	}
+	
+}
