5a18391a81d17779238ee7a7accbe523db226c1b
Luke Briggs <luke@kulestar.com>
2019-11-26 00:44:26 +0000

    Initial commit

---
+++ ForumReplyController.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Threading.Tasks;
+using Api.Permissions;
+using Microsoft.AspNetCore.Mvc;
+using Api.ForumThreads;
+using System.Collections.Generic;
+using Api.Results;
+using Api.Contexts;
+using Api.Eventing;
+using Newtonsoft.Json.Linq;
+using Api.AutoForms;
+
+namespace Api.ForumReplies
+{
+    /// <summary>
+    /// Handles forum reply endpoints.
+    /// </summary>
+
+    [Route("v1/forum/reply")]
+	[ApiController]
+	public partial class ForumReplyController : ControllerBase
+    {
+        private IForumReplyService _forumReplies;
+        private IForumThreadService _forumThreads;
+
+
+		/// <summary>
+		/// Instanced automatically.
+		/// </summary>
+		public ForumReplyController(
+            IForumReplyService forumReplies,
+			IForumThreadService forumThreads
+        )
+        {
+            _forumReplies = forumReplies;
+            _forumThreads = forumThreads;
+        }
+
+		/// <summary>
+		/// GET /v1/forum/reply/2/
+		/// Returns the reply data for a single reply.
+		/// </summary>
+		[HttpGet("{id}")]
+		public async Task<ForumReply> Load([FromRoute] int id)
+		{
+			var context = Request.GetContext();
+			var result = await _forumReplies.Get(context, id);
+			return await Events.ForumReplyLoad.Dispatch(context, result, Response);
+		}
+		
+		/// <summary>
+		/// DELETE /v1/forum/reply/2/
+		/// Deletes a forum reply
+		/// </summary>
+		[HttpDelete("{id}")]
+		public async Task<Success> Delete([FromRoute] int id)
+		{
+			var context = Request.GetContext();
+			var result = await _forumReplies.Get(context, id);
+			result = await Events.ForumReplyDelete.Dispatch(context, result, Response);
+
+			if (result == null || !await _forumReplies.Delete(context, id))
+			{
+				// The handlers have blocked this one from happening, or it failed
+				return null;
+			}
+
+			return new Success();
+		}
+
+		/// <summary>
+		/// GET /v1/forum/reply/list
+		/// Lists all forum replies available to this user.
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet("list")]
+		public async Task<Set<ForumReply>> List()
+		{
+			return await List(null);
+		}
+
+		/// <summary>
+		/// POST /v1/forum/reply/list
+		/// Lists filtered forum replies available to this user.
+		/// See the filter documentation for more details on what you can request here.
+		/// </summary>
+		/// <returns></returns>
+		[HttpPost("list")]
+		public async Task<Set<ForumReply>> List([FromBody] JObject filters)
+		{
+			var context = Request.GetContext();
+			var filter = new Filter<ForumReply>(filters);
+
+			filter = await Events.ForumReplyList.Dispatch(context, filter, Response);
+
+			if (filter == null)
+			{
+				// A handler rejected this request.
+				return null;
+			}
+
+			var results = await _forumReplies.List(context, filter);
+			return new Set<ForumReply>() { Results = results };
+		}
+
+		/// <summary>
+		/// POST /v1/forum/reply/
+		/// Creates a new forum reply. Returns the ID.
+		/// </summary>
+		[HttpPost]
+		public async Task<ForumReply> Create([FromBody] ForumReplyAutoForm form)
+		{
+			var context = Request.GetContext();
+			
+			// Get the thread so we can grab the forum ID:
+			var thread = await _forumThreads.Get(context, form.ThreadId);
+			
+			if(thread == null){
+				return null;
+			}
+			
+			// Start building up our object.
+			// Most other fields, particularly custom extensions, are handled by autoform.
+			var forumReply = new ForumReply
+			{
+				UserId = context.UserId,
+				ForumId = thread.ForumId
+			};
+			
+			if (!ModelState.Setup(form, forumReply))
+			{
+				return null;
+			}
+
+			form = await Events.ForumReplyCreate.Dispatch(context, form, Response);
+
+			if (form == null || form.Result == null)
+			{
+				// A handler rejected this request.
+				return null;
+			}
+
+			forumReply = await _forumReplies.Create(context, form.Result);
+
+			if (forumReply == null)
+			{
+				Response.StatusCode = 500;
+				return null;
+			}
+			
+            return forumReply;
+        }
+
+		/// <summary>
+		/// POST /v1/forum/reply/1/
+		/// Updates a forum reply with the given ID.
+		/// </summary>
+		[HttpPost("{id}")]
+		public async Task<ForumReply> Update([FromRoute] int id, [FromBody] ForumReplyAutoForm form)
+		{
+			var context = Request.GetContext();
+
+			var forumReply = await _forumReplies.Get(context, id);
+			
+			if (!ModelState.Setup(form, forumReply)) {
+				return null;
+			}
+
+			form = await Events.ForumReplyUpdate.Dispatch(context, form, Response);
+
+			if (form == null || form.Result == null)
+			{
+				// A handler rejected this request.
+				return null;
+			}
+			
+			forumReply = await _forumReplies.Update(context, form.Result);
+
+			if (forumReply == null)
+			{
+				Response.StatusCode = 500;
+				return null;
+			}
+			
+			return forumReply;
+		}
+		
+    }
+
+}
---
+++ ForumReplyService.cs
@@ -0,0 +1,119 @@
+﻿using Api.Contexts;
+using Api.Database;
+using Api.Eventing;
+using Api.Permissions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.DrawingCore;
+using System.DrawingCore.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Api.ForumReplies
+{
+	/// <summary>
+	/// Handles forum replies.
+	/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
+	/// </summary>
+	public partial class ForumReplyService : IForumReplyService
+    {
+        private IDatabaseService _database;
+		
+		private readonly Query<ForumReply> deleteQuery;
+		private readonly Query<ForumReply> createQuery;
+		private readonly Query<ForumReply> selectQuery;
+		private readonly Query<ForumReply> updateQuery;
+		private readonly Query<ForumReply> listQuery;
+
+
+		/// <summary>
+		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
+		/// </summary>
+		public ForumReplyService(IDatabaseService database)
+        {
+            _database = database;
+			
+			// Start preparing the queries. Doing this ahead of time leads to excellent performance savings, 
+			// whilst also using a high-level abstraction as another plugin entry point.
+			deleteQuery = Query.Delete<ForumReply>();
+			createQuery = Query.Insert<ForumReply>();
+			updateQuery = Query.Update<ForumReply>();
+			selectQuery = Query.Select<ForumReply>();
+			listQuery = Query.List<ForumReply>();
+        }
+		
+        /// <summary>
+        /// Deletes a forum reply by its ID.
+		/// Optionally includes uploaded content refs in there too.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<bool> Delete(Context context, int id, bool deleteUploads = true)
+        {
+            // Delete the entry:
+			await _database.Run(deleteQuery, id);
+			
+			if(deleteUploads){
+			}
+			
+			// Ok!
+			return true;
+        }
+        
+		/// <summary>
+		/// Gets a single reply by its ID.
+		/// </summary>
+		public async Task<ForumReply> Get(Context context, int id)
+		{
+			return await _database.Select(selectQuery, id);
+		}
+		
+		/// <summary>
+		/// List a filtered set of replies.
+		/// </summary>
+		/// <returns></returns>
+		public async Task<List<ForumReply>> List(Context context, Filter<ForumReply> filter)
+		{
+			filter = await Events.ForumReplyBeforeList.Dispatch(context, filter);
+			var list = await _database.List(listQuery, filter);
+			list = await Events.ForumReplyAfterList.Dispatch(context, list);
+			return list;
+		}
+
+		/// <summary>
+		/// Creates a new forum reply.
+		/// </summary>
+		public async Task<ForumReply> Create(Context context, ForumReply reply)
+		{
+			reply = await Events.ForumReplyBeforeCreate.Dispatch(context, reply);
+
+			// Note: The Id field is automatically updated by Run here.
+			if (reply == null || !await _database.Run(createQuery, reply))
+			{
+				return null;
+			}
+
+			reply = await Events.ForumReplyAfterCreate.Dispatch(context, reply);
+			return reply;
+		}
+
+		/// <summary>
+		/// Updates the given forum reply.
+		/// </summary>
+		public async Task<ForumReply> Update(Context context, ForumReply reply)
+		{
+			reply = await Events.ForumReplyBeforeUpdate.Dispatch(context, reply);
+
+			if (reply == null || !await _database.Run(updateQuery, reply, reply.Id))
+			{
+				return null;
+			}
+
+			reply = await Events.ForumReplyAfterUpdate.Dispatch(context, reply);
+			return reply;
+		}
+	}
+
+}
