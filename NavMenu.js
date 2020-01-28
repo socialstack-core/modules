a72b0961a474f86bbb3ebd66a2e4866c03b5599c
Luke Briggs <luke@kulestar.com>
2020-01-28 14:51:15 +0000

    Initial commit

---
+++ NavMenu.js
@@ -0,0 +1,16 @@
+import Loop from 'UI/Loop';
+import Canvas from 'UI/Canvas';
+import isNumeric from 'UI/Functions/IsNumeric';
+
+/**
+ * A nav menu. This is a very thin wrapper over Loop so it essentially does everything that Loop can (i.e. <NavMenu inline .. etc).
+ */
+
+export default (props) =>
+	<Loop over={'navmenu/' + (isNumeric(props.id) ? '' : 'key/') + props.id} {...props}>
+	{props.children.length ? props.children : item => 
+		<a href={item.target}>
+			<Canvas>{item.bodyJson}</Canvas>
+		</a>
+	}
+	</Loop>
