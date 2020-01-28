82e5285df996f9389305662640a2fc2682fa631e
Luke Briggs <luke@kulestar.com>
2020-01-28 15:05:24 +0000

    Initial commit

---
+++ Required.js
@@ -0,0 +1,18 @@
+import Text from 'UI/Text';
+
+/*
+	Ensures a value is provided. Returns translatable React element if it failed alongside a constant error code, or nothing.
+*/
+
+export const required = value => {
+	
+	if(!value || value.trim() == ''){
+		return {
+			error: 'EMPTY',
+			ui: <Text>
+				This field is required
+			</Text>
+		};
+	}
+	
+}
