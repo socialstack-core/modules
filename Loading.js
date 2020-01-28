d494595d93d1b6b3a4ad6ad9cb2619a80af472ff
Luke Briggs <luke@kulestar.com>
2020-01-28 14:56:24 +0000

    Initial commit

---
+++ Loading.js
@@ -0,0 +1,11 @@
+
+/**
+ * Standalone component which displays a loader (typically a spinner).
+ */
+export default class Loading extends React.Component {
+	
+	render() {
+		return ("Loading..");
+	}
+	
+}
