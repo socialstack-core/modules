d21bafb27e83a7ab848b26cbb717ae14d809a11c
Luke Briggs <luke@kulestar.com>
2020-01-28 14:57:16 +0000

    Initial commit

---
+++ Html.js
@@ -0,0 +1,15 @@
+/**
+ * This component displays html. Only ever use this with trusted text.
+*/
+
+export default class Html extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+	
+    render() {
+		return <span dangerouslySetInnerHTML={{__html: this.props.children[0]}} />;
+    }
+}
+
