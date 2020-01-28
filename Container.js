6341b59cf413b5f3271d7842ac564f0565b150be
Luke Briggs <luke@kulestar.com>
2020-01-28 15:43:14 +0000

    Initial commit

---
+++ Container.js
@@ -0,0 +1,5 @@
+/**
+ * A specific width container for content (maps directly to bootstrap 'container' by default).
+ */
+
+export default (props) => <div className="container">{props.children}</div>
