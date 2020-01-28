fd853c17af5429dbee667ff96d4e374ab8ff970e
Luke Briggs <luke@kulestar.com>
2020-01-28 15:01:17 +0000

    Initial commit

---
+++ GetModule.js
@@ -0,0 +1,10 @@
+
+/*
+* Gets a UI module by its public name. Case sensitive. E.g. getModule("UI/Start")
+*/
+
+export default function getModule(name) {
+	var parts = name.split('/');
+	name += '/' + parts[parts.length-1] + '.js';
+	return global.require(name).required;
+}
