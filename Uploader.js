b92967810a0885d7146a25a78f1e94e2afdef8b3
Luke Briggs <luke@kulestar.com>
2020-02-10 01:30:37 +0000

    Initial commit

---
+++ Uploader.js
@@ -0,0 +1,52 @@
+import Loading from 'UI/Loading';
+import webRequest from 'UI/Functions/WebRequest';
+
+/*
+* General purpose file uploader. Doesn't delcare a form so can be used inline anywhere.
+*/
+export default class Uploader extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: false
+		};
+	}
+	
+	onSelectedFile(e) {
+		this.setState({ loading: true });
+		var formData = new FormData();
+		var file = e.target.files[0];
+		formData.append('file', file);
+		this.props.onStarted && this.props.onStarted(file, formData);
+		
+		webRequest("uploader/upload", formData, {
+			onUploadProgress: this.props.onUploadProgress
+		}).then(response => {
+			
+			var uploadInfo = response.json;
+			// uploadInfo contains the upload file info, such as its original public url and ref.
+			
+			// Run the main callback:
+			this.props.onUploaded && this.props.onUploaded(uploadInfo);
+			
+			this.setState({loading: false, success: true});
+		});
+		
+	}
+	
+    render() {
+		const {
+			loading
+		} = this.state;
+		
+        return (
+			<div>
+				{loading && (
+					<Loading />
+				)}
+				<input type="file" onChange={e => this.onSelectedFile(e)} className="form-control-file" />
+			</div>
+        );
+    }
+}
