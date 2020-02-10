bd63da7f59fdf825beb14d8645f1e83d5141c924
Luke Briggs <luke@kulestar.com>
2020-02-10 01:30:05 +0000

    Now inline by default

--- Image.js
+++ Image.js
@@ -1,3 +1,9 @@
 import getRef from 'UI/Functions/GetRef';
+import omit from 'UI/Functions/Omit';
 
-export default (props) => <div className="image">{getRef(props.fileRef)}</div>
+/*
+Used to display an image from a fileRef.
+Min required props: size, fileRef. Size must be an available size from your uploader config. You should pick the nearest bigger size from the physical size you're after.
+<Image fileRef='public:2.jpg' size=100 />
+*/
+export default (props) => <div className="image" onClick={props.onClick}>{getRef(props.fileRef, {...(omit(props, ['fileRef', 'onClick']))})}</div>
