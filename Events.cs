a46a0a7253121e68c9c63b1e1ca06e19592161a2
Luke Briggs <luke@kulestar.com>
2020-02-19 03:26:47 +0000

    Upgraded to AutoService and AutoController

--- Events.cs
+++ Events.cs
@@ -4,88 +4,15 @@ using System.Collections.Generic;
 
 namespace Api.Eventing
 {
-
 	/// <summary>
 	/// Events are instanced automatically. 
 	/// You can however specify a custom type or instance them yourself if you'd like to do so.
 	/// </summary>
 	public partial class Events
 	{
-
-		#region Service events
-
 		/// <summary>
-		/// Just before a new forum thread is created. The given forum thread won't have an ID yet. Return null to cancel the creation.
+		/// Set of events for a ForumThread.
 		/// </summary>
-		public static EventHandler<ForumThread> ForumThreadBeforeCreate;
-
-		/// <summary>
-		/// Just after an forum thread has been created. The given forum thread object will now have an ID.
-		/// </summary>
-		public static EventHandler<ForumThread> ForumThreadAfterCreate;
-
-		/// <summary>
-		/// Just before an forum thread is being deleted. Return null to cancel the deletion.
-		/// </summary>
-		public static EventHandler<ForumThread> ForumThreadBeforeDelete;
-
-		/// <summary>
-		/// Just after an forum thread has been deleted.
-		/// </summary>
-		public static EventHandler<ForumThread> ForumThreadAfterDelete;
-
-		/// <summary>
-		/// Just before updating an forum thread. Optionally make additional changes, or return null to cancel the update.
-		/// </summary>
-		public static EventHandler<ForumThread> ForumThreadBeforeUpdate;
-
-		/// <summary>
-		/// Just after updating an forum thread.
-		/// </summary>
-		public static EventHandler<ForumThread> ForumThreadAfterUpdate;
-
-		/// <summary>
-		/// Just after an forum thread was loaded.
-		/// </summary>
-		public static EventHandler<ForumThread> ForumThreadAfterLoad;
-
-		/// <summary>
-		/// Just before a service loads an forumThread list.
-		/// </summary>
-		public static EventHandler<Filter<ForumThread>> ForumThreadBeforeList;
-
-		/// <summary>
-		/// Just after an forumThread list was loaded.
-		/// </summary>
-		public static EventHandler<List<ForumThread>> ForumThreadAfterList;
-
-		#endregion
-
-		#region Controller events
-
-		/// <summary>
-		/// Create a new forum thread.
-		/// </summary>
-		public static EndpointEventHandler<ForumThreadAutoForm> ForumThreadCreate;
-		/// <summary>
-		/// Delete an forum thread.
-		/// </summary>
-		public static EndpointEventHandler<ForumThread> ForumThreadDelete;
-		/// <summary>
-		/// Update forum thread metadata.
-		/// </summary>
-		public static EndpointEventHandler<ForumThreadAutoForm> ForumThreadUpdate;
-		/// <summary>
-		/// Load forum thread metadata.
-		/// </summary>
-		public static EndpointEventHandler<ForumThread> ForumThreadLoad;
-		/// <summary>
-		/// List forum threads.
-		/// </summary>
-		public static EndpointEventHandler<Filter<ForumThread>> ForumThreadList;
-
-		#endregion
-
+		public static EventGroup<ForumThread> ForumThread;
 	}
-
 }
--- ForumThreadService.cs
+++ ForumThreadService.cs
@@ -19,49 +19,23 @@ namespace Api.ForumThreads
 	/// Handles creations of forum threads - containers for forum posts.
 	/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 	/// </summary>
-	public partial class ForumThreadService : IForumThreadService
+	public partial class ForumThreadService : AutoService<ForumThread>, IForumThreadService
 	{
-        private IDatabaseService _database;
 		private IForumService _forums;
-
-		private readonly Query<ForumThread> deleteThreadQuery;
 		private readonly Query<ForumReply> deleteRepliesQuery;
-		private readonly Query<ForumThread> createQuery;
-		private readonly Query<ForumThread> selectQuery;
-		private readonly Query<ForumThread> updateQuery;
-		private readonly Query<ForumThread> listQuery;
 
 
 		/// <summary>
 		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 		/// </summary>
-		public ForumThreadService(IDatabaseService database, IForumService forums)
+		public ForumThreadService(IForumService forums) : base(Events.ForumThread)
         {
-            _database = database;
 			_forums = forums;
 
 			// Start preparing the queries. Doing this ahead of time leads to excellent performance savings, 
 			// whilst also using a high-level abstraction as another plugin entry point.
-			deleteThreadQuery = Query.Delete<ForumThread>();
 			deleteRepliesQuery = Query.Delete<ForumReply>();
 			deleteRepliesQuery.Where().EqualsArg("ThreadId", 0);
-			
-			createQuery = Query.Insert<ForumThread>();
-			updateQuery = Query.Update<ForumThread>();
-			selectQuery = Query.Select<ForumThread>();
-			listQuery = Query.List<ForumThread>();
-		}
-
-		/// <summary>
-		/// List a filtered set of threads.
-		/// </summary>
-		/// <returns></returns>
-		public async Task<List<ForumThread>> List(Context context, Filter<ForumThread> filter)
-		{
-			filter = await Events.ForumThreadBeforeList.Dispatch(context, filter);
-			var list = await _database.List(listQuery, filter);
-			list = await Events.ForumThreadAfterList.Dispatch(context, list);
-			return list;
 		}
 
 		/// <summary>
@@ -69,10 +43,27 @@ namespace Api.ForumThreads
 		/// Optionally includes deleting all replies and uploaded content refs in there too.
 		/// </summary>
 		/// <returns></returns>
-		public async Task<bool> Delete(Context context, int id, bool deleteThreads = true)
+		public override async Task<bool> Delete(Context context, int id)
         {
             // Delete the entry:
-			await _database.Run(deleteThreadQuery, id);
+			await _database.Run(deleteQuery, id);
+			
+			// Delete their replies:
+			await _database.Run(deleteRepliesQuery, id);
+			
+			// Ok!
+			return true;
+        }
+        
+		/// <summary>
+		/// Deletes a forum thread by its ID.
+		/// Optionally includes deleting all replies and uploaded content refs in there too.
+		/// </summary>
+		/// <returns></returns>
+		public async Task<bool> Delete(Context context, int id, bool deleteThreads)
+        {
+            // Delete the entry:
+			await base.Delete(context, id);
 			
 			if(deleteThreads){
 				// Delete their replies:
@@ -84,17 +75,9 @@ namespace Api.ForumThreads
         }
         
 		/// <summary>
-		/// Gets a single thread by its ID.
-		/// </summary>
-		public async Task<ForumThread> Get(Context context, int id)
-		{
-			return await _database.Select(selectQuery, id);
-		}
-
-		/// <summary>
 		/// Creates a new forum thread.
 		/// </summary>
-		public async Task<ForumThread> Create(Context context, ForumThread forumThread)
+		public override async Task<ForumThread> Create(Context context, ForumThread forumThread)
 		{
 			// Get the forum to obtain the default page ID:
 			var forum = await _forums.Get(context, forumThread.ForumId);
@@ -110,34 +93,9 @@ namespace Api.ForumThreads
 				// Default page ID applied now:
 				forumThread.PageId = forum.ThreadPageId;
 			}
-
-			forumThread = await Events.ForumThreadBeforeCreate.Dispatch(context, forumThread);
-
-			// Note: The Id field is automatically updated by Run here.
-			if (forumThread == null || !await _database.Run(createQuery, forumThread))
-			{
-				return null;
-			}
-
-			forumThread = await Events.ForumThreadAfterCreate.Dispatch(context, forumThread);
-			return forumThread;
-		}
-
-		/// <summary>
-		/// Updates the given forum thread.
-		/// </summary>
-		public async Task<ForumThread> Update(Context context, ForumThread forumThread)
-		{
-			forumThread = await Events.ForumThreadBeforeUpdate.Dispatch(context, forumThread);
-
-			if (forumThread == null || !await _database.Run(updateQuery, forumThread, forumThread.Id))
-			{
-				return null;
-			}
-
-			forumThread = await Events.ForumThreadAfterUpdate.Dispatch(context, forumThread);
-			return forumThread;
+			
+			return await base.Create(context, forumThread);
 		}
 	}
-    
+
 }
