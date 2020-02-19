226a7e4923f1906c2e17894139bac07023c9cffe
Luke Briggs <luke@kulestar.com>
2020-02-19 02:21:23 +0000

    Upgraded to use AutoController

--- Events.cs
+++ Events.cs
@@ -11,40 +11,10 @@ namespace Api.Eventing
 	/// </summary>
 	public partial class Events
 	{
-		#region Service events
-		
 		/// <summary>
 		/// Set of events for an Article.
 		/// </summary>
 		public static EventGroup<Article> Article;
-		
-		#endregion
-
-		#region Controller events
-
-		/// <summary>
-		/// Create a new article.
-		/// </summary>
-		public static EndpointEventHandler<ArticleAutoForm> ArticleCreate;
-		/// <summary>
-		/// Delete an article.
-		/// </summary>
-		public static EndpointEventHandler<Article> ArticleDelete;
-		/// <summary>
-		/// Update article metadata.
-		/// </summary>
-		public static EndpointEventHandler<ArticleAutoForm> ArticleUpdate;
-		/// <summary>
-		/// Load article metadata.
-		/// </summary>
-		public static EndpointEventHandler<Article> ArticleLoad;
-		/// <summary>
-		/// List articles.
-		/// </summary>
-		public static EndpointEventHandler<Filter<Article>> ArticleList;
-
-		#endregion
-
 	}
 
 }
