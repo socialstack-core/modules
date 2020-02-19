818ef9d067dfbad75d39c10d9a15371e22005e53
Luke Briggs <luke@kulestar.com>
2020-02-19 02:47:25 +0000

    Tidied up some spacing

--- ArticleController.cs
+++ ArticleController.cs
@@ -6,10 +6,8 @@ namespace Api.Articles
 	/// <summary>
 	/// Handles article endpoints.
 	/// </summary>
-
 	[Route("v1/article")]
 	public partial class ArticleController : AutoController<Article, ArticleAutoForm>
 	{
     }
-
 }
