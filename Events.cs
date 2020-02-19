17fcc4fd80dde26033680729e00d34d822d22f17
Luke Briggs <luke@kulestar.com>
2020-02-19 02:47:50 +0000

    Upgraded to AutoController

--- Events.cs
+++ Events.cs
@@ -4,48 +4,15 @@ using System.Collections.Generic;
 
 namespace Api.Eventing
 {
-
 	/// <summary>
 	/// Events are instanced automatically. 
 	/// You can however specify a custom type or instance them yourself if you'd like to do so.
 	/// </summary>
 	public partial class Events
 	{
-
-		#region Service events
-
 		/// <summary>
 		/// Set of events for a NavMenuItem.
 		/// </summary>
 		public static EventGroup<NavMenuItem> NavMenuItem;
-		
-		#endregion
-
-		#region Controller events
-
-		/// <summary>
-		/// Create a new nav menu item.
-		/// </summary>
-		public static EndpointEventHandler<NavMenuItemAutoForm> NavMenuItemCreate;
-		/// <summary>
-		/// Delete an nav menu item.
-		/// </summary>
-		public static EndpointEventHandler<NavMenuItem> NavMenuItemDelete;
-		/// <summary>
-		/// Update nav menu item metadata.
-		/// </summary>
-		public static EndpointEventHandler<NavMenuItemAutoForm> NavMenuItemUpdate;
-		/// <summary>
-		/// Load nav menu item metadata.
-		/// </summary>
-		public static EndpointEventHandler<NavMenuItem> NavMenuItemLoad;
-		/// <summary>
-		/// List nav menu items.
-		/// </summary>
-		public static EndpointEventHandler<Filter<NavMenuItem>> NavMenuItemList;
-
-		#endregion
-
 	}
-
 }
