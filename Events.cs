1a29682382ecfe1e726a42fa111e436b2833d1fe
Luke Briggs <luke@kulestar.com>
2020-02-19 02:22:55 +0000

    Upgraded to AutoController

--- Events.cs
+++ Events.cs
@@ -11,41 +11,10 @@ namespace Api.Eventing
 	/// </summary>
 	public partial class Events
 	{
-
-		#region Service events
-		
 		/// <summary>
 		/// Set of events for a Blog.
 		/// </summary>
 		public static EventGroup<Blog> Blog;
-		
-		#endregion
-
-		#region Controller events
-
-		/// <summary>
-		/// Create a new blog.
-		/// </summary>
-		public static EndpointEventHandler<BlogAutoForm> BlogCreate;
-		/// <summary>
-		/// Delete an blog.
-		/// </summary>
-		public static EndpointEventHandler<Blog> BlogDelete;
-		/// <summary>
-		/// Update blog metadata.
-		/// </summary>
-		public static EndpointEventHandler<BlogAutoForm> BlogUpdate;
-		/// <summary>
-		/// Load blog metadata.
-		/// </summary>
-		public static EndpointEventHandler<Blog> BlogLoad;
-		/// <summary>
-		/// List blogs.
-		/// </summary>
-		public static EndpointEventHandler<Filter<Blog>> BlogList;
-
-		#endregion
-
 	}
 
 }
