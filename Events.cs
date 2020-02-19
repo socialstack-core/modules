8a3daa35e3f0816ca370659cc29e6c271d3068fc
Luke Briggs <luke@kulestar.com>
2020-02-19 01:39:27 +0000

    Upgraded to AutoService

--- Events.cs
+++ Events.cs
@@ -13,52 +13,12 @@ namespace Api.Eventing
 	{
 
 		#region Service events
-
+		
 		/// <summary>
-		/// Just before a new project is created. The given project won't have an ID yet. Return null to cancel the creation.
+		/// Set of events for a Project.
 		/// </summary>
-		public static EventHandler<Project> ProjectBeforeCreate;
-
-		/// <summary>
-		/// Just after an project has been created. The given project object will now have an ID.
-		/// </summary>
-		public static EventHandler<Project> ProjectAfterCreate;
-
-		/// <summary>
-		/// Just before an project is being deleted. Return null to cancel the deletion.
-		/// </summary>
-		public static EventHandler<Project> ProjectBeforeDelete;
-
-		/// <summary>
-		/// Just after an project has been deleted.
-		/// </summary>
-		public static EventHandler<Project> ProjectAfterDelete;
-
-		/// <summary>
-		/// Just before updating an project. Optionally make additional changes, or return null to cancel the update.
-		/// </summary>
-		public static EventHandler<Project> ProjectBeforeUpdate;
-
-		/// <summary>
-		/// Just after updating an project.
-		/// </summary>
-		public static EventHandler<Project> ProjectAfterUpdate;
-
-		/// <summary>
-		/// Just after an project was loaded.
-		/// </summary>
-		public static EventHandler<Project> ProjectAfterLoad;
-
-		/// <summary>
-		/// Just before a service loads an project list.
-		/// </summary>
-		public static EventHandler<Filter<Project>> ProjectBeforeList;
-
-		/// <summary>
-		/// Just after an project list was loaded.
-		/// </summary>
-		public static EventHandler<List<Project>> ProjectAfterList;
-
+		public static EventGroup<Project> Project;
+		
 		#endregion
 
 		#region Controller events
--- ProjectService.cs
+++ ProjectService.cs
@@ -11,99 +11,13 @@ namespace Api.Projects
 	/// Handles projects.
 	/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 	/// </summary>
-	public partial class ProjectService : IProjectService
+	public partial class ProjectService : AutoService<Project>, IProjectService
     {
-        private IDatabaseService _database;
-		
-		private readonly Query<Project> deleteQuery;
-		private readonly Query<Project> createQuery;
-		private readonly Query<Project> selectQuery;
-		private readonly Query<Project> listQuery;
-		private readonly Query<Project> updateQuery;
-
-
 		/// <summary>
 		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 		/// </summary>
-		public ProjectService(IDatabaseService database)
+		public ProjectService() : base(Events.Project)
         {
-            _database = database;
-			
-			// Start preparing the queries. Doing this ahead of time leads to excellent performance savings, 
-			// whilst also using a high-level abstraction as another plugin entry point.
-			deleteQuery = Query.Delete<Project>();
-			
-			createQuery = Query.Insert<Project>();
-			updateQuery = Query.Update<Project>();
-			selectQuery = Query.Select<Project>();
-			listQuery = Query.List<Project>();
-		}
-
-		/// <summary>
-		/// List a filtered set of projects.
-		/// </summary>
-		/// <returns></returns>
-		public async Task<List<Project>> List(Context context, Filter<Project> filter)
-		{
-			filter = await Events.ProjectBeforeList.Dispatch(context, filter);
-			var list = await _database.List(listQuery, filter);
-			list = await Events.ProjectAfterList.Dispatch(context, list);
-			return list;
-		}
-
-		/// <summary>
-		/// Deletes a Event by its ID.
-		/// Optionally includes uploaded content refs in there too.
-		/// </summary>
-		/// <returns></returns>
-		public async Task<bool> Delete(Context context, int id)
-        {
-            // Delete the entry:
-			await _database.Run(deleteQuery, id);
-			
-			// Ok!
-			return true;
-        }
-        
-		/// <summary>
-		/// Gets a single Event by its ID.
-		/// </summary>
-		public async Task<Project> Get(Context context, int id)
-		{
-			return await _database.Select(selectQuery, id);
-		}
-
-		/// <summary>
-		/// Creates a new project.
-		/// </summary>
-		public async Task<Project> Create(Context context, Project prj)
-		{
-			prj = await Events.ProjectBeforeCreate.Dispatch(context, prj);
-
-			// Note: The Id field is automatically updated by Run here.
-			if (prj == null || !await _database.Run(createQuery, prj))
-			{
-				return null;
-			}
-
-			prj = await Events.ProjectAfterCreate.Dispatch(context, prj);
-			return prj;
-		}
-
-		/// <summary>
-		/// Updates the given project.
-		/// </summary>
-		public async Task<Project> Update(Context context, Project prj)
-		{
-			prj = await Events.ProjectBeforeUpdate.Dispatch(context, prj);
-
-			if (prj == null || !await _database.Run(updateQuery, prj, prj.Id))
-			{
-				return null;
-			}
-
-			prj = await Events.ProjectAfterUpdate.Dispatch(context, prj);
-			return prj;
 		}
 	}
     
