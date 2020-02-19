ce332ea00a1d6231b759d9dabb331733e94d039c
Luke Briggs <luke@kulestar.com>
2020-02-19 03:26:55 +0000

    Upgraded to AutoService and AutoController

--- Events.cs
+++ Events.cs
@@ -4,88 +4,15 @@ using System.Collections.Generic;
 
 namespace Api.Eventing
 {
-
 	/// <summary>
 	/// Events are instanced automatically. 
 	/// You can however specify a custom type or instance them yourself if you'd like to do so.
 	/// </summary>
 	public partial class Events
 	{
-
-		#region Service events
-
 		/// <summary>
-		/// Just before a new forum is created. The given forum won't have an ID yet. Return null to cancel the creation.
+		/// Set of events for a Forum.
 		/// </summary>
-		public static EventHandler<Forum> ForumBeforeCreate;
-
-		/// <summary>
-		/// Just after an forum has been created. The given forum object will now have an ID.
-		/// </summary>
-		public static EventHandler<Forum> ForumAfterCreate;
-
-		/// <summary>
-		/// Just before an forum is being deleted. Return null to cancel the deletion.
-		/// </summary>
-		public static EventHandler<Forum> ForumBeforeDelete;
-
-		/// <summary>
-		/// Just after an forum has been deleted.
-		/// </summary>
-		public static EventHandler<Forum> ForumAfterDelete;
-
-		/// <summary>
-		/// Just before updating an forum. Optionally make additional changes, or return null to cancel the update.
-		/// </summary>
-		public static EventHandler<Forum> ForumBeforeUpdate;
-
-		/// <summary>
-		/// Just after updating an forum.
-		/// </summary>
-		public static EventHandler<Forum> ForumAfterUpdate;
-
-		/// <summary>
-		/// Just after an forum was loaded.
-		/// </summary>
-		public static EventHandler<Forum> ForumAfterLoad;
-
-		/// <summary>
-		/// Just before a service loads an forum list.
-		/// </summary>
-		public static EventHandler<Filter<Forum>> ForumBeforeList;
-
-		/// <summary>
-		/// Just after an forum list was loaded.
-		/// </summary>
-		public static EventHandler<List<Forum>> ForumAfterList;
-
-		#endregion
-
-		#region Controller events
-
-		/// <summary>
-		/// Create a new forum.
-		/// </summary>
-		public static EndpointEventHandler<ForumAutoForm> ForumCreate;
-		/// <summary>
-		/// Delete an forum.
-		/// </summary>
-		public static EndpointEventHandler<Forum> ForumDelete;
-		/// <summary>
-		/// Update forum metadata.
-		/// </summary>
-		public static EndpointEventHandler<ForumAutoForm> ForumUpdate;
-		/// <summary>
-		/// Load forum metadata.
-		/// </summary>
-		public static EndpointEventHandler<Forum> ForumLoad;
-		/// <summary>
-		/// List forums.
-		/// </summary>
-		public static EndpointEventHandler<Filter<Forum>> ForumList;
-
-		#endregion
-
+		public static EventGroup<Forum> Forum;
 	}
-
 }
--- ForumService.cs
+++ ForumService.cs
@@ -13,17 +13,10 @@ namespace Api.Forums
 	/// Handles creations of forums - containers for forum threads.
 	/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 	/// </summary>
-	public partial class ForumService : IForumService
+	public partial class ForumService : AutoService<Forum>, IForumService
     {
-        private IDatabaseService _database;
-		
-		private readonly Query<Forum> deleteForumQuery;
 		private readonly Query<ForumThread> deleteThreadsQuery;
 		private readonly Query<ForumReply> deleteRepliesQuery;
-		private readonly Query<Forum> createQuery;
-		private readonly Query<Forum> selectQuery;
-		private readonly Query<Forum> listQuery;
-		private readonly Query<Forum> updateQuery;
 		private readonly Query<Forum> updateThreadCountQuery;
 		private readonly Query<Forum> updateReplyCountQuery;
 
@@ -31,37 +24,28 @@ namespace Api.Forums
 		/// <summary>
 		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 		/// </summary>
-		public ForumService(IDatabaseService database)
+		public ForumService() : base(Events.Forum)
         {
-            _database = database;
-			
 			// Start preparing the queries. Doing this ahead of time leads to excellent performance savings, 
 			// whilst also using a high-level abstraction as another plugin entry point.
-			deleteForumQuery = Query.Delete<Forum>();
-
 			deleteThreadsQuery = Query.Delete<ForumThread>();
 			deleteThreadsQuery.Where().EqualsArg("ForumId", 0);
 
 			deleteRepliesQuery = Query.Delete<ForumReply>();
 			deleteRepliesQuery.Where().EqualsArg("ForumId", 0);
 			
-			createQuery = Query.Insert<Forum>();
-			updateQuery = Query.Update<Forum>();
-			selectQuery = Query.Select<Forum>();
-			listQuery = Query.List<Forum>();
-			
 			updateThreadCountQuery = Query.Update<Forum>().RemoveAllBut("Id", "ThreadCount");
 			updateReplyCountQuery = Query.Update<Forum>().RemoveAllBut("Id", "ReplyCount");
 			
 			// Add some events to bump our cached counters whenever a reply/ thread is added:
-			Events.ForumThreadAfterCreate.AddEventListener(async (Context context, ForumThread thread) =>
+			Events.ForumThread.AfterCreate.AddEventListener(async (Context context, ForumThread thread) =>
 			{
 				await _database.Run(updateThreadCountQuery, thread.ForumId, 1);
 
 				return thread;
 			});
 			
-			Events.ForumReplyAfterCreate.AddEventListener(async (Context context, ForumReply reply) =>
+			Events.ForumReply.AfterCreate.AddEventListener(async (Context context, ForumReply reply) =>
 			{
 				await _database.Run(updateReplyCountQuery, reply.ForumId, 1);
 
@@ -71,13 +55,33 @@ namespace Api.Forums
 		
         /// <summary>
         /// Deletes a forum by its ID.
+		/// Includes deleting all replies, threads and uploaded content refs in there too.
+        /// </summary>
+        /// <returns></returns>
+        public override async Task<bool> Delete(Context context, int id)
+        {
+            // Delete the forum entry:
+			await base.Delete(context, id);
+			
+			// Delete threads:
+			await _database.Run(deleteThreadsQuery, id);
+			
+			// Delete their replies:
+			await _database.Run(deleteRepliesQuery, id);
+			
+			// Ok!
+			return true;
+        }
+		
+        /// <summary>
+        /// Deletes a forum by its ID.
 		/// Optionally includes deleting all replies, threads and uploaded content refs in there too.
         /// </summary>
         /// <returns></returns>
-        public async Task<bool> Delete(Context context, int id, bool deleteThreads = true)
+        public async Task<bool> Delete(Context context, int id, bool deleteThreads)
         {
             // Delete the forum entry:
-			await _database.Run(deleteForumQuery, id);
+			await base.Delete(context, id);
 			
 			if(deleteThreads){
 				// Delete threads:
@@ -90,59 +94,6 @@ namespace Api.Forums
 			// Ok!
 			return true;
         }
-
-		/// <summary>
-		/// List a filtered set of forums.
-		/// </summary>
-		/// <returns></returns>
-		public async Task<List<Forum>> List(Context context, Filter<Forum> filter)
-		{
-			filter = await Events.ForumBeforeList.Dispatch(context, filter);
-			var list = await _database.List(listQuery, filter);
-			list = await Events.ForumAfterList.Dispatch(context, list);
-			return list;
-		}
-
-		/// <summary>
-		/// Gets a single forum by its ID.
-		/// </summary>
-		public async Task<Forum> Get(Context context, int id)
-		{
-			return await _database.Select(selectQuery, id);
-		}
-
-		/// <summary>
-		/// Creates a new forum.
-		/// </summary>
-		public async Task<Forum> Create(Context context, Forum forum)
-		{
-			forum = await Events.ForumBeforeCreate.Dispatch(context, forum);
-
-			// Note: The Id field is automatically updated by Run here.
-			if (forum == null || !await _database.Run(createQuery, forum))
-			{
-				return null;
-			}
-
-			forum = await Events.ForumAfterCreate.Dispatch(context, forum);
-			return forum;
-		}
-
-		/// <summary>
-		/// Updates the given forum.
-		/// </summary>
-		public async Task<Forum> Update(Context context, Forum forum)
-		{
-			forum = await Events.ForumBeforeUpdate.Dispatch(context, forum);
-
-			if (forum == null || !await _database.Run(updateQuery, forum, forum.Id))
-			{
-				return null;
-			}
-
-			forum = await Events.ForumAfterUpdate.Dispatch(context, forum);
-			return forum;
-		}
 	}
     
 }
