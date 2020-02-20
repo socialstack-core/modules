8f84655fe4bcfd2c999dfbbc05e09b2c80a28747
Luke Briggs <luke@kulestar.com>
2020-02-20 21:33:00 +0000

    Added propTypes spec

--- Image.js
+++ Image.js
@@ -6,4 +6,13 @@ Used to display an image from a fileRef.
 Min required props: size, fileRef. Size must be an available size from your uploader config. You should pick the nearest bigger size from the physical size you're after.
 <Image fileRef='public:2.jpg' size=100 />
 */
-export default (props) => <div className="image" onClick={props.onClick}>{getRef(props.fileRef, {...(omit(props, ['fileRef', 'onClick']))})}</div>
+export default class Image extends React.Component {
+	render(){
+		return <div className="image" onClick={this.props.onClick}>{getRef(this.props.fileRef, {...(omit(this.props, ['fileRef', 'onClick']))})}</div>;
+	}
+}
+
+Image.propTypes = {
+	fileRef: 'string',
+	size: 'int'
+};
