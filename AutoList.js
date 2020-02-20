89fdf5ba84773ec61a06eaccfc28d0da834b86d3
Luke Briggs <luke@kulestar.com>
2020-02-20 12:10:46 +0000

    All fields in AutoList are now sortable

--- AutoList.js
+++ AutoList.js
@@ -7,18 +7,68 @@ export default class AutoList extends React.Component {
 	
 	constructor(props){
 		super(props);
+		
+		this.state={
+			// If an id field is specified, that's the default sort
+			sort: this.props.fields.find(field => field == 'id') ? {field: 'id', direction: 'desc'} : null
+		};
 	}
 	
 	render(){
-		return <Tile>
-			<Loop asTable over={this.props.endpoint + "/list"} {...this.props}>
-			{
+		var combinedFilter = {...this.props.filter};
+		
+		if(!combinedFilter.sort && this.state.sort){
+			combinedFilter.sort = this.state.sort;
+		}
+		
+		return <Tile className="auto-list">
+			<Loop asTable over={this.props.endpoint + "/list"} {...this.props} filter={combinedFilter}>
+			{[
+				allContent => {
+					// Header (Optional)
+					return this.props.fields.map(field => {
+						
+						// Class for styling the sort buttons:
+						var sortingByThis = this.state.sort && this.state.sort.field == field;
+						var className = '';
+						
+						if(sortingByThis){
+							className = this.state.sort.direction == 'desc' ? 'sorted-desc' : 'sorted-asc';
+						}
+						
+						// Field name with its first letter uppercased:
+						var ucFirstFieldName = field.length ? field.charAt(0).toUpperCase() + field.slice(1) : '';
+						
+						return (
+							<th className={className}>
+								{ucFirstFieldName} <i className="fa fa-caret-down" onClick={() => {
+									// Sort desc
+									this.setState({
+										sort: {
+											field,
+											direction: 'desc'
+										}
+									});
+								}}/> <i className="fa fa-caret-up" onClick={() => {
+									// Sort asc
+									this.setState({
+										sort: {
+											field,
+											direction: 'asc'
+										}
+									});
+								}}/>
+							</th>
+						);
+					});
+				},
 				entry => {
+					// Each row
 					return this.props.fields.map(field => <td><a href={this.props.path + '' + entry.id}>{
 							field.endsWith("Json") ? <Canvas>{entry[field]}</Canvas> : entry[field]
 						}</a></td>);
 				}
-			}
+			]}
 			</Loop>
 		</Tile>;
 	}
