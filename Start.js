9137bf81e2337dc1b3ce7127b9c84e9160d68f5c
Luke Briggs <luke@kulestar.com>
2020-02-25 20:53:04 +0000

    Start now exports a function in order to avoid needing to make it a special case when iterating over modules. Also contains a small event mechanism for UI modules to communicate with each other.

--- Start.js
+++ Start.js
@@ -1,32 +1,51 @@
 import App from './App.js';
 
-if(typeof document != 'undefined'){
-	// We're server side otherwise. It would've set global.app internally.
-
-	/*
-	* Navigates in a safe in-page way.
-	*/
-	global.navigateReact = function(url, data){
-		global.__history.push(url);
-	};
-
-	// Start logging in now and update the loader text:
-	var loader = document.getElementById('react-loading');
-	if(loader){
-		loader.parentNode.removeChild(loader);
-	}
-
-	global.hashFields = {};
-
-	(global.location && global.location.hash) && global.location.hash.substring(1).split('&').forEach(e => {
-		var parts = e.split('=');
-		global.hashFields[parts[0]] = parts.length>1 ? parts[1] : true;
-	});
-
-	// Render the root now! When the App instance is created, it sets itself up as global.app
-	React.render(
-		<App />,
-		document.getElementById('react-root')
-	);
+module.exports = function(){
 	
-}
+	// Setup a simple event mechanism for communication between modules (note: EventTarget isn't available in Node):
+	global.events = {
+		get: function(name){
+			if(!global.events[name]){
+				return global.events[name] = {};
+			}
+			return global.events[name];
+		}
+	};
+	
+	// Init all modules.
+	for(var m in __mm){
+		__mm[m] && require(m);
+	}
+	
+	if(typeof document != 'undefined'){
+		// We're server side otherwise. It would've set global.app internally.
+
+		/*
+		* Navigates in a safe in-page way.
+		*/
+		global.navigateReact = function(url, data){
+			global.__history.push(url);
+		};
+
+		// Start logging in now and update the loader text:
+		var loader = document.getElementById('react-loading');
+		if(loader){
+			loader.parentNode.removeChild(loader);
+		}
+
+		global.hashFields = {};
+
+		(global.location && global.location.hash) && global.location.hash.substring(1).split('&').forEach(e => {
+			var parts = e.split('=');
+			global.hashFields[parts[0]] = parts.length>1 ? parts[1] : true;
+		});
+
+		// Render the root now! When the App instance is created, it sets itself up as global.app
+		React.render(
+			<App />,
+			document.getElementById('react-root')
+		);
+		
+	}
+	
+};
