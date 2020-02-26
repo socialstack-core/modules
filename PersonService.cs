a8c41b20df4d08cbfcc2cf987497958da0f124d4
Luke Briggs <luke@kulestar.com>
2020-02-26 16:02:22 +0000

    Initial commit

---
+++ PersonService.cs
@@ -0,0 +1,24 @@
+﻿using Api.Database;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Api.Permissions;
+using Api.Contexts;
+using Api.Eventing;
+
+namespace Api.Persons
+{
+	/// <summary>
+	/// Handles people.
+	/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
+	/// </summary>
+	public partial class PersonService : AutoService<Person>, IPersonService
+    {
+		/// <summary>
+		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
+		/// </summary>
+		public PersonService() : base(Events.Person)
+        {
+		}
+	}
+    
+}
