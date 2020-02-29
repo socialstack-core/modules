f826cf2548069036064e7e1fd755d63bcb9d8975
Luke Briggs <luke@kulestar.com>
2020-02-29 14:46:42 +0000

    Now has a public API so it's available to content editors

--- Alert.js
+++ Alert.js
@@ -2,23 +2,32 @@
 	A bubble like block message. Not a popup or toast message (although it can be used as either via nesting).
 */
 
-export default (props) => {
+export default class Alert extends React.Component {
 	
-	switch(props.type){
-		case "fail":
-		case "failure":
-		case "failed":
-		case "error":
-			return <div className="alert alert-danger">{props.children}</div>;
-		case "warn":
-		case "warning":
-			return <div className="alert alert-warning">{props.children}</div>;
-		default:
-		case "success":
-		case "successful":
-		case "ok":
-		case "good":
-			return <div className="alert alert-success">{props.children}</div>;
-		break;
+	render(){
+		switch(this.props.type){
+			case "fail":
+			case "failure":
+			case "failed":
+			case "error":
+				return <div className="alert alert-danger">{this.props.children}</div>;
+			case "warn":
+			case "warning":
+				return <div className="alert alert-warning">{this.props.children}</div>;
+			default:
+			case "success":
+			case "successful":
+			case "ok":
+			case "good":
+				return <div className="alert alert-success">{this.props.children}</div>;
+			break;
+		}
 	}
 }
+
+Alert.propTypes = {
+	type: ['error', 'warning', 'success'],
+	children: true
+};
+
+Alert.icon='exclamation-circle';
