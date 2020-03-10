77c380dd7d64fb8cc55066e1298718574b15f3ac
Michael Rogers <michaelr@conceptsoftworks.com>
2020-03-10 10:38:57 -0700

    Added Permissions.cs

---
+++ Permissions.cs
@@ -0,0 +1,30 @@
+﻿using Api.Startup;
+using Api.Eventing;
+using Api.Contexts;
+using Api.Permissions;
+using System.Threading.Tasks;
+
+namespace Api.Comments
+{
+	/// <summary>
+	/// Instances capabilities during the very earliest phases of startup.
+	/// </summary>
+	[EventListener]
+	public class Permissions
+	{
+		/// <summary>
+		/// Instanced automatically.
+		/// </summary>
+		public Permissions()
+		{
+			// Hook the default role setup. It's done like this so it can be removed by a plugin if wanted.
+			Events.CapabilityOnSetup.AddEventListener((Context context, object source) =>
+			{
+				// Public - the role used by anonymous users.
+				Roles.Guest.Grant("comment_create");
+				Roles.Member.Grant("comment_create");
+				return Task.FromResult(source);
+			});
+		}
+	}
+}
