bf47c2ac74b70e51bf46be21796e5b56de828862
Luke Briggs <luke@kulestar.com>
2020-03-14 03:16:28 +0000

    Added ability to get the complete locale object

--- Context.cs
+++ Context.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Api.Permissions;
 using Api.Startup;
+using Api.Translate;
 using Api.Users;
 
 
@@ -18,11 +19,45 @@ namespace Api.Contexts
 		/// </summary>
 		private static System.Security.Principal.GenericIdentity GenericIdentity= new System.Security.Principal.GenericIdentity("User");
 		private static IUserService _users;
+		private static ILocaleService _locales;
 
 		/// <summary>
 		/// The current locale or the site default.
 		/// </summary>
 		public int LocaleId = 1;
+
+		/// <summary>
+		/// The full locale object, if it has been requested.
+		/// </summary>
+		private Locale _locale;
+
+		/// <summary>
+		/// Gets the locale for this context.
+		/// </summary>
+		/// <returns></returns>
+		public async Task<Locale> GetLocale()
+		{
+			if (_locale != null)
+			{
+				return _locale;
+			}
+			
+			if (_locales == null)
+			{
+				_locales = Services.Get<ILocaleService>();
+			}
+
+			// Get the user now:
+			_locale = await _locales.GetCached(this, LocaleId <= 0 ? 1 : LocaleId);
+
+			if (_locale == null)
+			{
+				// Dodgy locale in the cookie. Locale #1 always exists.
+				return await _locales.GetCached(this, 1);
+			}
+			
+			return _locale;
+		}
 
 		/// <summary>
 		///  The logged in users ID.
