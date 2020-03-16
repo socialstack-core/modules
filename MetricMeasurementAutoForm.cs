0c19cda469a6867b28852708c199c9d91e703371
Luke Briggs <luke@kulestar.com>
2020-03-16 16:25:36 +0000

    Added metric measurement API

---
+++ MetricMeasurementAutoForm.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Api.AutoForms;
+using System;
+
+namespace Api.Metrics
+{
+    /// <summary>
+    /// Used when creating or updating a metric measurement.
+    /// </summary>
+    public partial class MetricMeasurementAutoForm : AutoForm<MetricMeasurement>
+    {
+		/// <summary>
+		/// The metric that this will be supplying data to.
+		/// </summary>
+		public int MetricId;
+	}
+}
---
+++ MetricMeasurementController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Api.Metrics
+{
+    /// <summary>
+    ///  Handles Metric measurement endpoints.
+    /// </summary>
+    [Route("v1/metricmeasurement")]
+    public partial class MetricMeasurementController: AutoController<MetricMeasurement, MetricMeasurementAutoForm>
+    {
+    }
+}
