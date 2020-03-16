dd5e77111a42a7a0bc800adea3c3d1358b354cc7
Luke Briggs <luke@kulestar.com>
2020-03-16 23:50:02 +0000

    Can now use the spacer from the editor

--- Spacer.js
+++ Spacer.js
@@ -2,7 +2,17 @@
 Just an invisible space of a specified height. The default is 20px.
 */
 
+export default class Spacer extends React.Component {
+	
+	render(){
+		return <div className="spacer-container">
+			<div className="spacer" style={{height: this.props.height ? (this.props.height + 'px') : '20px'}}/>
+		</div>
+	}
+}
 
-export default (props) => <div className="spacer-container">
-		<div className="spacer" style={{height: props.height ? (props.height + 'px') : '20px'}}/>
-	</div>
+Spacer.propTypes = {
+	height: 'int'
+};
+
+Spacer.icon = 'sort';
