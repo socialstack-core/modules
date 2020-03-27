f6a054d543c26f553ff7ab75c136a09e34b3fbb7
Luke Briggs <luke@kulestar.com>
2020-03-27 00:14:16 +0000

    Initial commit

---
+++ HasCapability.js
@@ -0,0 +1,34 @@
+import webRequest from 'UI/Functions/WebRequest';
+
+/* cache */
+var cache = null;
+
+/*
+* Gets the list of content types on the site. Returns a promise.
+*/
+export default (capName) => {
+	var gs = global.app.state;
+	
+	if(gs.loadingUser){
+		return gs.loadingUser.then(() => loadCached(capName));
+	}
+	
+	return loadCached(capName);
+}
+
+function loadCached(capName) {
+	if(cache != null){
+		return Promise.resolve(cache).then(caps => !!caps[capName]);
+	}
+	
+	var gs = global.app.state;
+	var roleId = gs.user ? gs.user.role : 0;
+	
+	return cache = webRequest("permission/role/" + roleId).then(response => {
+		cache = {};
+		response.json.capabilities.forEach(c => {
+			cache[c.name] = c;
+		});
+		return cache;
+	}).then(caps => !!caps[capName]);
+}
