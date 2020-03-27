43bef6d495e63ef8c65563a41f8163b567a7c508
Michael Rogers <michaelr@conceptsoftworks.com>
2020-03-27 11:45:09 -0700

    Articles extends HasCategories

---
+++ Types/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Categories;
+
+namespace Api.Articles
+{
+
+	/// <summary>
+	/// An article, typically used in e.g. help guides or knowledge bases.
+	/// </summary>
+	public partial class Article : IHaveCategories
+	{
+		public List<Category> Categories { get; set; }
+	}
+}
