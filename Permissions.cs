7a93a2ebab54d6f9c6e2a2ed5efbbadd73213982
Michael Rogers <michaelr@conceptsoftworks.com>
2020-03-30 16:05:26 -0700

    Added Permission for loading user with guests and public.

---
+++ Permissions.cs
@@ -0,0 +1,31 @@
+﻿using Api.Startup;
+using Api.Eventing;
+using Api.Contexts;
+using Api.Permissions;
+using System.Threading.Tasks;
+
+namespace Api.Users
+{
+	/// <summary>
+	/// Instances capabilities during the very earliest phases of startup.
+	/// </summary>
+	[EventListener]
+	public class Permissions
+	{
+		/// <summary>
+		/// Instanced automatically.
+		/// </summary>
+		public Permissions()
+		{
+			// Hook the default role setup. It's done like this so it can be removed by a plugin if wanted.
+			Events.CapabilityOnSetup.AddEventListener((Context context, object source) =>
+			{
+				// Public - the role used by anonymous users.
+				Roles.Guest.Grant("user_load");
+				Roles.Public.Grant("user_load");
+				Roles.Member.Grant("user_load");
+				return Task.FromResult(source);
+			});
+		}
+	}
+}
