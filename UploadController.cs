3b769a76ab027879e5beae8227c83b49ace8a0bd
Luke Briggs <luke@kulestar.com>
2020-03-31 00:26:39 +0100

    Fully automated AutoForm

--- UploadController.cs
+++ UploadController.cs
@@ -18,131 +18,15 @@ namespace Api.Uploader
     /// </summary>
 
     [Route("v1/uploader")]
-	[ApiController]
-	public partial class UploadController : ControllerBase
+	public partial class UploadController : AutoController<Upload>
     {
-        private IUploadService _uploads;
-
-
 		/// <summary>
 		/// Instanced automatically.
 		/// </summary>
-		public UploadController(
-			IUploadService Uploads
-        )
+		public UploadController() : base()
         {
-			_uploads = Uploads;
         }
-
-		/// <summary>
-		/// GET /v1/upload/2/
-		/// Returns the upload data for a single item.
-		/// </summary>
-		[HttpGet("{id}")]
-		public async Task<Upload> Load([FromRoute] int id)
-		{
-			var context = Request.GetContext();
-			var result = await _uploads.Get(context, id);
-			return await Events.UploadLoad.Dispatch(context, result, Response);
-		}
 		
-		/// <summary>
-		/// DELETE /v1/upload/2/
-		/// Deletes an upload
-		/// </summary>
-		[HttpDelete("{id}")]
-		public async Task<Upload> Delete([FromRoute] int id)
-		{
-			var context = Request.GetContext();
-			var result = await _uploads.Get(context, id);
-			result = await Events.UploadDelete.Dispatch(context, result, Response);
-
-			if (result == null || !await _uploads.Delete(context, id))
-			{
-				// The handlers have blocked this one from happening, or it failed
-				return null;
-			}
-
-			return result;
-		}
-
-		/// <summary>
-		/// GET /v1/upload/list
-		/// Lists all uploads available to this user.
-		/// </summary>
-		/// <returns></returns>
-		[HttpGet("list")]
-		public async Task<Set<Upload>> List()
-		{
-			return await List(null);
-		}
-
-		/// <summary>
-		/// POST /v1/upload/list
-		/// Lists filtered uploads available to this user.
-		/// See the filter documentation for more details on what you can request here.
-		/// </summary>
-		/// <returns></returns>
-		[HttpPost("list")]
-		public async Task<Set<Upload>> List([FromBody] JObject filters)
-		{
-			var context = Request.GetContext();
-			var filter = new Filter<Upload>(filters);
-
-			filter = await Events.UploadList.Dispatch(context, filter, Response);
-
-			if (filter == null)
-			{
-				// A handler rejected this request.
-				return null;
-			}
-
-			var results = await _uploads.List(context, filter);
-			return new Set<Upload>() { Results = results };
-		}
-
-		/*
-		/// <summary>
-		/// POST /v1/upload/
-		/// Creates a new upload. Returns the ID.
-		/// </summary>
-		[HttpPost]
-		public async Task<Upload> Create([FromBody] UploadAutoForm form)
-		{
-			var context = Request.GetContext();
-			
-			// Start building up our object.
-			// Most other fields, particularly custom extensions, are handled by autoform.
-			var upload = new Upload
-			{
-				UserId = context.UserId
-			};
-			
-			if (!ModelState.Setup(form, upload))
-			{
-				return null;
-			}
-
-			form = await Events.UploadCreate.Dispatch(context, form, Response);
-
-			if (form == null || form.Result == null)
-			{
-				// A handler rejected this request.
-				return null;
-			}
-
-			upload = await _uploads.Create(context, form.Result);
-
-			if (upload == null)
-			{
-				Response.StatusCode = 500;
-				return null;
-			}
-			
-            return upload;
-        }
-		*/
-
 		/// <summary>
 		/// Upload a file for this user.
 		/// </summary>
@@ -175,40 +59,6 @@ namespace Api.Uploader
 				publicUrl = upload.GetPublicUrl("original"),
 				isImage = upload.IsImage
 			};
-		}
-
-		/// <summary>
-		/// POST /v1/upload/1/
-		/// Updates a nav menu item with the given ID.
-		/// </summary>
-		[HttpPost("{id}")]
-		public async Task<Upload> Update([FromRoute] int id, [FromBody] UploadAutoForm form)
-		{
-			var context = Request.GetContext();
-
-			var upload = await _uploads.Get(context, id);
-			
-			if (!ModelState.Setup(form, upload)) {
-				return null;
-			}
-
-			form = await Events.UploadUpdate.Dispatch(context, form, Response);
-
-			if (form == null || form.Result == null)
-			{
-				// A handler rejected this request.
-				return null;
-			}
-			
-			upload = await _uploads.Update(context, form.Result);
-
-			if (upload == null)
-			{
-				Response.StatusCode = 500;
-				return null;
-			}
-			
-			return upload;
 		}
 
     }
