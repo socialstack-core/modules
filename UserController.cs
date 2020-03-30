2e3f8161fa00b62311f6af3c92711f3e560f2b8b
Luke Briggs <luke@kulestar.com>
2020-03-31 00:23:33 +0100

    AutoForm upgrade (first round)

--- UserController.cs
+++ UserController.cs
@@ -19,72 +19,21 @@ namespace Api.Users
     /// </summary>
 
     [Route("v1/user")]
-	[ApiController]
-	public partial class UserController : ControllerBase
+	public partial class UserController : AutoController<User>
     {
-        private IDatabaseService _database;
-        private IUserService _users;
-        private IEmailService _email;
 		private IContextService _contexts;
-		private IUploadService _uploads;
-		private IPasswordResetService _passwordReset;
-		private readonly Query<User> listQuery;
 
 
 		/// <summary>
 		/// Instanced automatically.
 		/// </summary>
 		public UserController(
-            IDatabaseService database, IUserService users, IContextService contexts, 
-            IEmailService email, IUploadService uploads, IPasswordResetService passwordReset
+            IContextService contexts
 		)
         {
-            _database = database;
-            _users = users;
-            _email = email;
 			_contexts = contexts;
-            _uploads = uploads;
-			_passwordReset = passwordReset;
-
-			listQuery = Query.List<User>();
-
 		}
-
-		/// <summary>
-		/// GET /v1/user/list
-		/// Lists all users visible to this user.
-		/// </summary>
-		/// <returns></returns>
-		[HttpGet("list")]
-		public async Task<Set<User>> List()
-		{
-			return await List(null);
-		}
-
-		/// <summary>
-		/// POST /v1/user/list
-		/// Lists filtered users visible to this user.
-		/// See the filter documentation for more details on what you can request here.
-		/// </summary>
-		/// <returns></returns>
-		[HttpPost("list")]
-		public async Task<Set<User>> List([FromBody] JObject filters)
-		{
-			var context = Request.GetContext();
-			var filter = new Filter<User>(filters);
-
-			filter = await Events.UserList.Dispatch(context, filter, Response);
-
-			if (filter == null)
-			{
-				// A handler rejected this request.
-				return null;
-			}
-
-			var results = await _users.List(context, filter);
-			return new Set<User>() { Results = results };
-		}
-
+		
 		/// <summary>
 		/// Gets the currently logged in user, or null if there isn't one.
 		/// </summary>
@@ -120,38 +69,6 @@ namespace Api.Users
 				return null;
 			}
 			
-			return result;
-		}
-
-		/// <summary>
-		/// GET /v1/user/2/
-		/// Returns the user data for a single user.
-		/// </summary>
-		[HttpGet("{id}")]
-		public async Task<User> Load([FromRoute] int id)
-		{
-			var context = Request.GetContext();
-			var result = await _users.Get(context, id);
-			return await Events.UserLoad.Dispatch(context, result, Response);
-        }
-
-		/// <summary>
-		/// DELETE /v1/user/2/
-		/// Deletes a user
-		/// </summary>
-		[HttpDelete("{id}")]
-		public async Task<User> Delete([FromRoute] int id)
-		{
-			var context = Request.GetContext();
-			var result = await _users.Get(context, id);
-			result = await Events.UserDelete.Dispatch(context, result, Response);
-
-			if (result == null || !await _users.Delete(context, id))
-			{
-				// The handlers have blocked this one from happening, or it failed
-				return null;
-			}
-
 			return result;
 		}
 
@@ -214,92 +131,6 @@ namespace Api.Users
 			return result;
         }
 
-		/// <summary>
-		/// POST /v1/user/
-		/// Creates a new user. Returns the ID.
-		/// </summary>
-		[HttpPost]
-		public async Task<User> Create([FromBody] UserAutoForm form)
-		{
-			var context = Request.GetContext();
-
-			int role = Roles.Member.Id;
-
-			var user = new User()
-			{
-				Role = role,
-				JoinedUtc = DateTime.UtcNow
-			};
-
-			if (!ModelState.Setup(form, user))
-			{
-				return null;
-			}
-
-			form = await Events.UserCreate.Dispatch(context, form, Response);
-
-			if (form == null || form.Result == null)
-			{
-				// A handler rejected this request.
-				return null;
-			}
-
-			await _users.Create(context, form.Result);
-
-			return user;
-		}
-
-		/// <summary>
-		/// POST /v1/user/{id}/
-		/// Updates a user account
-		/// </summary>
-		[HttpPost("{id}")]
-		public async Task<User> Update([FromRoute] int id, [FromBody] UserAutoForm form)
-		{
-			var context = Request.GetContext();
-			
-			// Get the user:
-			var user = await _users.Get(context, id);
-
-			if (!ModelState.Setup(form, user))
-			{
-				return null;
-			}
-
-			form = await Events.UserUpdate.Dispatch(context, form, Response);
-
-			if (form == null || form.Result == null)
-			{
-				// A handler rejected this request.
-				return null;
-			}
-			
-			/*
-            // Can edit internal details and internal details are being submitted:
-            if (body.Role != null)
-            {
-				user.Role = body.Role.Value;
-
-				// Bump login ref and broadcast a revoke for prior values:
-				_contexts.Revoke(user.Id, user.LoginRevokeCount);
-
-				user.LoginRevokeCount++;
-            }
-
-			if (body.Password != null)
-            {
-                // Setting a new password:
-                var newPasswordHash = PasswordStorage.CreateHash(body.Password);
-				user.PasswordHash = newPasswordHash;
-			}
-			*/
-
-			// Update now:
-			await _users.Update(context, form.Result);
-
-			return user;
-        }
-		
     }
 
 }
