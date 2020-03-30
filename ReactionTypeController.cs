85948134064ef556be0cbbc2c1a1430aa46e128d
Luke Briggs <luke@kulestar.com>
2020-03-31 00:29:39 +0100

    Fully automated AutoForm

--- ReactionTypeController.cs
+++ ReactionTypeController.cs
@@ -6,7 +6,7 @@ namespace Api.Reactions
     /// Handles reaction types (creating a new type of like/ upvote etc).
     /// </summary>
     [Route("v1/reactiontype")]
-	public partial class ReactionTypeController : AutoController<ReactionType, ReactionTypeAutoForm>
+	public partial class ReactionTypeController : AutoController<ReactionType>
     {
     }
 }
