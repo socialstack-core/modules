f4567431e310ba73454dcfeeffde9c654e1ed6c7
Luke Briggs <luke@kulestar.com>
2020-03-31 01:54:44 +0100

    Upgraded to AutoService

--- Events.cs
+++ Events.cs
@@ -11,81 +11,10 @@ namespace Api.Eventing
 	/// </summary>
 	public partial class Events
     {
-
-		#region Service events
-
 		/// <summary>
-		/// Just before a new feed story is created. The given feed story won't have an ID yet. Return null to cancel the creation.
+		/// Set of events for a FeedStory.
 		/// </summary>
-		public static EventHandler<FeedStory> FeedStoryBeforeCreate;
-
-		/// <summary>
-		/// Just after an feed story has been created. The given feed story object will now have an ID.
-		/// </summary>
-		public static EventHandler<FeedStory> FeedStoryAfterCreate;
-
-		/// <summary>
-		/// Just before an feed story is being deleted. Return null to cancel the deletion.
-		/// </summary>
-		public static EventHandler<FeedStory> FeedStoryBeforeDelete;
-
-		/// <summary>
-		/// Just after an feed story has been deleted.
-		/// </summary>
-		public static EventHandler<FeedStory> FeedStoryAfterDelete;
-
-		/// <summary>
-		/// Just before updating an feed story. Optionally make additional changes, or return null to cancel the update.
-		/// </summary>
-		public static EventHandler<FeedStory> FeedStoryBeforeUpdate;
-
-		/// <summary>
-		/// Just after updating an feed story.
-		/// </summary>
-		public static EventHandler<FeedStory> FeedStoryAfterUpdate;
-
-		/// <summary>
-		/// Just after an feed story was loaded.
-		/// </summary>
-		public static EventHandler<FeedStory> FeedStoryAfterLoad;
-
-		/// <summary>
-		/// Just before a service loads an feed story list.
-		/// </summary>
-		public static EventHandler<Filter<FeedStory>> FeedStoryBeforeList;
-
-		/// <summary>
-		/// Just after an feed story list was loaded.
-		/// </summary>
-		public static EventHandler<List<FeedStory>> FeedStoryAfterList;
-
-		#endregion
-
-		#region Controller events
-
-		/// <summary>
-		/// Create a new feed story.
-		/// </summary>
-		public static EndpointEventHandler<FeedStoryAutoForm> FeedStoryCreate;
-		/// <summary>
-		/// Delete an feed story.
-		/// </summary>
-		public static EndpointEventHandler<FeedStory> FeedStoryDelete;
-		/// <summary>
-		/// Update feed story metadata.
-		/// </summary>
-		public static EndpointEventHandler<FeedStoryAutoForm> FeedStoryUpdate;
-		/// <summary>
-		/// Load feed story metadata.
-		/// </summary>
-		public static EndpointEventHandler<FeedStory> FeedStoryLoad;
-		/// <summary>
-		/// List feed storys.
-		/// </summary>
-		public static EndpointEventHandler<Filter<FeedStory>> FeedStoryList;
-
-		#endregion
-
+		public static EventGroup<FeedStory> FeedStory;
 	}
 
 }
--- FeedStoryService.cs
+++ FeedStoryService.cs
@@ -18,102 +18,14 @@ namespace Api.FeedStories
 	/// Handles feed stories (entries within the news feed).
 	/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 	/// </summary>
-	public partial class FeedStoryService : IFeedStoryService
+	public partial class FeedStoryService : AutoService<FeedStory>, IFeedStoryService
     {
-        private IDatabaseService _database;
-		
-		private readonly Query<FeedStory> deleteQuery;
-		private readonly Query<FeedStory> createQuery;
-		private readonly Query<FeedStory> selectQuery;
-		private readonly Query<FeedStory> updateQuery;
-		private readonly Query<FeedStory> listQuery;
-
-
 		/// <summary>
 		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 		/// </summary>
-		public FeedStoryService(IDatabaseService database)
+		public FeedStoryService() : base(Events.FeedStory)
         {
-            _database = database;
-			
-			// Start preparing the queries. Doing this ahead of time leads to excellent performance savings, 
-			// whilst also using a high-level abstraction as another plugin entry point.
-			deleteQuery = Query.Delete<FeedStory>();
-			createQuery = Query.Insert<FeedStory>();
-			updateQuery = Query.Update<FeedStory>();
-			selectQuery = Query.Select<FeedStory>();
-			listQuery = Query.List<FeedStory>();
 		}
-
-		/// <summary>
-		/// List a filtered set of feed stories.
-		/// </summary>
-		/// <returns></returns>
-		public async Task<List<FeedStory>> List(Context context, Filter<FeedStory> filter)
-		{
-			filter = await Events.FeedStoryBeforeList.Dispatch(context, filter);
-			var list = await _database.List(context, listQuery, filter);
-			list = await Events.FeedStoryAfterList.Dispatch(context, list);
-			return list;
-		}
-
-		/// <summary>
-		/// Deletes a feed story by its ID.
-		/// Optionally includes uploaded content refs in there too.
-		/// </summary>
-		/// <returns></returns>
-		public async Task<bool> Delete(Context context, int id, bool deleteUploads = true)
-        {
-            // Delete the entry:
-			await _database.Run(context, deleteQuery, id);
-			
-			if(deleteUploads){
-			}
-			
-			// Ok!
-			return true;
-        }
-        
-		/// <summary>
-		/// Gets a single feed story by its ID.
-		/// </summary>
-		public async Task<FeedStory> Get(Context context, int id)
-		{
-			return await _database.Select(context, selectQuery, id);
-		}
-		
-		/// <summary>
-		/// Creates a new feed story.
-		/// </summary>
-		public async Task<FeedStory> Create(Context context, FeedStory feedStory)
-		{
-			feedStory = await Events.FeedStoryBeforeCreate.Dispatch(context, feedStory);
-
-			// Note: The Id field is automatically updated by Run here.
-			if (feedStory == null || !await _database.Run(context, createQuery, feedStory)) {
-				return null;
-			}
-
-			feedStory = await Events.FeedStoryAfterCreate.Dispatch(context, feedStory);
-			return feedStory;
-		}
-		
-		/// <summary>
-		/// Updates the given feed story.
-		/// </summary>
-		public async Task<FeedStory> Update(Context context, FeedStory feedStory)
-		{
-			feedStory = await Events.FeedStoryBeforeUpdate.Dispatch(context, feedStory);
-
-			if (feedStory == null || !await _database.Run(context, updateQuery, feedStory, feedStory.Id))
-			{
-				return null;
-			}
-
-			feedStory = await Events.FeedStoryAfterUpdate.Dispatch(context, feedStory);
-			return feedStory;
-		}
-		
     }
     
 }
