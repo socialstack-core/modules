a3b7492f090e45c16fee8d651222bd9bc488b717
Luke Briggs <luke@kulestar.com>
2020-03-31 03:09:21 +0100

    Upgrade to AutoService

--- IStoryAttachmentService.cs
+++ IStoryAttachmentService.cs
@@ -17,7 +17,7 @@ namespace Api.StoryAttachments
 		/// Optionally includes uploaded content refs in there too.
 		/// </summary>
 		/// <returns></returns>
-		Task<bool> Delete(Context context, int id, bool deleteContent = true);
+		Task<bool> Delete(Context context, int id);
 
 		/// <summary>
 		/// Gets a single story attachment by its ID.
