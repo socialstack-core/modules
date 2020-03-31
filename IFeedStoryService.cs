b5b26e49bdfccead318f018a95da6c0959e461c8
Luke Briggs <luke@kulestar.com>
2020-03-31 03:09:07 +0100

    Upgrade to AutoService

--- IFeedStoryService.cs
+++ IFeedStoryService.cs
@@ -17,7 +17,7 @@ namespace Api.FeedStories
 		/// Optionally includes uploaded content refs in there too.
 		/// </summary>
 		/// <returns></returns>
-		Task<bool> Delete(Context context, int id, bool deleteContent = true);
+		Task<bool> Delete(Context context, int id);
 
 		/// <summary>
 		/// Gets a single feed story by its ID.
