7ba1cafba64de38838022fde0573deeff24e03c3
Luke Briggs <luke@kulestar.com>
2020-03-31 02:14:21 +0100

    Upgrade to AutoService

--- ConnectionService.cs
+++ ConnectionService.cs
@@ -18,102 +18,13 @@ namespace Api.Connections
 	/// Handles connections (subscribers).
 	/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 	/// </summary>
-	public partial class ConnectionService : IConnectionService
+	public partial class ConnectionService : AutoService<Connection>, IConnectionService
     {
-        private IDatabaseService _database;
-		
-		private readonly Query<Connection> deleteQuery;
-		private readonly Query<Connection> createQuery;
-		private readonly Query<Connection> selectQuery;
-		private readonly Query<Connection> updateQuery;
-		private readonly Query<Connection> listQuery;
-
-
 		/// <summary>
 		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 		/// </summary>
-		public ConnectionService(IDatabaseService database)
+		public ConnectionService() : base(Events.Connection)
         {
-            _database = database;
-			
-			// Start preparing the queries. Doing this ahead of time leads to excellent performance savings, 
-			// whilst also using a high-level abstraction as another plugin entry point.
-			deleteQuery = Query.Delete<Connection>();
-			createQuery = Query.Insert<Connection>();
-			updateQuery = Query.Update<Connection>();
-			selectQuery = Query.Select<Connection>();
-			listQuery = Query.List<Connection>();
 		}
-
-		/// <summary>
-		/// List a filtered set of connections.
-		/// </summary>
-		/// <returns></returns>
-		public async Task<List<Connection>> List(Context context, Filter<Connection> filter)
-		{
-			filter = await Events.ConnectionBeforeList.Dispatch(context, filter);
-			var list = await _database.List(listQuery, filter);
-			list = await Events.ConnectionAfterList.Dispatch(context, list);
-			return list;
-		}
-
-		/// <summary>
-		/// Deletes a connection by its ID.
-		/// Optionally includes uploaded content refs in there too.
-		/// </summary>
-		/// <returns></returns>
-		public async Task<bool> Delete(Context context, int id, bool deleteUploads = true)
-        {
-            // Delete the entry:
-			await _database.Run(deleteQuery, id);
-			
-			if(deleteUploads){
-			}
-			
-			// Ok!
-			return true;
-        }
-        
-		/// <summary>
-		/// Gets a single connection by its ID.
-		/// </summary>
-		public async Task<Connection> Get(Context context, int id)
-		{
-			return await _database.Select(selectQuery, id);
-		}
-		
-		/// <summary>
-		/// Creates a new connection.
-		/// </summary>
-		public async Task<Connection> Create(Context context, Connection connection)
-		{
-			connection = await Events.ConnectionBeforeCreate.Dispatch(context, connection);
-
-			// Note: The Id field is automatically updated by Run here.
-			if (connection == null || !await _database.Run(createQuery, connection)) {
-				return null;
-			}
-
-			connection = await Events.ConnectionAfterCreate.Dispatch(context, connection);
-			return connection;
-		}
-		
-		/// <summary>
-		/// Updates the given connection.
-		/// </summary>
-		public async Task<Connection> Update(Context context, Connection connection)
-		{
-			connection = await Events.ConnectionBeforeUpdate.Dispatch(context, connection);
-
-			if (connection == null || !await _database.Run(updateQuery, connection))
-			{
-				return null;
-			}
-
-			connection = await Events.ConnectionAfterUpdate.Dispatch(context, connection);
-			return connection;
-		}
-		
     }
-    
 }
--- Events.cs
+++ Events.cs
@@ -11,81 +11,10 @@ namespace Api.Eventing
 	/// </summary>
 	public partial class Events
     {
-
-		#region Service events
-
 		/// <summary>
-		/// Just before a new connection is created. The given connection won't have an ID yet. Return null to cancel the creation.
+		/// Set of events for a Connection.
 		/// </summary>
-		public static EventHandler<Connection> ConnectionBeforeCreate;
-
-		/// <summary>
-		/// Just after an connection has been created. The given connection object will now have an ID.
-		/// </summary>
-		public static EventHandler<Connection> ConnectionAfterCreate;
-
-		/// <summary>
-		/// Just before an connection is being deleted. Return null to cancel the deletion.
-		/// </summary>
-		public static EventHandler<Connection> ConnectionBeforeDelete;
-
-		/// <summary>
-		/// Just after an connection has been deleted.
-		/// </summary>
-		public static EventHandler<Connection> ConnectionAfterDelete;
-
-		/// <summary>
-		/// Just before updating an connection. Optionally make additional changes, or return null to cancel the update.
-		/// </summary>
-		public static EventHandler<Connection> ConnectionBeforeUpdate;
-
-		/// <summary>
-		/// Just after updating an connection.
-		/// </summary>
-		public static EventHandler<Connection> ConnectionAfterUpdate;
-
-		/// <summary>
-		/// Just after an connection was loaded.
-		/// </summary>
-		public static EventHandler<Connection> ConnectionAfterLoad;
-
-		/// <summary>
-		/// Just before a service loads an connection list.
-		/// </summary>
-		public static EventHandler<Filter<Connection>> ConnectionBeforeList;
-
-		/// <summary>
-		/// Just after an connection list was loaded.
-		/// </summary>
-		public static EventHandler<List<Connection>> ConnectionAfterList;
-
-		#endregion
-
-		#region Controller events
-
-		/// <summary>
-		/// Create a new connection.
-		/// </summary>
-		public static EndpointEventHandler<ConnectionAutoForm> ConnectionCreate;
-		/// <summary>
-		/// Delete an connection.
-		/// </summary>
-		public static EndpointEventHandler<Connection> ConnectionDelete;
-		/// <summary>
-		/// Update connection metadata.
-		/// </summary>
-		public static EndpointEventHandler<ConnectionAutoForm> ConnectionUpdate;
-		/// <summary>
-		/// Load connection metadata.
-		/// </summary>
-		public static EndpointEventHandler<Connection> ConnectionLoad;
-		/// <summary>
-		/// List connections.
-		/// </summary>
-		public static EndpointEventHandler<Filter<Connection>> ConnectionList;
-
-		#endregion
-
+		public static EventGroup<Connection> Connection;
 	}
 
 }
