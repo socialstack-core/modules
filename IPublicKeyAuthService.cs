e3730d37d438301ab69960b8e4a39bf76a9044c2
Luke Briggs <luke@kulestar.com>
2020-04-04 18:49:34 +0100

    Initial commit

---
+++ IPublicKeyAuthService.cs
@@ -0,0 +1,16 @@
+﻿using Api.Contexts;
+using Api.Permissions;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Api.PublicKeyAuth
+{
+	/// <summary>
+	/// Public key authentication scheme. The user's device has a private key which it uses to sign a challenge.
+	/// </summary>
+	public partial interface IPublicKeyAuthService
+	{
+        
+	}
+}
