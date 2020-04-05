5a522b12665d3aeacf7514d4a9b2996b56690cd3
Luke Briggs <luke@kulestar.com>
2020-04-05 12:01:03 +0100

    Now exports the method as default

--- EmailAddress.js
+++ EmailAddress.js
@@ -3,7 +3,7 @@ import Text from 'UI/Text';
 /*
 	Email address validation method. Returns translatable React element if it failed alongside a constant error code, or nothing.
 */
-export const emailAddress = (value) => {
+export default value => {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
     if(!re.test(String(value).toLowerCase())){
