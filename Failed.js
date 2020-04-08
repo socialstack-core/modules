b91c87844206f6fedcb1c68c68bb1cc3c6fc4f39
Conan Ablewhite <conan.ablewhite@4-roads.com>
2020-04-08 11:37:11 +0100

    class updated to className

--- Failed.js
+++ Failed.js
@@ -1,8 +1,8 @@
 export default class Failed extends React.Component{
     render(){
         return(
-            <div class="alert alert-danger" role="alert" style = {{textAlign: "center"}}>
-                <i class="fad fa-wifi-slash"></i>
+            <div className="alert alert-danger" role="alert" style = {{textAlign: "center"}}>
+                <i className="fad fa-wifi-slash" />
                 <p>The service is currently unavailable. This may be because your device is currently offline.</p>
             </div>
         );
