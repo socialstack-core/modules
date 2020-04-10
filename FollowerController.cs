032210b6a403b42352c86355fc522b4dc860b83c
RobN <robert.nash@4-roads.com>
2020-04-10 22:37:48 +0100

    Added permissions and protection around followers create so you can only have one instance

--- FollowerController.cs
+++ FollowerController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Api.Contexts;
+using Api.Permissions;
+using Api.Startup;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace Api.Followers
 {
@@ -8,5 +14,23 @@ namespace Api.Followers
     [Route("v1/follower")]
 	public partial class FollowerController : AutoController<Follower>
     {
-	}
+        [HttpPost]
+        public override async Task<object> Create([FromBody] JObject body)
+        {
+            var entity = new Follower();
+
+            var context = Request.GetContext();
+
+            await SetFieldsOnObject(entity, context, body, JsonFieldGroup.Default);
+
+            var follower = await _service.List(context, new Filter<Follower>().Equals("UserId", entity.UserId).And().Equals("SubscribedToId", entity.SubscribedToId));
+
+            if (follower.Any())
+            {
+                return follower.First();
+            }
+
+            return base.Create(body);
+        }
+    }
 }
---
+++ Permissions.cs
@@ -0,0 +1,34 @@
+﻿using Api.Startup;
+using Api.Eventing;
+using Api.Contexts;
+using Api.Permissions;
+using System.Threading.Tasks;
+
+namespace Api.Followers
+{
+	/// <summary>
+	/// Instances capabilities during the very earliest phases of startup.
+	/// </summary>
+	[EventListener]
+	public class Permissions
+	{
+		/// <summary>
+		/// Instanced automatically.
+		/// </summary>
+		public Permissions()
+		{
+			// Hook the default role setup. It's done like this so it can be removed by a plugin if wanted.
+			Events.CapabilityOnSetup.AddEventListener((Context context, object source) =>
+			{
+				// Public - the role used by anonymous users.
+                Roles.Member.Revoke("follower_delete", "follower_list")
+                    .Grant("follower_create","follower_delete", "follower_list");
+
+                Roles.Public.Revoke("follower_load");
+				Roles.Public.Revoke("follower_list");
+
+                return Task.FromResult(source);
+			}, 20);
+		}
+	}
+}
