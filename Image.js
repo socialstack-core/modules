48affef3928f498aa646b7020b33668d81db423a
Luke Briggs <luke@kulestar.com>
2020-05-04 00:14:51 +0100

    Added support for linked images and title prop

--- Image.js
+++ Image.js
@@ -8,12 +8,19 @@ Min required props: size, fileRef. Size must be an available size from your uplo
 */
 export default class Image extends React.Component {
 	render(){
-		return <div className="image" onClick={this.props.onClick}>{getRef(this.props.fileRef, {...(omit(this.props, ['fileRef', 'onClick']))})}</div>;
+		const {onClick, fileRef, linkUrl, size} = this.props;
+		
+		var attribs = omit(this.props, ['fileRef', 'onClick', 'linkUrl', 'size']);
+		attribs.alt = attribs.alt || attribs.title;
+		var img = <div className="image" onClick={this.props.onClick}>{getRef(this.props.fileRef, {attribs, size})}</div>;
+		return linkUrl ? <a alt={attribs.alt} title={attribs.title} href={linkUrl}>{img}</a> : img;
 	}
 }
 
 Image.propTypes = {
 	fileRef: 'string',
+	linkUrl: 'string',
+	title: 'string',
 	size: ['original', '1024', '512', '256', '200', '128', '100', '64', '32'] // todo: pull from api
 };
 
