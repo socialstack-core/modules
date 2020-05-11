d56b363804d95b5d22cacf2d2aa7469257e71d11
Luke Briggs <luke@kulestar.com>
2020-05-11 14:10:34 +0100

    Revisions are now restricted by default

---
+++ Permissions.cs
@@ -0,0 +1,31 @@
+using Api.Startup;
+using Api.Eventing;
+using Api.Contexts;
+using Api.Permissions;
+using System.Threading.Tasks;
+
+namespace Api.Revisions
+{
+	/// <summary>
+	/// Instances capabilities during the very earliest phases of startup.
+	/// </summary>
+	[EventListener]
+	public class Permissions
+	{
+		/// <summary>
+		/// Instanced automatically.
+		/// </summary>
+		public Permissions()
+		{
+			Events.CapabilityOnSetup.AddEventListener((Context context, object source) =>
+			{
+				// Block all revision EPs:
+				Roles.Member.RevokeIfEndsWith("revision_list", "revision_create", "revision_load", "revision_delete");
+				Roles.Guest.RevokeIfEndsWith("revision_list", "revision_create", "revision_load", "revision_delete");
+				Roles.Public.RevokeIfEndsWith("revision_list", "revision_create", "revision_load", "revision_delete");
+				
+				return Task.FromResult(source);
+			});
+		}
+	}
+}
