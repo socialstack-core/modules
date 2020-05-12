cb5e7120d55b2fb2529c68ab082c4ddcd4f4b665
Luke Briggs <luke@kulestar.com>
2020-05-12 03:41:28 +0100

    Row can now be added via the editor

--- Row.js
+++ Row.js
@@ -1,5 +1,26 @@
+import omit from 'UI/Functions/Omit';
+
 /**
  * A responsive row.
  */
 
-export default (props) => <div className={"row " + (props.noGutters ? "no-gutters" : "") + " " + (props.className || '')} >{props.children}</div>
+export default class Row extends React.Component {
+	
+	render(){
+		var props=this.props;
+		return <div 
+			className={"row " + (props.noGutters ? "no-gutters" : "") + " " + (props.className || '')}
+			{...(omit(this.props, ['className', 'noGutters', 'children', '__canvas']))}
+		>
+			{props.children}
+		</div>;
+	}
+
+}
+
+Row.propTypes={
+	noGutters: 'boolean',
+	children: true
+};
+
+Row.icon = 'columns';
