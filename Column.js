2f467ff11561d31eddcc6ca53aa8488b42411f22
Luke Briggs <luke@kulestar.com>
2020-05-12 03:41:44 +0100

    Column can now be added via the editor

--- Column.js
+++ Column.js
@@ -1,5 +1,27 @@
+import omit from 'UI/Functions/Omit';
+
 /**
  * A 12 segment responsive column. Usually used within a <Row>.
  */
 
-export default (props) => <div className={"col-md-" + (props.size || 6) + " " + (props.noGutters ? "no-gutters" : "")}>{props.children}</div>
+export default class Column extends React.Component {
+	
+	render(){
+		var props=this.props;
+		return <div
+			className={"col-md-" + (props.size || 6) + " " + (props.noGutters ? "no-gutters" : "")}
+			{...(omit(this.props, ['className', 'noGutters', 'children', '__canvas']))}
+		>
+			{props.children}
+		</div>;
+	}
+	
+}
+
+Column.propTypes={
+	noGutters: 'boolean',
+	size: [1,2,3,4,5,6,7,8,9,10,11,12],
+	children: true
+};
+
+Column.icon = 'columns';
