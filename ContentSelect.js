61bdc01d39c3681e44124a2cd1ff52a980f517c3
Luke Briggs <luke@kulestar.com>
2020-06-05 21:53:09 +0100

    Initial commit

---
+++ ContentSelect.js
@@ -0,0 +1,45 @@
+import webRequest from 'UI/Functions/WebRequest';
+import Input from 'UI/Input';
+
+/**
+ * Dropdown to select a piece of content.
+ */
+export default class ContentSelect extends React.Component {
+	
+	constructor(props){
+		super(props);
+		this.state = {};
+		this.load(props);
+	}
+	
+	componentWillReceiveProps(props){
+		this.load(props);
+	}
+	
+	load(props){
+		console.log(props);
+		webRequest(props.contentType.toLowerCase() + '/list').then(response => {
+			var all = response.json.results;
+			all.unshift(null);
+			this.setState({all});
+		});
+	}
+	
+	render(){
+		var all = this.state.all;
+		
+		return (<Input {...this.props} type="select">{
+			all ? all.map(content => {
+				if(!content){
+					return <option value={'0'}>None</option>;
+				}
+				return <option value={content.id}>{
+					content.title || content.firstName || content.username || content.name || content.url || content.id
+				}</option>;
+			}) : [
+			]
+		}</Input>);
+		
+	}
+	
+}
