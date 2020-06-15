3fbefb504313908e43e98d68fc723b4717cee1cf
Luke Briggs <luke@kulestar.com>
2020-06-15 12:56:16 +0100

    Added some extra config options

--- Banner.js
+++ Banner.js
@@ -2,17 +2,43 @@ import getRef from 'UI/Functions/GetRef';
 
 export default class Banner extends React.Component{
     render(){
-		var col = this.props.fontColor || 'white';
+		var {fontColor, backgroundRef, backgroundColor, title, description, height, textWidth} = this.props;
+		
+		var style = {};
+		
+		if(backgroundRef && backgroundRef.length){
+			style.backgroundImage = "url("+getRef(backgroundRef, {url: true, size: 'original'})+")";
+		}
+		
+		if(backgroundColor && backgroundColor.length){
+			style.backgroundColor = backgroundColor;
+		}
+		
+		if(height){
+			style.height = height;
+		}
+		
+		if(!textWidth){
+			textWidth = 6;
+		}
+		
+		var halfWidth = (12-textWidth)/2;
+		
+		var textStyle = {};
+		if(fontColor && fontColor.length){
+			textStyle.color = fontColor;
+		}
+		
         return(
-			<div className="hero-banner" style ={{backgroundImage: "url("+getRef(this.props.backgroundRef, {url: true, size: 'original'})+")"}}>
+			<div className="hero-banner" style ={style}>
 				<div className="container h-100">
 					<div className="row h-100 align-items-center">
-						<div className="col-3"></div>
-						<div className="col-6 banner-text">
-							<h1 className="title" style={{color: col}} dangerouslySetInnerHTML={{__html: this.props.title}} />
+						<div className={"col-" + halfWidth}></div>
+						<div className={"col-" + textWidth + " banner-text"}>
+							<h1 className="title" style={textStyle} dangerouslySetInnerHTML={{__html: title}} />
 							<br/>
 							<br/>
-							<p style={{color: col}} dangerouslySetInnerHTML={{__html: this.props.description}}/>
+							<p style={textStyle} dangerouslySetInnerHTML={{__html: description}}/>
 						</div>
 					</div>
 				</div>
@@ -24,8 +50,11 @@ export default class Banner extends React.Component{
 Banner.propTypes = {
 	fontColor: 'color',
 	backgroundRef: 'image',
+	backgroundColor: 'color',
 	title: 'string',
-	description: 'string'
+	description: 'string',
+	height: 'string',
+	textWidth: [2,4,6,8,10,12]
 };
 
 Banner.icon='bullhorn';
