5b83b3219bd5779b1f37488e9b7545540ea26358
Luke Briggs <luke@kulestar.com>
2020-06-23 21:38:38 +0100

    Added stub service to identify revision support

---
+++ RevisionService.cs
@@ -0,0 +1,24 @@
+﻿using Api.Database;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Api.Permissions;
+using Api.Contexts;
+using Api.Eventing;
+
+namespace Api.Revisions
+{
+	/// <summary>
+	/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
+	/// </summary>
+	public partial class RevisionService : IRevisionService
+    {
+		/// <summary>
+		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
+		/// </summary>
+		public RevisionService()
+        {
+			// Exists as a convenient way to check if revisions are supported.
+		}
+	}
+    
+}
