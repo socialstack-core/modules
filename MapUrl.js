7db51bfd24d78aaa9bc8ee8773464dbea73a0f01
Luke Briggs <luke@kulestar.com>
2020-07-13 14:07:14 +0100

    Removal of startsWith usage for IE11

--- MapUrl.js
+++ MapUrl.js
@@ -1,7 +1,7 @@
 import apiEndpoint from 'UI/Functions/ApiEndpoint';
 
 export default function mapUrl(url) {
-	if(url && !url.startsWith("http:") && !url.startsWith("https:") && !url.startsWith("//")){
+	if(url && url.indexOf("http:") != 0 && url.indexOf("https:") != 0 && url.indexOf("//") != 0){
 		// Site relative URL. Api URL?
 		if(url[0] != '/'){
 			// All site page URLs should be root relative except API requests which are not.
