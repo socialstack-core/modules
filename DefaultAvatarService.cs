806e3a5d4e906e0685744a6989118dc080bdb53c
Luke Briggs <luke@kulestar.com>
2020-07-31 22:07:26 +0100

    Initial commit

---
+++ DefaultAvatarService.cs
@@ -0,0 +1,63 @@
+﻿using Api.Database;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Api.Permissions;
+using Api.Contexts;
+using Api.Eventing;
+using Microsoft.Extensions.Configuration;
+using Api.Configuration;
+using Microsoft.Extensions.Hosting;
+using Api.Users;
+using System;
+
+namespace Api.DefaultAvatar
+{
+	/// <summary>
+	/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
+	/// </summary>
+	public partial class DefaultAvatarService : IDefaultAvatarService
+    {
+		private DefaultAvatarConfig _configuration; 
+		
+		/// <summary>
+		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
+		/// </summary>
+		public DefaultAvatarService()
+        {
+			var section = AppSettings.GetSection("DefaultAvatarImages");
+
+			if (section == null)
+			{
+				return;
+			}
+
+			_configuration = section.Get<DefaultAvatarConfig>();
+
+			if (_configuration == null)
+			{
+				return;
+			}
+
+			var images = _configuration.Images;
+			
+			if(images == null || images.Length == 0)
+			{
+				return;
+			}
+
+			var rand = new Random();
+
+			Events.User.BeforeCreate.AddEventListener((Context ctx, User user) => {
+
+				if (string.IsNullOrEmpty(user.AvatarRef))
+				{
+					user.AvatarRef = images[rand.Next(0, images.Length)];
+				}
+
+				return Task.FromResult(user);
+			}, 6);
+			
+		}
+	}
+    
+}
---
+++ IDefaultAvatarService.cs
@@ -0,0 +1,17 @@
+﻿using Api.Contexts;
+using Api.Permissions;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Api.DefaultAvatar
+{
+	/// <summary>
+	/// Handles default avatar.
+	/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
+	/// </summary>
+	public partial interface IDefaultAvatarService
+    {
+
+	}
+}
