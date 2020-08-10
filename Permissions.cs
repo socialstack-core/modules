b8c3c7a98c6b84e3fa64e523d769c94b5a4c8d26
Luke Briggs <luke@kulestar.com>
2020-08-10 20:18:14 +0100

    Added initial round of default permissions

--- Permissions.cs
+++ Permissions.cs
@@ -20,40 +20,22 @@ namespace Api.PrivateChats
 			// Hook the default role setup. It's done like this so it can be removed by a plugin if wanted.
 			Events.CapabilityOnSetup.AddEventListener((Context context, object source) =>
 			{
-				/*
-				Example permission rules.
-				
-				Member role: A verified user account. Not an admin.
-				Guest role: A user account. The transition from guest to member is up to you.
-				Public role: Not logged in at all.
-				
-				// Allow public creation (as it's disabled by default):
-				Roles.Member.Grant("privateChat_create");
-				Roles.Public.Grant("privateChat_create");
-				Roles.Guest.Grant("privateChat_create");
+				// Allow member creation (as it's disabled by default):
+				Roles.Member.Grant("privatechat_create");
+				Roles.Member.Grant("privatechatmessage_create");
 				
 				// Remove public viewing (as it's enabled by default):
 				Roles.Guest.Revoke("privateChat_load", "privateChat_list");
 				Roles.Public.Revoke("privateChat_load", "privateChat_list");
-				Roles.Member.Revoke("privateChat_load", "privateChat_list");
-				*/
-				/*
-				Example permission rules.
-				
-				Member role: A verified user account. Not an admin.
-				Guest role: A user account. The transition from guest to member is up to you.
-				Public role: Not logged in at all.
-				
-				// Allow public creation (as it's disabled by default):
-				Roles.Member.Grant("privateChatMessage_create");
-				Roles.Public.Grant("privateChatMessage_create");
-				Roles.Guest.Grant("privateChatMessage_create");
+				// Roles.Member.Revoke("privateChat_load", "privateChat_list");
 				
 				// Remove public viewing (as it's enabled by default):
 				Roles.Guest.Revoke("privateChatMessage_load", "privateChatMessage_list");
 				Roles.Public.Revoke("privateChatMessage_load", "privateChatMessage_list");
-				Roles.Member.Revoke("privateChatMessage_load", "privateChatMessage_list");
-				*/
+				// Roles.Member.Revoke("privateChatMessage_load", "privateChatMessage_list");
+				
+				// Grant member load/ list if they're able to view the chat.
+				// (TODO)
 				
 				return Task.FromResult(source);
 			}, 20);
