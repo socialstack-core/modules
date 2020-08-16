9278593e13ece352712e9783faac386b2de7e596
Conan Ablewhite <conan.ablewhite@4-roads.com>
2020-08-16 04:24:23 +0100

    logo added to main menu; active menu item now highlighted / padding fixed

--- MainMenu.js
+++ MainMenu.js
@@ -1,14 +1,20 @@
 import NavMenu from 'UI/NavMenu';
 import Canvas from 'UI/Canvas';
 import getRef from 'UI/Functions/GetRef';
+import logo from './logo.svg';
 
 export default class MainMenu extends React.Component {
 	render() {
 		return (
 			<div className="main-menu">
+				<img className="logo" src={logo} alt="Logo" />
 				<NavMenu id={'admin_primary'} asUl>
-					{item => <a href={item.target}>
-							{getRef(item.iconRef)}
+					{item =>
+						<a href={item.target} className={
+							item.target == '/en-admin/' ?
+								(window.location.pathname == item.target ? 'active' : '') :
+								(window.location.pathname.startsWith(item.target) ? 'active' : '')}>
+							{getRef(item.iconRef, { className: 'fa-fw' })}
 							<Canvas>{item.bodyJson}</Canvas>
 						</a>
 					}
