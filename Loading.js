47360e04a59830dc579361680a3b20ff740ff106
Conan Ablewhite <conan.ablewhite@4-roads.com>
2020-08-17 13:09:02 +0100

    recent SVG enhancements to Loading backed out (in favour of applying at project level)

--- Loading.js
+++ Loading.js
@@ -1,31 +1,18 @@
-import Html from 'UI/Html';
 
 /**
  * Standalone component which displays a loader (typically a spinner).
  */
 export default class Loading extends React.Component {
-	
+
     render() {
         let message = this.props.message ? this.props.message : "Loading ... ";
 
         return (
             <div className="alert alert-info loading">
                 {message}
-
-                {this.props.svg && 
-                    <div className="fa-spin">
-                        <Html>
-                            {this.props.svg}
-                        </Html>
-                    </div>
-                }
-
-                {!this.props.svg &&
-                    <i className="fas fa-spinner fa-spin" />
-                }
-
+                <i className="fas fa-spinner fa-spin" />
             </div>
         );
-	}
-	
+    }
+
 }
