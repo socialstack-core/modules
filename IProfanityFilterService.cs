5df58154355523be93b47bc3232eff6b7683389d
Luke Briggs <luke@kulestar.com>
2020-08-18 02:07:50 +0100

    Initial commit

---
+++ IProfanityFilterService.cs
@@ -0,0 +1,22 @@
+﻿using Api.Contexts;
+using Api.Permissions;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Api.ProfanityFilter
+{
+	/// <summary>
+	/// Handles profanityFilter.
+	/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
+	/// </summary>
+	public partial interface IProfanityFilterService
+    {
+
+		/// <summary>
+		/// Returns the number of profanity filter hits the given text has.
+		/// </summary>
+		int Measure(string text);
+
+	}
+}
