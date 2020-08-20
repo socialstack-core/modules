be945e9d46cec259c726c24c16cd10d8edb3b08b
Luke Briggs <luke@kulestar.com>
2020-08-20 23:24:22 +0100

    Added title to quiz

--- PubQuiz.cs
+++ PubQuiz.cs
@@ -12,33 +12,12 @@ namespace Api.PubQuizzes
 	/// </summary>
 	public partial class PubQuiz : RevisionRow
 	{
-		// Example fields. None are required:
-		/*
         /// <summary>
         /// The name of the PubQuiz
         /// </summary>
         [DatabaseField(Length = 200)]
 		[Localized]
-		public string Name;
-
-		/// <summary>
-		/// A page this PubQuiz appears on.
-		/// </summary>
-		public int PageId;
-		
-		/// <summary>
-		/// The content of this PubQuiz.
-		/// </summary>
-		[Localized]
-		public string BodyJson;
-
-		/// <summary>
-		/// The feature image ref
-		/// </summary>
-		[DatabaseField(Length = 80)]
-		public string FeatureRef;
-		*/
-		
+		public string Title;
 	}
 
 }
