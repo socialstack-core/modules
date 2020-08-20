dd4a6a4fc358f857f0e0547e6dbd9b365313fe9b
Luke Briggs <luke@kulestar.com>
2020-08-20 23:41:11 +0100

    Now sets quiz ID on answers automatically

--- PubQuizQuestionService.cs
+++ PubQuizQuestionService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Api.Permissions;
 using Api.Contexts;
 using Api.Eventing;
+using Api.Startup;
 
 namespace Api.PubQuizzes
 {
@@ -17,8 +18,62 @@ namespace Api.PubQuizzes
 		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 		/// </summary>
 		public PubQuizQuestionService() : base(Events.PubQuizQuestion)
-        {
+		{
 			InstallAdminPages(null, null, new string[] { "id", "questionJson" });
+
+			Events.PubQuizAnswer.BeforeCreate.AddEventListener(async (Context context, PubQuizAnswer answer) => {
+
+				if (answer.PubQuizQuestionId == 0)
+				{
+					// Question required.
+					return null;
+				}
+
+				var question = await Get(context, answer.PubQuizQuestionId);
+
+				if (question == null)
+				{
+					return null;
+				}
+
+				answer.PubQuizId = question.PubQuizId;
+
+				return answer;
+			});
+
+			Events.PubQuizAnswer.BeforeSettable.AddEventListener((Context context, JsonField<PubQuizAnswer> field) =>
+			{
+				if (field == null)
+				{
+					return Task.FromResult(field);
+				}
+
+				if (field.Name == "PubQuizId")
+				{
+					// This field isn't settable (it appears as a textarea in the admin panel otherwise).
+					field = null;
+				}
+
+				return Task.FromResult(field);
+			}
+			);
+
+			Events.PubQuizQuestion.BeforeSettable.AddEventListener((Context context, JsonField<PubQuizQuestion> field) =>
+				{
+					if (field == null)
+					{
+						return Task.FromResult(field);
+					}
+
+					if (field.Name == "Answers")
+					{
+						// This field isn't settable (it appears as a textarea in the admin panel otherwise).
+						field = null;
+					}
+
+					return Task.FromResult(field);
+				}
+			);
 		}
 	}
     
