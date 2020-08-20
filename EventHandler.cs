16e97d31829dce921152dea43489aa0ef53477e3
Luke Briggs <luke@kulestar.com>
2020-08-20 23:16:56 +0100

    Initial commit

---
+++ EventHandler.cs
@@ -0,0 +1,70 @@
+using Api.Database;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Api.Permissions;
+using Api.Contexts;
+using Api.Eventing;
+using System.Linq;
+using Api.Users;
+using Api.Startup;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Api.Huddles
+{
+	/// <summary>
+	/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
+	/// </summary>
+	[EventListener]
+	public partial class HuddleActivityEventHandler
+    {
+		/// <summary>
+		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
+		/// </summary>
+		public HuddleActivityEventHandler()
+        {
+			Events.Huddle.AfterLoad.AddEventListener(async (Context context, Huddle huddle) =>
+			{
+				if (huddle == null)
+				{
+					// Due to the way how event chains work, the primary object can be null.
+					// Safely ignore this.
+					return null;
+				}
+
+				if (huddle.ActivityContentId != 0)
+				{
+					huddle.Activity = await Content.Get(context, huddle.ActivityContentTypeId, huddle.ActivityContentId);
+				}
+				
+				return huddle;
+			});
+			
+			Events.Huddle.AfterList.AddEventListener(async (Context context, List<Huddle> huddles) =>
+			{
+				if (huddles == null)
+				{
+					return null;
+				}
+
+				await Content.ApplyMixed(
+					context,
+					huddles,
+					src => {
+						var huddle = src as Huddle;
+						return new ContentTypeAndId(huddle.ActivityContentTypeId, huddle.ActivityContentId);
+					},
+					(object src, object content) => {
+						var huddle = src as Huddle;
+						huddle.Activity = content;
+					}
+				);
+				
+				return huddles;
+			});
+
+		}
+
+	}
+    
+}
---
+++ Huddle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Api.Database;
+using Api.Translate;
+using Api.Users;
+using Api.WebSockets;
+
+namespace Api.Huddles
+{
+
+	/// <summary>
+	/// A Huddle
+	/// </summary>
+	public partial class Huddle : IAmLive
+	{
+		/// <summary>
+		/// If there's an active activity, the time it started at.
+		/// </summary>
+		public DateTime? ActivityStartUtc;
+		
+		/// <summary>
+		/// Activity content type ID, e.g. "Quiz".
+		/// </summary>
+		public int ActivityContentTypeId;
+		
+		/// <summary>
+		/// E.g. quiz ID.
+		/// </summary>
+		public int ActivityContentId;
+		
+		/// <summary>
+		/// The current activity, if there is one.
+		/// </summary>
+		public object Activity {get; set;}
+	}
+}
