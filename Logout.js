db99a19fae2bf8a77be34f572f18da00a1e6d2bc
Luke Briggs <luke@kulestar.com>
2020-08-26 14:14:58 +0100

    Initial commit

---
+++ Logout.js
@@ -0,0 +1,22 @@
+import webRequest from 'UI/Functions/WebRequest';
+
+function clearAndNav(url){
+	var state = {};
+	
+	for(var k in global.app.state){
+		if(k == 'url' || k == 'loadingUser'){
+			continue;
+		}
+		state[k] = null;
+	}
+	
+	state.role={id: 0};
+	global.app.setState(state);
+	global.pageRouter.go(url);
+}
+
+module.exports = (url) => {
+	return webRequest('user/logout')
+		.then(() => clearAndNav(url || '/'))
+		.catch(e => clearAndNav(url || '/'));
+};
