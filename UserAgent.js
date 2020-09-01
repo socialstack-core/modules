14f70de2ba95e195e3f8c30b49dd56e1a0e4240b
Conan Ablewhite <conan.ablewhite@4-roads.com>
2020-09-01 23:58:02 +0100

    IE10+ / desktop detection added

--- UserAgent.js
+++ UserAgent.js
@@ -19,6 +19,7 @@ if (global.document && global.document.getElementsByTagName("html").length) {
         //window.navigator.vendor === "Google Inc." &&
         !isOpera &&
         !isEdge;
+    var isIE10Plus = window.matchMedia("(-ms-high-contrast: active)").matches || window.matchMedia("(-ms-high-contrast: none)").matches;
 
     var htmlClassList = global.document.getElementsByTagName("html")[0].classList;
 
@@ -43,6 +44,9 @@ if (global.document && global.document.getElementsByTagName("html").length) {
     if (isChrome) {
         htmlClassList.add("chrome");
     }
+    if (isIE10Plus) {
+        htmlClassList.add("ie10-plus");
+    }
 
     // platform checks
     var platform = navigator.platform.toLowerCase();
@@ -56,53 +60,69 @@ if (global.document && global.document.getElementsByTagName("html").length) {
     }
 
     // mobile checks (appproximate!)
+    var isMobile = false;
 
     // small mobile (>= 320px)
     if (window.matchMedia('(max-width: 320px) and (pointer: coarse)').matches) {
         htmlClassList.add("device-mobile", "device-mobile-small");
+        isMobile = true;
     }
 
     // medium mobile (321px - 411px)
     if (window.matchMedia('(min-width: 321px) and (max-width: 411px) and (pointer: coarse)').matches) {
         htmlClassList.add("device-mobile", "device-mobile-medium");
+        isMobile = true;
     }
 
     // large mobile (412px - 767px)
     if (window.matchMedia('(min-width: 412px) and (max-width: 767px) and (pointer: coarse)').matches) {
         htmlClassList.add("device-mobile", "device-mobile-large");
+        isMobile = true;
     }
 
 
     // iPad checks
+    var isiPad = false;
 
     // iPad Mini
     if (window.matchMedia('(device-width: 768px) and (device-height: 1024px) and (orientation: portrait)').matches ||
         window.matchMedia('(device-width: 1024px) and (device-height: 768px) and (orientation: landscape)').matches) {
         htmlClassList.add("device-ipad", "device-ipad-mini");
+        isiPad = true;
     }
 
     // iPad Pro 10.2"
     if (window.matchMedia('(device-width: 810px) and (device-height: 1080px) and (orientation: portrait)').matches ||
         window.matchMedia('(device-width: 1080px) and (device-height: 810px) and (orientation: landscape)').matches) {
         htmlClassList.add("device-ipad", "device-ipad-pro", "device-ipad-pro-10-2");
+        isiPad = true;
     }
 
     // iPad Pro 10.5"
     if (window.matchMedia('(device-width: 834px) and (device-height: 1112px) and (orientation: portrait)').matches ||
         window.matchMedia('(device-width: 1112px) and (device-height: 834px) and (orientation: landscape)').matches) {
         htmlClassList.add("device-ipad", "device-ipad-pro", "device-ipad-pro-10-5");
+        isiPad = true;
     }
 
     // iPad Pro 11"
     if (window.matchMedia('(device-width: 834px) and (device-height: 1194px) and (orientation: portrait)').matches ||
         window.matchMedia('(device-width: 1194px) and (device-height: 834px) and (orientation: landscape)').matches) {
         htmlClassList.add("device-ipad", "device-ipad-pro", "device-ipad-pro-11");
+        isiPad = true;
     }
 
     // iPad Pro 12.9"
     if (window.matchMedia('(device-width: 1024px) and (device-height: 1366px) and (orientation: portrait)').matches ||
         window.matchMedia('(device-width: 1366px) and (device-height: 1024px) and (orientation: landscape)').matches) {
         htmlClassList.add("device-ipad", "device-ipad-pro", "device-ipad-pro-12-9");
+        isiPad = true;
+    }
+
+    // desktop (approximate!)
+    // NB: fallback for IE10+ which doesn't support pointer media queries
+    if (!isMobile && !isiPad && (window.matchMedia('(pointer: fine)').matches || isIE10Plus)) {
+        htmlClassList.add("device-desktop");
     }
 
 }
