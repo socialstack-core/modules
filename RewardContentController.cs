65a8fe973be69bda61cdc5e282303794f03a31d8
Michael Rogers <michaelr@conceptsoftworks.com>
2020-09-02 08:48:16 -0700

    Added RewardContentController.cs

---
+++ RewardContentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Rewards
+{
+    /// <summary>
+    /// Handles reward content endpoints.
+    /// </summary>
+    [Route("v1/rewardcontent")]
+	public partial class RewardContentController : AutoController<RewardContent>
+    {
+    }
+}
