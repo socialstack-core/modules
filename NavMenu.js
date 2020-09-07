0da338c927302e042e1f2f9a1d4d57819f9a8dca
Conan Ablewhite <conan.ablewhite@4-roads.com>
2020-09-07 12:23:25 +0100

    default sort order added to NavMenu

--- NavMenu.js
+++ NavMenu.js
@@ -6,11 +6,30 @@ import isNumeric from 'UI/Functions/IsNumeric';
  * A nav menu. This is a very thin wrapper over Loop so it essentially does everything that Loop can (i.e. <NavMenu inline .. etc).
  */
 
-export default (props) =>
-	<Loop over={'navmenuitem/' + (isNumeric(props.id) ? '' : 'key/') + props.id} {...props}>
-	{(props.children && props.children.length) ? props.children : item => 
-		<a href={item.target}>
-			<Canvas>{item.bodyJson}</Canvas>
-		</a>
-	}
-	</Loop>
+export default (props) => {
+    var filter = props.filter || {};
+
+    if (!filter.where) {
+        filter.where = {};
+    }
+
+    if (isNumeric(props.id)) {
+        filter.where.NavMenuId = props.id;
+    } else {
+        filter.where.MenuKey = props.id;
+    }
+
+    if (!filter.sort) {
+        filter.sort = {
+            field: 'Order'
+        };
+    }
+
+    return <Loop over={'navmenuitem/list'} {...props} filter={filter}>
+        {(props.children && props.children.length) ? props.children : item =>
+            <a href={item.target}>
+                <Canvas>{item.bodyJson}</Canvas>
+            </a>
+        }
+    </Loop>
+}
