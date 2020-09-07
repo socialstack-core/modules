2b5dc22d25519c197571352e47004686218fc50a
Conan Ablewhite <conan.ablewhite@4-roads.com>
2020-09-08 00:58:09 +0100

    Object.entries() polyfill added

--- BackwardsCompatibility.js
+++ BackwardsCompatibility.js
@@ -30,6 +30,22 @@ if (!String.prototype.padStart) {
     };
 }
 
+/**
+ * Object.entries() polyfill
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries#Polyfill
+ */
+if (!Object.entries) {
+    Object.entries = function (obj) {
+        var ownProps = Object.keys(obj),
+            i = ownProps.length,
+            resArray = new Array(i); // preallocate the Array
+        while (i--)
+            resArray[i] = [ownProps[i], obj[ownProps[i]]];
+
+        return resArray;
+    };
+}
+
 // check CSS selector
 function supportsSelector(selector) {
     try {
