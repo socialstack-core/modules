20bfaf1c30c14adb521747efbadf10f149d12e35
Luke Briggs <luke@kulestar.com>
2020-09-21 02:30:34 +0100

    Generalised the tagContent flow

--- TagContent.cs
+++ TagContent.cs
@@ -5,33 +5,28 @@ using Api.Translate;
 
 namespace Api.Tags
 {
-	
+
 	/// <summary>
 	/// Content tagged with a particular tag.
 	/// </summary>
-	public partial class TagContent : DatabaseRow
+	public partial class TagContent : MappingRow
 	{
 		/// <summary>
 		/// The ID of the tag.
 		/// </summary>
 		public int TagId;
-		/// <summary>
-		/// The type ID of the tagged content. See also: Api.Database.ContentTypes
-		/// </summary>
-		public int ContentTypeId;
-		/// <summary>
-		/// The ID of the tagged content.
-		/// </summary>
-		public int ContentId;
-		/// <summary>
-		/// The UTC creation date. Read/ delete only rows so an edited date isn't present here.
-		/// </summary>
-		public DateTime CreatedUtc;
 
 		/// <summary>
-		/// The ID of a particular revision that these tags are on. Zero if it's on the live content.
+		/// ID of the tag.
 		/// </summary>
-		public int RevisionId;
+		public override int TargetContentId
+		{
+			get{
+				return TagId;
+			}
+			set {
+				TagId = value;
+			}
+		}
 	}
-	
 }
