1d576d6f00123b6717fc6564e204154bd80cd309
Luke Briggs <luke@kulestar.com>
2020-09-22 17:51:45 +0100

    Upgraded to the new MappingRow type, greatly simplifying the module

--- RewardContent.cs
+++ RewardContent.cs
@@ -10,24 +10,25 @@ namespace Api.Rewards
 	/// <summary>
 	/// Content tagged with a particular reward.
 	/// </summary>
-	public partial class RewardContent : DatabaseRow, IAmLive
+	public partial class RewardContent : MappingRow, IAmLive
 	{
 		/// <summary>
 		/// The ID of the reward.
 		/// </summary>
 		public int RewardId;
+		
 		/// <summary>
-		/// The type ID of the rewardd content. See also: Api.Database.ContentTypes
+		/// ID of the reward.
 		/// </summary>
-		public int ContentTypeId;
-		/// <summary>
-		/// The ID of the rewardd content.
-		/// </summary>
-		public int ContentId;
-		/// <summary>
-		/// The UTC creation date. Read/ delete only rows so an edited date isn't present here.
-		/// </summary>
-		public DateTime CreatedUtc;
+		public override int TargetContentId
+		{
+			get{
+				return RewardId;
+			}
+			set {
+				RewardId = value;
+			}
+		}
 	}
 	
 }
