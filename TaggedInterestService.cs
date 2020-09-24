cde9f66b572e4674ec022c2428154cd12cc9210e
Luke Briggs <luke@kulestar.com>
2020-09-24 23:57:20 +0100

    Initial commit

---
+++ TaggedInterestService.cs
@@ -0,0 +1,45 @@
+using Api.Database;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Api.Permissions;
+using Api.Contexts;
+using Api.Eventing;
+using Api.Tags;
+using Api.Users;
+using Api.Startup;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+
+namespace Api.TaggedInterests
+{
+	/// <summary>
+	/// Handles interests.
+	/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
+	/// </summary>
+	public partial class TaggedInterestService
+    {	
+		/// <summary>
+		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
+		/// </summary>
+		public TaggedInterestService(IDatabaseService _database)
+        {
+			// Interests which are simply thinly renamed Tags.
+
+			// Define the IHaveInterests handler:
+			var mapper = new IHaveArrayHandler<IHaveInterests, Tag, TagContent>()
+			{
+				WhereFieldName = "Interests",
+				MapperFieldName = "TagId",
+				OnSetResult = (IHaveInterests content, List<Tag> results) =>
+				{
+					content.Interests = results;
+				},
+				Database = _database
+			};
+
+			mapper.Map();
+		}
+	}
+    
+}
