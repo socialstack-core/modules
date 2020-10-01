ab259ba752ae4bf25cd1597e7171d86695780de7
RobN <robert.nash@4-roads.com>
2020-10-01 13:40:20 +0100

    removed interfaces

--- ArticleService.cs
+++ ArticleService.cs
@@ -11,7 +11,7 @@ namespace Api.Articles
 	/// Handles articles - containers for individual article posts.
 	/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 	/// </summary>
-	public partial class ArticleService : AutoService<Article>, IArticleService
+	public partial class ArticleService : AutoService<Article>
     {
 		/// <summary>
 		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
