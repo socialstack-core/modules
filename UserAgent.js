dc2f32e06c7719a087034768dcd860cad88caef8
Conan Ablewhite <conan.ablewhite@4-roads.com>
2020-10-01 21:08:27 +0100

    UserAgent utility methods added

--- UserAgent.js
+++ UserAgent.js
@@ -1,3 +1,122 @@
+function getMobileInfo() {
+    var mobileInfo = {
+        isMobile: false,
+        small: false,
+        medium: false,
+        large: false
+    };
+
+    // small mobile (>= 320px)
+    if (window.matchMedia('(max-width: 320px) and (pointer: coarse) and (orientation: portrait)').matches ||
+        window.matchMedia('(max-height: 320px) and (pointer: coarse) and (orientation: landscape)').matches) {
+        mobileInfo.isMobile = true;
+        mobileInfo.small = true;
+    }
+
+    // medium mobile (321px - 411px)
+    if (window.matchMedia('(min-width: 321px) and (max-width: 411px) and (pointer: coarse) and (orientation: portrait)').matches ||
+        window.matchMedia('(min-height: 321px) and (max-height: 411px) and (pointer: coarse) and (orientation: landscape)').matches) {
+        htmlClassList.add("device-mobile", "device-mobile-medium");
+        mobileInfo.isMobile = true;
+        mobileInfo.medium = true;
+    }
+
+    // large mobile (412px - 767px)
+    if (window.matchMedia('(min-width: 412px) and (max-width: 767px) and (pointer: coarse) and (orientation: portrait)').matches ||
+        window.matchMedia('(min-height: 412px) and (max-height: 767px) and (pointer: coarse) and (orientation: landscape)').matches) {
+        mobileInfo.isMobile = true;
+        mobileInfo.large = true;
+    }
+
+    return mobileInfo;
+}
+
+function isMobile() {
+    var mobileInfo = getMobileInfo();
+
+    return mobileInfo.isMobile;
+}
+
+// TODO: extend to cover non-Apple tablet devices
+function getTabletInfo() {
+    var tabletInfo = {
+        isTablet: false,
+        isIPad: false,
+        isIPadMini: false,
+        isIPadPro: false,
+        isIPad10_2: false, // 10.2"
+        isIPad10_5: false, // 10.5"
+        isIPad11: false, // 11"
+        isIPad12_9: false // 12.9"
+    };
+
+    // iPad Mini
+    if (window.matchMedia('(device-width: 768px) and (device-height: 1024px) and (orientation: portrait)').matches ||
+        window.matchMedia('(device-width: 1024px) and (device-height: 768px) and (orientation: landscape)').matches) {
+        tabletInfo.isTablet = true;
+        tabletInfo.isIPad = true;
+        tabletInfo.isIPadMini = true;
+    }
+
+    // iPad Pro 10.2"
+    if (window.matchMedia('(device-width: 810px) and (device-height: 1080px) and (orientation: portrait)').matches ||
+        window.matchMedia('(device-width: 1080px) and (device-height: 810px) and (orientation: landscape)').matches) {
+        tabletInfo.isTablet = true;
+        tabletInfo.isIPad = true;
+        tabletInfo.isIPadPro = true;
+        tabletInfo.isIPad10_2 = true;
+    }
+
+    // iPad Pro 10.5"
+    if (window.matchMedia('(device-width: 834px) and (device-height: 1112px) and (orientation: portrait)').matches ||
+        window.matchMedia('(device-width: 1112px) and (device-height: 834px) and (orientation: landscape)').matches) {
+        tabletInfo.isTablet = true;
+        tabletInfo.isIPad = true;
+        tabletInfo.isIPadPro = true;
+        tabletInfo.isIPad10_5 = true;
+    }
+
+    // iPad Pro 11"
+    if (window.matchMedia('(device-width: 834px) and (device-height: 1194px) and (orientation: portrait)').matches ||
+        window.matchMedia('(device-width: 1194px) and (device-height: 834px) and (orientation: landscape)').matches) {
+        tabletInfo.isTablet = true;
+        tabletInfo.isIPad = true;
+        tabletInfo.isIPadPro = true;
+        tabletInfo.isIPad11 = true;
+    }
+
+    // iPad Pro 12.9"
+    if (window.matchMedia('(device-width: 1024px) and (device-height: 1366px) and (orientation: portrait)').matches ||
+        window.matchMedia('(device-width: 1366px) and (device-height: 1024px) and (orientation: landscape)').matches) {
+        tabletInfo.isTablet = true;
+        tabletInfo.isIPad = true;
+        tabletInfo.isIPadPro = true;
+        tabletInfo.isIPad12_9 = true;
+    }
+
+    return tabletInfo;
+}
+
+function isTablet() {
+    var tabletInfo = getTabletInfo();
+
+    return tabletInfo.isTablet;
+}
+
+function isIPad() {
+    var tabletInfo = getTabletInfo();
+
+    return tabletInfo.isIPad;
+}
+
+function isDesktop() {
+    var mobileInfo = getMobileInfo();
+    var tabletInfo = getTabletInfo();
+
+    // NB: fallback for IE10+ which doesn't support pointer media queries
+    return !mobileInfo.isMobile && !tabletInfo.isTablet && (window.matchMedia('(pointer: fine)').matches || isIE10Plus);
+}
+
 if (global.document && global.document.getElementsByTagName("html").length) {
     // user agent detection
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
@@ -59,73 +178,75 @@ if (global.document && global.document.getElementsByTagName("html").length) {
         htmlClassList.add("platform-mac");
     }
 
-    // mobile checks (appproximate!)
-    var isMobile = false;
+    // mobile checks
+    var mobileInfo = getMobileInfo();
 
-    // small mobile (>= 320px)
-    if (window.matchMedia('(max-width: 320px) and (pointer: coarse) and (orientation: portrait)').matches ||
-        window.matchMedia('(max-height: 320px) and (pointer: coarse) and (orientation: landscape)').matches) {
-        htmlClassList.add("device-mobile", "device-mobile-small");
-        isMobile = true;
+    if (mobileInfo.isMobile) {
+        htmlClassList.add("device-mobile");
+
+        if (mobileInfo.small) {
+            htmlClassList.add("device-mobile-small");
+        }
+
+        if (mobileInfo.medium) {
+            htmlClassList.add("device-mobile-medium");
+        }
+
+        if (mobileInfo.large) {
+            htmlClassList.add("device-mobile-large");
+        }
+
     }
 
-    // medium mobile (321px - 411px)
-    if (window.matchMedia('(min-width: 321px) and (max-width: 411px) and (pointer: coarse) and (orientation: portrait)').matches ||
-        window.matchMedia('(min-height: 321px) and (max-height: 411px) and (pointer: coarse) and (orientation: landscape)').matches) {
-        htmlClassList.add("device-mobile", "device-mobile-medium");
-        isMobile = true;
-    }
+    // tablet checks
+    var tabletInfo = getTabletInfo();
 
-    // large mobile (412px - 767px)
-    if (window.matchMedia('(min-width: 412px) and (max-width: 767px) and (pointer: coarse) and (orientation: portrait)').matches ||
-        window.matchMedia('(min-height: 412px) and (max-height: 767px) and (pointer: coarse) and (orientation: landscape)').matches) {
-        htmlClassList.add("device-mobile", "device-mobile-large");
-        isMobile = true;
-    }
+    if (tabletInfo.isTablet) {
+        htmlClassList.add("device-tablet");
 
+        if (tabletInfo.isIPad) {
+            htmlClassList.add("device-ipad");
 
-    // iPad checks
-    var isiPad = false;
+            if (tabletInfo.isIPadMini) {
+                htmlClassList.add("device-ipad-mini");
+            }
 
-    // iPad Mini
-    if (window.matchMedia('(device-width: 768px) and (device-height: 1024px) and (orientation: portrait)').matches ||
-        window.matchMedia('(device-width: 1024px) and (device-height: 768px) and (orientation: landscape)').matches) {
-        htmlClassList.add("device-ipad", "device-ipad-mini");
-        isiPad = true;
-    }
+            if (tabletInfo.isIPadPro) {
+                htmlClassList.add("device-ipad-pro");
+            }
 
-    // iPad Pro 10.2"
-    if (window.matchMedia('(device-width: 810px) and (device-height: 1080px) and (orientation: portrait)').matches ||
-        window.matchMedia('(device-width: 1080px) and (device-height: 810px) and (orientation: landscape)').matches) {
-        htmlClassList.add("device-ipad", "device-ipad-pro", "device-ipad-pro-10-2");
-        isiPad = true;
-    }
+            if (tabletInfo.isIPad10_2) {
+                htmlClassList.add("device-ipad-pro-10-2");
+            }
 
-    // iPad Pro 10.5"
-    if (window.matchMedia('(device-width: 834px) and (device-height: 1112px) and (orientation: portrait)').matches ||
-        window.matchMedia('(device-width: 1112px) and (device-height: 834px) and (orientation: landscape)').matches) {
-        htmlClassList.add("device-ipad", "device-ipad-pro", "device-ipad-pro-10-5");
-        isiPad = true;
-    }
+            if (tabletInfo.isIPad10_5) {
+                htmlClassList.add("device-ipad-pro-10-5");
+            }
 
-    // iPad Pro 11"
-    if (window.matchMedia('(device-width: 834px) and (device-height: 1194px) and (orientation: portrait)').matches ||
-        window.matchMedia('(device-width: 1194px) and (device-height: 834px) and (orientation: landscape)').matches) {
-        htmlClassList.add("device-ipad", "device-ipad-pro", "device-ipad-pro-11");
-        isiPad = true;
-    }
+            if (tabletInfo.isIPad11) {
+                htmlClassList.add("device-ipad-pro-11");
+            }
 
-    // iPad Pro 12.9"
-    if (window.matchMedia('(device-width: 1024px) and (device-height: 1366px) and (orientation: portrait)').matches ||
-        window.matchMedia('(device-width: 1366px) and (device-height: 1024px) and (orientation: landscape)').matches) {
-        htmlClassList.add("device-ipad", "device-ipad-pro", "device-ipad-pro-12-9");
-        isiPad = true;
+            if (tabletInfo.isIPad12_9) {
+                htmlClassList.add("device-ipad-pro-12-9");
+            }
+
+        }
+
     }
 
     // desktop (approximate!)
-    // NB: fallback for IE10+ which doesn't support pointer media queries
-    if (!isMobile && !isiPad && (window.matchMedia('(pointer: fine)').matches || isIE10Plus)) {
+    if (isDesktop()) {
         htmlClassList.add("device-desktop");
     }
 
 }
+
+module.exports = {
+    getMobileInfo,
+    isMobile,
+    getTabletInfo,
+    isTablet,
+    isIPad,
+    isDesktop
+};
