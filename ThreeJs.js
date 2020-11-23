da64652af84ddb08e06e648f2a86fb7d3ddea93a
Luke Briggs <luke@kulestar.com>
2020-11-23 06:31:01 +0000

    Added update() method to CSS objects

--- ThreeJs.js
+++ ThreeJs.js
@@ -25,6 +25,34 @@ THREE.CSS3DObject = function ( element ) {
 		} );
 
 	} );
+	
+	function epsilon( value ) {
+		return Math.abs( value ) < 1e-10 ? 0 : value;
+	}
+
+	this.update = function(){
+		this.updateMatrixWorld();
+		
+		var elements = this.matrixWorld.elements;
+		this._cssMatrix = 'matrix3d(' +
+			epsilon( elements[ 0 ] ) + ',' +
+			epsilon( elements[ 1 ] ) + ',' +
+			epsilon( elements[ 2 ] ) + ',' +
+			epsilon( elements[ 3 ] ) + ',' +
+			epsilon( - elements[ 4 ] ) + ',' +
+			epsilon( - elements[ 5 ] ) + ',' +
+			epsilon( - elements[ 6 ] ) + ',' +
+			epsilon( - elements[ 7 ] ) + ',' +
+			epsilon( elements[ 8 ] ) + ',' +
+			epsilon( elements[ 9 ] ) + ',' +
+			epsilon( elements[ 10 ] ) + ',' +
+			epsilon( elements[ 11 ] ) + ',' +
+			epsilon( elements[ 12 ] ) + ',' +
+			epsilon( elements[ 13 ] ) + ',' +
+			epsilon( elements[ 14 ] ) + ',' +
+			epsilon( elements[ 15 ] ) +
+		')';
+	}
 
 };
 
