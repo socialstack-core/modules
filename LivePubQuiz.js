3b975fc236afe721a9a87815a4840acb7f1949ae
Luke Briggs <luke@kulestar.com>
2020-11-26 06:51:14 +0000

    30s default

--- LivePubQuiz.js
+++ LivePubQuiz.js
@@ -12,7 +12,7 @@ import webSocket from 'UI/Functions/WebSocket';
 // <LivePubQuiz startTime={time} id={quizId} />
 
 // in seconds
-var questionDuration = 25;
+var questionDuration = 30;
 
 export default class LivePubQuiz extends React.Component {
 	
