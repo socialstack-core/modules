cdc4acef586ea3948d99c58da369f0ef08189452
Luke Briggs <luke@kulestar.com>
2020-11-26 02:39:08 +0000

    GetRef now works with modern image formats like webP and gained some other useful utility functions like isImage(aRef)

--- GetRef.js
+++ GetRef.js
@@ -7,25 +7,8 @@
 */
 
 export default function getRef(ref, options) {
-	if(!ref){
-		return null;
-	}
-	var protoIndex = ref.indexOf(':')
-	
-	if(protoIndex == -1){
-		return null;
-	}
-	
-	var proto = ref.substring(0, protoIndex);
-	var handler = protocolHandlers[proto];
-	if(!handler){
-		return null;
-	}
-	
-	options = options || {};
-	options.protocol = proto;
-	ref = ref.substring(protoIndex+1);
-	return handler(ref, options);
+	var r = getRef.parse(ref);
+	return r ? r.handler(r.ref, options, r) : null;
 }
 
 function basicUrl(url, options){
@@ -37,8 +20,8 @@ function basicUrl(url, options){
 	return (<img src={url} {...options.attribs} />);
 }
 
-function contentFile(ref, options){
-	var url = options.protocol == 'public' ? '/content/' : '/content-private/';
+function contentFile(ref, options, r){
+	var url = r.scheme == 'public' ? '/content/' : '/content-private/';
 	
 	var dirs = ref.split('/');
 	ref = dirs.pop();
@@ -69,7 +52,7 @@ function contentFile(ref, options){
 	
 	var video = (type == 'mp4' || type == 'ogg' || type == 'webm' || type == 'avi');
 	
-	url = url + id + '-' + (video ? 'original' : (options.size || 'original')) + '.' + type;
+	url = url + id + '-' + ((video || type == 'svg' || type == 'apng' || type == 'gif') ? 'original' : (options.size || 'original')) + '.' + type;
 	
 	if(global.apiHost){
 		url = global.apiHost + url;
@@ -80,21 +63,21 @@ function contentFile(ref, options){
 	}
 	
 	if(video){
-		return (<video src={url} width={300} controls {...options.attribs} />);
+		return (<video src={url} width={options.size || 256} controls {...options.attribs} />);
 	}
 	
 	// React component by default:
-	return (<img src={url} {...options.attribs} />);
+	return (<img src={url} width={options.size || undefined} {...options.attribs} />);
 }
 
-function fontAwesomeIcon(ref, options){
+function fontAwesomeIcon(ref, options, r){
 	// Note: doesn't support options.url (yet!)
 	if(options.url){
 		return '';
 	}
 
 	// allows us to specify FontAwesome modifier classes, e.g. fa-fw
-	var className = options.protocol + ' ' + ref;
+	var className = r.scheme + ' ' + ref;
 
 	if (options.className) {
 		className += ' ' + options.className;
@@ -116,6 +99,8 @@ var protocolHandlers = {
 	'public': contentFile,
 	'private': contentFile,
 	'url': basicUrl,
+	'http': basicUrl,
+	'https': basicUrl,
 	'fa': fontAwesomeIcon,
 	'fas': fontAwesomeIcon,
 	'far': fontAwesomeIcon,
@@ -125,3 +110,62 @@ var protocolHandlers = {
 	'fr': fontAwesomeIcon, // Four Roads icon fonts use "fr" as opposed to "fa"
 	'emoji': emojiStr
 };
+
+getRef.parse = (ref) => {
+	if(!ref){
+		return null;
+	}
+	if(ref.scheme){
+		return ref;
+	}
+	var protoIndex = ref.indexOf(':')
+	
+	if(protoIndex == -1){
+		return null;
+	}
+	
+	var scheme = ref.substring(0, protoIndex);
+	var handler = protocolHandlers[scheme];
+	
+	if(!handler){
+		return null;
+	}
+	
+	ref = ref.substring(protoIndex+1);
+	var fileParts = null;
+	var fileType = null;
+	
+	if(ref.indexOf('.') != -1){
+		fileParts = ref.split('.');
+		fileType = fileParts[fileParts.length-1];
+	}
+	
+	return {
+		scheme,
+		handler,
+		ref,
+		fileType,
+		fileParts
+	};
+};
+
+/*
+* Convenience method for identifying visual refs (including videos).
+*/
+var imgTypes = ['png', 'jpeg', 'jpg', 'gif', 'mp4', 'svg', 'bmp', 'apng', 'avif', 'webp'];
+
+getRef.isImage = (ref) => {
+	var info = getRef.parse(ref);
+	if(!info){
+		return false;
+	}
+	
+	if(info.scheme == 'private'){
+		return false;
+	}else if(info.scheme == 'url' || info.scheme == 'http' || info.scheme == 'https' || info.scheme == 'public'){
+		return (imgTypes.indexOf(info.fileType) != -1);
+	}
+	
+	// All other ref types are visual:
+	return true;
+}
