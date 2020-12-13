8659a275640c6fdc4e7ec1f80c619197684ff992
Luke Briggs <luke@kulestar.com>
2020-12-13 22:57:04 +0000

    Initial commit

---
+++ ChatList/ChatList.js
@@ -0,0 +1,118 @@
+import Time from 'UI/Time';
+import Loop from 'UI/Loop';
+import Modal from 'UI/Modal';
+import getRef from 'UI/Functions/GetRef';
+
+export default class ChatList extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	handleHeaderToggle(replacementTitle) {
+		// only do this for mobile
+		// (effectively anything below iPad portrait res in this case)
+		if (window.matchMedia("(max-width: 767px) and (pointer: coarse)").matches) {
+			global.app.setState({
+				searchableHeaderShowClose: true,
+				searchableHeaderTitle: replacementTitle
+			});
+		}
+	}
+	
+	renderUser(user, subject, dateEdited, users) {
+		return <div className="user-chat">
+			{getRef(user.avatarRef, { size: 256 })}
+			<div className="user-details">
+				<h3 className="user-name">
+					{user.firstName + ' ' + user.lastName}
+				</h3>
+				{!this.props.meetingRequest && 
+					<p className="subject-title">
+						{subject}
+					</p>
+				}
+			</div>
+			{this.props.showTime && dateEdited &&
+                <Time className="chat-date" absolute compact shortDay compactDayOnly withDate date={dateEdited} />
+			}
+		</div>;
+	}
+	
+	render(){
+		
+		var { user } = global.app.state;
+		
+		if(!user){
+			return;
+		}
+		
+		var paginationSettings = {
+			pageSize: 10,
+			showInput: false,
+			maxLinks: 4
+		};
+		
+		var filter = {
+		...(this.props.filter || {}),
+			sort:{
+				field: 'EditedUtc',
+				direction: 'desc'
+			}
+		};
+		
+		return <div className="live-support-chats">
+			<Loop raw paged={paginationSettings} live='LiveSupportChat' over={'livesupportchat/list'} filter={filter}>
+				{
+					chatChannel => {
+						// Establish who we're chatting with.
+						// The chat is with all of the given permitted users (one of whom is "me")
+						var users = chatChannel.permittedUsers || [];
+						users = users.map(permit => permit.permitted).filter(p => p!=null && !(p.type == "User" && p.id == user.id));
+						
+						if(!users.length){
+							// This chat channel is with a deleted user, 
+							// or anon (as is the case on coms from the homepage).
+							users.push({
+								type: 'User',
+								firstName: 'Anonymous',
+								lastName:''
+							});
+						}
+						
+						var chattingWith = users[0];
+						return (
+							<button type="button" className="chat-request" onClick={() => {
+								var name = "Message";
+								
+								if (chattingWith.firstName && chattingWith.lastName) {
+									name = chattingWith.firstName + ' ' + chattingWith.lastName;
+								} else {
+
+									if (chattingWith.name) {
+										name = chattingWith.name;
+									}
+
+								}
+
+								setTimeout(() => {
+									// this.handleHeaderToggle(name);
+									this.props.onClick && this.props.onClick(chatChannel);
+								}, 10);
+							}}>
+								{this.renderUser(chattingWith, chatChannel.subject, chatChannel.editedUtc, users)}
+							</button>
+						);
+						
+					}
+				}
+			</Loop>
+		</div>;
+		
+	}
+	
+}
+
+ChatList.propTypes = {
+	companyInbox: 'bool'
+};
---
+++ LiveSupport.js
@@ -0,0 +1,63 @@
+import Row from 'UI/Row';
+import Col from 'UI/Column';
+import ChatList from 'Admin/LiveSupportAdmin/ChatList';
+import MessageList from 'UI/LiveSupport/MessageList';
+import webRequest from 'UI/Functions/WebRequest';
+
+
+export default class LiveSupport extends React.Component {
+	
+	constructor(props){
+		super(props);
+	}
+	
+	render() {
+		var { user } = global.app.state;
+		
+		if(!user){
+			return;
+		}
+		
+		var landingPageClass = "live-chat-admin";
+	
+		if (this.state.chat) {
+			landingPageClass += " chat-visible";	
+		}
+		
+		return <div className={landingPageClass}>
+			<Row>
+				<Col className="messages-navigation" sizeXs={12} sizeSm={6} sizeMd={5}>
+					<section className="meeting-chats">
+						<h2 className="meeting-chats-title">
+							Open support requests
+						</h2>
+						<ChatList showTime filter={{AssignedToUserId: null}} onClick={chat => {
+							this.setState({
+								meetingRequest: null,
+								chat
+							})
+						}} />
+						<h2 className="meeting-chats-title">
+							Your requests
+						</h2>
+						<ChatList showTime filter={{AssignedToUserId: user.id}} onClick={chat => {
+							this.setState({
+								meetingRequest: null,
+								chat
+							})
+						}} />
+					</section>
+				</Col>
+
+				<Col className="messages-preview" sizeXs={12} sizeSm={6} sizeMd={7}>
+					{ this.state.chat && <MessageList chat={this.state.chat} /> }
+				</Col>
+			</Row>
+		</div>;
+		
+	}
+	
+}
+
+LiveSupport.propTypes = {
+};
