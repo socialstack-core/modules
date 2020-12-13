10da4cee73f6b9e62b7bfecb61185e6818e0f3c7
Luke Briggs <luke@kulestar.com>
2020-12-13 23:42:38 +0000

    Removed UserRestrictions as these aren't going to function as group chats anymore

--- LiveSupportChat.cs
+++ LiveSupportChat.cs
@@ -13,28 +13,13 @@ namespace Api.LiveSupportChats
 	/// <summary>
 	/// A LiveSupportChat
 	/// </summary>
-	public partial class LiveSupportChat : RevisionRow, IHaveUserRestrictions, IAmLive
+	public partial class LiveSupportChat : RevisionRow, IAmLive
 	{
         /// <summary>
 		/// Total messages in this chat.
 		/// Updating this triggers the LastEditedUtc date to change.
 		/// </summary>
 		public int MessageCount;
-		
-		/// <summary>
-		/// Access to a chat is restricted to specific people.
-		/// </summary>
-		public bool UserRestrictionsActive => true;
-
-		/// <summary>
-		/// Users are allowed to see other users who are in the chat with them.
-		/// </summary>
-		public bool PermittedUsersListVisible => true;
-
-		/// <summary>
-		/// Users in a chat
-		/// </summary>
-		public List<PermittedContent> PermittedUsers { get; set; }
 		
 		/// <summary>
 		/// User ID of current support worker responding to the chat.
