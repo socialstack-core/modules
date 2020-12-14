ca6c53346f5672c3960004d35ffa147d293fe2e8
Luke Briggs <luke@kulestar.com>
2020-12-14 00:35:30 +0000

    Added human agent option

--- MessageTypeSelect/MessageTypeSelect.js
+++ MessageTypeSelect/MessageTypeSelect.js
@@ -6,6 +6,7 @@ export default class MessageTypeSelect extends React.Component {
 		return <Input {...this.props} type="select">
 				<option value="0">User response is just free text</option>
 				<option value="1">Form input - payload contains some sort of form field for the user to fill</option>
+				<option value="2">Hand over to human agent</option>
 			</Input>;
 	}
 	
