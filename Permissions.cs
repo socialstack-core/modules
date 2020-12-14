e429b3b21dc5a4716092f7d3c138f8c4bec0d7e4
Luke Briggs <luke@kulestar.com>
2020-12-14 00:34:27 +0000

    Basic permissions (needs securing)

--- Permissions.cs
+++ Permissions.cs
@@ -26,30 +26,33 @@ namespace Api.LiveSupportChats
 				Member role: A verified user account. Not an admin.
 				Guest role: A user account. The transition from guest to member is up to you.
 				Public role: Not logged in at all.
+				*/
 				
 				// Allow public creation (as it's disabled by default):
 				Roles.Member.Grant("liveSupportChat_create");
 				Roles.Public.Grant("liveSupportChat_create");
 				Roles.Guest.Grant("liveSupportChat_create");
 				
+				/*
 				// Remove public viewing (as it's enabled by default):
 				Roles.Guest.Revoke("liveSupportChat_load", "liveSupportChat_list");
 				Roles.Public.Revoke("liveSupportChat_load", "liveSupportChat_list");
 				Roles.Member.Revoke("liveSupportChat_load", "liveSupportChat_list");
 				*/
-				
 				/*
 				Example permission rules.
 				
 				Member role: A verified user account. Not an admin.
 				Guest role: A user account. The transition from guest to member is up to you.
 				Public role: Not logged in at all.
+				*/
 				
 				// Allow public creation (as it's disabled by default):
 				Roles.Member.Grant("liveSupportMessage_create");
 				Roles.Public.Grant("liveSupportMessage_create");
 				Roles.Guest.Grant("liveSupportMessage_create");
 				
+				/*
 				// Remove public viewing (as it's enabled by default):
 				Roles.Guest.Revoke("liveSupportMessage_load", "liveSupportMessage_list");
 				Roles.Public.Revoke("liveSupportMessage_load", "liveSupportMessage_list");
