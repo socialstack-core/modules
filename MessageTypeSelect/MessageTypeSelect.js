9b6af63de7338b11d92c391b1723a774aad8aebe
Michael Rogers <michaelr@conceptsoftworks.com>
2020-12-17 23:25:02 -0700

    Added support for more MessageTypeSelects for validation messages and chat termination messages.

--- MessageTypeSelect/MessageTypeSelect.js
+++ MessageTypeSelect/MessageTypeSelect.js
@@ -7,6 +7,10 @@ export default class MessageTypeSelect extends React.Component {
 				<option value="0">User response is just free text</option>
 				<option value="1">Form input - payload contains some sort of form field for the user to fill</option>
 				<option value="2">Hand over to human agent</option>
+				<option value="3">Validated response must be a full name</option>
+				<option value="4">Validated response must be an email address</option>
+				<option value="5">Validated response must be a phone number</option>
+				<option value="6">When received, terminates chat.</option>
 			</Input>;
 	}
 	
