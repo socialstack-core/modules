e003efb1692b13f19b84a0860d489bc002415b87
Michael Rogers <michaelr@conceptsoftworks.com>
2020-12-22 22:58:37 -0700

    Added long form date format for EU dates.

--- FormatTime.js
+++ FormatTime.js
@@ -3,6 +3,8 @@ import dateTools from 'UI/Functions/DateTools';
 /*
 * Very basic date/time formatting, using a US date or EU date for now format.
 */
+const longMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 export default function FormatTime(date, format){
     if(!date){
         return '-';
@@ -36,6 +38,9 @@ export default function FormatTime(date, format){
     }
     else if(format == "eu") {
         return day + "-" + month + "-" + year + " " + hour + ":" + minute + " " + meridiem;
+    }
+    else if (format == "eu-readable") {
+        return day + " " + longMonths[month - 1] + " " + year;
     }
     else {
         // Defaulting to Euro, even though its listed twice.
