b436919622adefc92ed5dc067704ed1114204fd1
Luke Briggs <luke@kulestar.com>
2021-01-19 00:01:30 +0000

    Removed project specific fade

--- Heading.js
+++ Heading.js
@@ -1,5 +1,4 @@
 import omit from 'UI/Functions/Omit';
-import userAgent from 'UI/Functions/UserAgent';
 
 /**
  * For h1/h2/h3 etc.
@@ -9,11 +8,7 @@ export default class Heading extends React.Component {
 	render() {
 		var Mod = 'h' + (this.props.size || '1');
 		var className = 'heading ' + (this.props.className || '');
-
-		// fade vertically on mobile to prevent triggering horizontal scrolling issues
-		var fadeDirection = userAgent.isMobile() ? "fade-up" : "fade-right";
-		
-		return <Mod data-aos={fadeDirection} {...omit(this.props, ['children', 'className'])}>{this.props.children}</Mod>;
+		return <Mod {...omit(this.props, ['children', 'className'])}>{this.props.children}</Mod>;
 	}
 	
 }
