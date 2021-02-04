27a4e16c21ca5bdcb3124fa3e96725d019966670
Ferdinand Fabrigas Jr <ferdie.fabrigas@4-roads.com>
2021-02-04 18:17:32 +0800

    Minor styling changes for mobile

--- Banner.js
+++ Banner.js
@@ -3,42 +3,41 @@ import getRef from 'UI/Functions/GetRef';
 export default class Banner extends React.Component{
     render(){
 		var {fontColor, backgroundRef, backgroundColor, title, description, height, textWidth} = this.props;
-		
-		var style = {};
-		
+
+		var style = {
+      minHeight: height || '80vh'
+    };
+
 		if(backgroundRef && backgroundRef.length){
 			style.backgroundImage = "url("+getRef(backgroundRef, {url: true, size: 'original'})+")";
 		}
-		
+
 		if(backgroundColor && backgroundColor.length){
 			style.backgroundColor = backgroundColor;
 		}
-		
-		if(height){
-			style.height = height;
-		}
-		
+
 		if(!textWidth){
 			textWidth = 6;
 		}
-		
+
 		var halfWidth = (12-textWidth)/2;
-		
+
 		var textStyle = {};
 		if(fontColor && fontColor.length){
 			textStyle.color = fontColor;
 		}
-		
+
         return(
 			<div className="hero-banner" style ={style}>
-				<div className="container h-100">
-					<div className="row h-100 align-items-center">
+				<div className="container">
+					<div className="row align-items-center">
 						<div className={"col-" + halfWidth}></div>
 						<div className={"col-" + textWidth + " banner-text"}>
 							<h1 className="title" style={textStyle} dangerouslySetInnerHTML={{__html: title}} />
-							<br/>
-							<br/>
-							<p style={textStyle} dangerouslySetInnerHTML={{__html: description}}/>
+              {description && <>}
+							   <br/><br/>
+                 <p style={textStyle} dangerouslySetInnerHTML={{__html: description}}/>
+               </>}
 						</div>
 					</div>
 				</div>
