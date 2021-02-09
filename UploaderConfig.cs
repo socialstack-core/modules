3563cac4723b9824938b8b69ce3f2d3d4b29bdae
Luke Briggs <luke@kulestar.com>
2021-02-09 18:59:42 +0000

    Added 2048 as a new default size

--- UploaderConfig.cs
+++ UploaderConfig.cs
@@ -13,7 +13,7 @@ namespace Api.Uploader
 		/// <summary>
 		/// When an image is uploaded, it'll be automatically resized to each of these sizes.
 		/// </summary>
-		public int[] ImageSizes = new int[]{ 32, 64, 100, 128, 200, 256, 512, 1024 };
+		public int[] ImageSizes = new int[]{ 32, 64, 100, 128, 200, 256, 512, 1024, 2048 };
     }
 	
 }
