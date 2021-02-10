fcff17d8196777a18c222dec571402465ceacfb8
Luke Briggs <luke@kulestar.com>
2021-02-10 16:55:29 +0000

    Initial commit

---
+++ GoogleAnalyticsServiceConfig.cs
@@ -0,0 +1,15 @@
+using Api.Configuration;
+​
+namespace Api.GoogleAnalytics
+{
+    /// <summary>
+    /// Configurations used by the GoogleAnalyticsService
+    /// </summary>
+    public class GoogleAnalyticsServiceConfig : Config
+    {
+        /// <summary>
+        /// The google analytics Id.
+        /// </summary>
+        public string Id;
+    }
+}
