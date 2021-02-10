189866e578a53379718426ae96d25decbe3709cf
Michael Rogers <michaelr@conceptsoftworks.com>
2021-02-10 15:43:20 -0700

    Added Meta tags for blogPost image.

--- BlogPost.cs
+++ BlogPost.cs
@@ -2,6 +2,7 @@ using System;
 using Api.Database;
 using Api.Startup;
 using Api.Users;
+using Api.Pages;
 
 namespace Api.Blogs
 {
@@ -15,16 +16,19 @@ namespace Api.Blogs
 		/// The blog this post is in.
 		/// </summary>
 		public int BlogId;
+
 		/// <summary>
 		/// The primary ID of the page that this blog post appears on.
 		/// </summary>
 		public int PageId;
+
 		/// <summary>
 		/// The post title in the site default language.
 		/// </summary>
 		[DatabaseField(Length = 200)]
 		[Meta("title")]
 		public string Title;
+
 		/// <summary>
 		/// The JSON body of the post. It's JSON because it is a *canvas*. 
 		/// This means the reply can easily include other components such as polls etc 
@@ -32,15 +36,18 @@ namespace Api.Blogs
 		/// </summary>
 		// [DatabaseField(Length = 8000)]
 		public string BodyJson;
+
 		/// <summary>
 		/// URL slug (optional).
 		/// </summary>
 		[DatabaseField(Length = 200)]
 		public string Slug;
+
 		/// <summary>
 		/// The feature image ref. See also: "Upload.Ref" in the Uploads module.
 		/// </summary>
 		[DatabaseField(Length = 200)]
+		[Meta("image")]
 		public string FeatureRef;
 
 		/// <summary>
