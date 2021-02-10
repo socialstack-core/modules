7aa162ced6d0902acc845860463bd625af806a28
Conan Ablewhite <conan.ablewhite@4-roads.com>
2021-02-10 19:34:00 +0000

    Image float / 2048px size support added

--- Image.js
+++ Image.js
@@ -9,11 +9,27 @@ Min required props: size, fileRef. Size must be an available size from your uplo
 */
 export default class Image extends React.Component {
 	render(){
-		const {onClick, fileRef, linkUrl, size, fullWidth} = this.props;
+		const { onClick, fileRef, linkUrl, size, fullWidth, float } = this.props;
+
+		var imageClass = "image";
+
+		switch (float) {
+			case "Left":
+				imageClass += " image-left";
+				break;
+
+			case "Right":
+				imageClass += " image-right";
+				break;
+		}
+
+		if (fullWidth) {
+			imageClass += " image-wide";
+		}
 		
 		var attribs = omit(this.props, ['fileRef', 'onClick', 'linkUrl', 'size', 'fullWidth']);
 		attribs.alt = attribs.alt || attribs.title;
-		var img = <div className={fullWidth ? "image image-wide" : "image"} onClick={this.props.onClick}>
+		var img = <div className={imageClass} onClick={this.props.onClick}>
 			{getRef(this.props.fileRef, {attribs, size})}
 		</div>;
 		return linkUrl ? <a alt={attribs.alt} title={attribs.title} href={linkUrl}>{img}</a> : img;
@@ -25,7 +41,8 @@ Image.propTypes = {
 	linkUrl: 'string',
 	title: 'string',
 	fullWidth: 'bool',
-	size: ['original', '1024', '512', '256', '200', '128', '100', '64', '32'] // todo: pull from api
+	size: ['original', '2048', '1024', '512', '256', '200', '128', '100', '64', '32'], // todo: pull from api
+	float: { type: ['None', 'Left', 'Right'], default: 'None' }
 };
 
 Image.icon = 'image';
