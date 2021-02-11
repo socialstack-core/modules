b69270d53380f55a87966185225e1b5f6d29215a
Luke Briggs <luke@kulestar.com>
2021-02-11 20:12:28 +0000

    Fixed a doubling-up of /v1/ on urls

--- SubmitForm.js
+++ SubmitForm.js
@@ -43,6 +43,10 @@ export default function (e, options) {
 	}
 	
 	var action = e.target.action;
+	var actionPieces = action.split('/v1/');
+	if(actionPieces.length >= 2){
+		action = actionPieces[1];
+	}
 	
 	values.setAction = (newAction) => {
 		action = newAction;
