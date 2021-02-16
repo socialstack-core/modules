daa8f062e8d2db5eb2073944415a5b2eeccd36a2
Luke Briggs <luke@kulestar.com>
2021-02-16 13:59:08 +0000

    Changed to stateless functional component

--- Loading.js
+++ Loading.js
@@ -2,17 +2,13 @@
 /**
  * Standalone component which displays a loader (typically a spinner).
  */
-export default class Loading extends React.Component {
-
-    render() {
-        let message = this.props.message ? this.props.message : "Loading ... ";
-
-        return (
-            <div className="alert alert-info loading">
-                {message}
-                <i className="fas fa-spinner fa-spin" />
-            </div>
-        );
-    }
-
+export default function Loading (props) {
+	let message = props.message || "Loading ... ";
+	
+	return (
+		<div className="alert alert-info loading">
+			{message}
+			<i className="fas fa-spinner fa-spin" />
+		</div>
+	);
 }
