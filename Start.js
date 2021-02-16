e53738f4d6300cfd540cdd2917d4a83c6c0eb794
Luke Briggs <luke@kulestar.com>
2021-02-16 18:59:59 +0000

    Further removal of unnecessary code from starter

--- Start.js
+++ Start.js
@@ -1,7 +1,6 @@
 import App from './App.js';
 
 export default function(custom){
-	
 	if(!custom){
 		// Init all modules.
 		for(var m in __mm){
@@ -9,28 +8,19 @@ export default function(custom){
 		}
 	}
 	
-	if(!global.server){
-		// We're server side otherwise. It would've set global.app internally.
+	var root = document.getElementById('react-root');
+	
+	if(root){
+		// We're server side otherwise.
 		
 		// Start logging in now and update the loader text:
 		var loader = document.getElementById('react-loading');
-		if(loader){
-			loader.parentNode.removeChild(loader);
-		}
+		loader && loader.parentNode.removeChild(loader);
 		
-		global.hashFields = {};
-
-		(location && location.hash) && location.hash.substring(1).split('&').forEach(e => {
-			var parts = e.split('=');
-			global.hashFields[parts[0]] = parts.length>1 ? parts[1] : true;
-		});
-		
-		// Render the root now! When the App instance is created, it sets itself up as this.context.app (as seen by other *mounted* components).
+		// Render the root now! When the App instance is created, it sets itself up as this.context.app (as seen by other mounted components).
 		(React.render || ReactDom.render)(
 			<App />,
-			document.getElementById('react-root')
+			root
 		);
-		
 	}
-	
 };
