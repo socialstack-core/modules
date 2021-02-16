00ff2cc5d48bd17971a23d9ed38fa071b6958b3c
Luke Briggs <luke@kulestar.com>
2021-02-16 14:24:07 +0000

    Made component stateless

--- Spacer.js
+++ Spacer.js
@@ -2,13 +2,10 @@
 Just an invisible space of a specified height. The default is 20px.
 */
 
-export default class Spacer extends React.Component {
-	
-	render(){
-		return <div className="spacer-container">
-			<div className="spacer" style={{height: this.props.height ? (this.props.height + 'px') : '20px'}}/>
-		</div>
-	}
+export default function Spacer (props) {
+	return <div className="spacer-container">
+		<div className="spacer" style={{height: props.height ? (props.height + 'px') : '20px'}}/>
+	</div>;
 }
 
 Spacer.propTypes = {
