d1354e139ed04f0dcb477fe6b3269f50d83946cb
Luke Briggs <luke@kulestar.com>
2021-02-16 02:36:55 +0000

    Stateless component

--- Column.js
+++ Column.js
@@ -4,51 +4,47 @@ import omit from 'UI/Functions/Omit';
  * A 12 segment responsive column. Usually used within a <Row>.
  */
 
-export default class Column extends React.Component {
+export default function Column (props) {
+	var colClass = '';
+	// only define a default col-md- width if we don't have a col-xs- / col-sm- value which overrides it
+	var sizeMd = props.sizeMd || props.size || (!props.sizeXs && !props.sizeMd ? 6 : undefined);
 
-	render() {
-		var props = this.props;
-		var colClass = '';
-		// only define a default col-md- width if we don't have a col-xs- / col-sm- value which overrides it
-		var sizeMd = props.sizeMd || props.size || (!props.sizeXs && !props.sizeMd ? 6 : undefined);
-
-		if (props.sizeXs) {
-			// NB: no col-xs- prefix
-			// ref: https://getbootstrap.com/docs/4.3/layout/grid/
-			colClass = 'col-' + props.sizeXs;
-		}
-
-		if (props.sizeSm) {
-			colClass += ' col-sm-' + props.sizeSm;
-		}
-
-		if (sizeMd) {
-			colClass += ' col-md-' + sizeMd;
-		}
-
-		if (props.sizeLg) {
-			colClass += ' col-lg-' + props.sizeLg;
-		}
-
-		if (props.sizeXl) {
-			colClass += ' col-xl-' + props.sizeXl;
-		}
-
-		if (props.noGutters) {
-			colClass += ' no-gutters';
-		}
-
-		if (props.className) {
-			colClass += ' ' + props.className;
-		}
-
-		return <div
-			className={colClass}
-			{...(omit(this.props, ['className', 'noGutters', 'children', '__canvas']))}
-		>
-			{props.children}
-		</div>;
+	if (props.sizeXs) {
+		// NB: no col-xs- prefix
+		// ref: https://getbootstrap.com/docs/4.3/layout/grid/
+		colClass = 'col-' + props.sizeXs;
 	}
+
+	if (props.sizeSm) {
+		colClass += ' col-sm-' + props.sizeSm;
+	}
+
+	if (sizeMd) {
+		colClass += ' col-md-' + sizeMd;
+	}
+
+	if (props.sizeLg) {
+		colClass += ' col-lg-' + props.sizeLg;
+	}
+
+	if (props.sizeXl) {
+		colClass += ' col-xl-' + props.sizeXl;
+	}
+
+	if (props.noGutters) {
+		colClass += ' no-gutters';
+	}
+
+	if (props.className) {
+		colClass += ' ' + props.className;
+	}
+
+	return <div
+		className={colClass}
+		{...(omit(this.props, ['className', 'noGutters', 'children', '__canvas']))}
+	>
+		{props.children}
+	</div>;
 
 }
 
