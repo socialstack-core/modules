fc9d54d898b8041df0e1bb64fe98f2cca14cffe7
Luke Briggs <luke@kulestar.com>
2021-02-16 02:42:10 +0000

    Stateless component

--- Failed.js
+++ Failed.js
@@ -1,10 +1,8 @@
-export default class Failed extends React.Component{
-    render(){
-        return(
-            <div className="alert alert-danger" role="alert" style = {{textAlign: "center"}}>
-                <i className="fad fa-wifi-slash" />
-                <p>The service is currently unavailable. This may be because your device is currently offline.</p>
-            </div>
-        );
-    }
+export default function Failed () {
+	return(
+		<div className="alert alert-danger" role="alert" style = {{textAlign: "center"}}>
+			<i className="fad fa-wifi-slash" />
+			<p>The service is currently unavailable. This may be because your device is currently offline.</p>
+		</div>
+	);
 }
