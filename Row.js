54b7ef1c041f28530432b8788c2cfa2e18ba1bd8
Luke Briggs <luke@kulestar.com>
2021-02-16 02:37:42 +0000

    Stateless component

--- Row.js
+++ Row.js
@@ -4,18 +4,13 @@ import omit from 'UI/Functions/Omit';
  * A responsive row.
  */
 
-export default class Row extends React.Component {
-	
-	render(){
-		var props=this.props;
-		return <div 
-			className={"row " + (props.noGutters ? "no-gutters" : "") + " " + (props.className || '')}
-			{...(omit(this.props, ['className', 'noGutters', 'children', '__canvas']))}
-		>
-			{props.children}
-		</div>;
-	}
-
+export default function Row (props) {
+	return <div 
+		className={"row " + (props.noGutters ? "no-gutters" : "") + " " + (props.className || '')}
+		{...(omit(this.props, ['className', 'noGutters', 'children', '__canvas']))}
+	>
+		{props.children}
+	</div>;
 }
 
 Row.propTypes={
