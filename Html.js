c8688032aadec9d2d59361afa99dffff941a00e8
Luke Briggs <luke@kulestar.com>
2021-02-16 02:44:05 +0000

    Stateless component

--- Html.js
+++ Html.js
@@ -2,16 +2,8 @@
  * This component displays html. Only ever use this with trusted text.
 */
 
-export default class Html extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-	
-    render() {
-		var ch = this.props.html || this.props.children;
-        return <span {...this.props} dangerouslySetInnerHTML={{__html: ch}} />;
-    }
+export default function Html (props) {
+   return <span {...this.props} dangerouslySetInnerHTML={{__html: props.html || props.children}} />;
 }
 
 Html.propTypes={
