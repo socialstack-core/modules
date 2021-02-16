9bae3c35d5d262e09536e947c3875380ebc6a8e3
Luke Briggs <luke@kulestar.com>
2021-02-16 01:45:42 +0000

    Shrunk further

--- Align.js
+++ Align.js
@@ -1,10 +1,8 @@
 import omit from 'UI/Functions/Omit';
 
-export default Align = () => {
-    render() {
-		return <div style={{textAlign: props.type}} {...(omit(props, ['type', 'children']))}>{props.children}</div>;
-    }
-}
+export default Align = () => <div style={{textAlign: props.type}} {...(omit(props, ['type', 'children']))}>
+	{props.children}
+</div>
 
 Align.propTypes = {
 	type: ['left', 'right', 'center', 'justify'],
