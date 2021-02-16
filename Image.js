eeb1f67c15de8f517dd7ee49a734afcd3c47a062
Luke Briggs <luke@kulestar.com>
2021-02-16 12:48:05 +0000

    Made component functional as it's stateless

--- Image.js
+++ Image.js
@@ -7,33 +7,31 @@ Used to display an image from a fileRef.
 Min required props: size, fileRef. Size must be an available size from your uploader config. You should pick the nearest bigger size from the physical size you're after.
 <Image fileRef='public:2.jpg' size=100 />
 */
-export default class Image extends React.Component {
-	render(){
-		const { onClick, fileRef, linkUrl, size, fullWidth, float } = this.props;
+export default function Image (props) {
+	const { onClick, fileRef, linkUrl, size, fullWidth, float } = props;
 
-		var imageClass = "image";
+	var imageClass = "image";
 
-		switch (float) {
-			case "Left":
-				imageClass += " image-left";
-				break;
+	switch (float) {
+		case "Left":
+			imageClass += " image-left";
+			break;
 
-			case "Right":
-				imageClass += " image-right";
-				break;
-		}
-
-		if (fullWidth) {
-			imageClass += " image-wide";
-		}
-		
-		var attribs = omit(this.props, ['fileRef', 'onClick', 'linkUrl', 'size', 'fullWidth']);
-		attribs.alt = attribs.alt || attribs.title;
-		var img = <div className={imageClass} onClick={this.props.onClick}>
-			{getRef(this.props.fileRef, {attribs, size})}
-		</div>;
-		return linkUrl ? <a alt={attribs.alt} title={attribs.title} href={linkUrl}>{img}</a> : img;
+		case "Right":
+			imageClass += " image-right";
+			break;
 	}
+
+	if (fullWidth) {
+		imageClass += " image-wide";
+	}
+	
+	var attribs = omit(props, ['fileRef', 'onClick', 'linkUrl', 'size', 'fullWidth']);
+	attribs.alt = attribs.alt || attribs.title;
+	var img = <div className={imageClass} onClick={props.onClick}>
+		{getRef(props.fileRef, {attribs, size})}
+	</div>;
+	return linkUrl ? <a alt={attribs.alt} title={attribs.title} href={linkUrl}>{img}</a> : img;
 }
 
 Image.propTypes = {
