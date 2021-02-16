bb5874d3aee032a86f1e766127a5720e9aa6ab86
Luke Briggs <luke@kulestar.com>
2021-02-16 01:50:41 +0000

    Change to being stateless

--- Banner.js
+++ Banner.js
@@ -1,49 +1,47 @@
 import getRef from 'UI/Functions/GetRef';
 
-export default class Banner extends React.Component{
-    render(){
-		var {fontColor, backgroundRef, backgroundColor, title, description, height, textWidth} = this.props;
+export default Banner = props => {
+	var {fontColor, backgroundRef, backgroundColor, title, description, height, textWidth} = props;
 
-		var style = {
-      minHeight: height || '80vh'
-    };
+	var style = {
+		minHeight: height || '80vh'
+	};
 
-		if(backgroundRef && backgroundRef.length){
-			style.backgroundImage = "url("+getRef(backgroundRef, {url: true, size: 'original'})+")";
-		}
+	if(backgroundRef && backgroundRef.length){
+		style.backgroundImage = "url("+getRef(backgroundRef, {url: true, size: 'original'})+")";
+	}
 
-		if(backgroundColor && backgroundColor.length){
-			style.backgroundColor = backgroundColor;
-		}
+	if(backgroundColor && backgroundColor.length){
+		style.backgroundColor = backgroundColor;
+	}
 
-		if(!textWidth){
-			textWidth = 6;
-		}
+	if(!textWidth){
+		textWidth = 6;
+	}
 
-		var halfWidth = (12-textWidth)/2;
+	var halfWidth = (12-textWidth)/2;
 
-		var textStyle = {};
-		if(fontColor && fontColor.length){
-			textStyle.color = fontColor;
-		}
+	var textStyle = {};
+	if(fontColor && fontColor.length){
+		textStyle.color = fontColor;
+	}
 
-        return(
-			<div className="hero-banner" style ={style}>
-				<div className="container">
-					<div className="row align-items-center">
-						<div className={"col-" + halfWidth}></div>
-						<div className={"col-" + textWidth + " banner-text"}>
-							<h1 className="title" style={textStyle} dangerouslySetInnerHTML={{__html: title}} />
-              {description && <>}
-							   <br/><br/>
-                 <p style={textStyle} dangerouslySetInnerHTML={{__html: description}}/>
-               </>}
-						</div>
+	return(
+		<div className="hero-banner" style ={style}>
+			<div className="container">
+				<div className="row align-items-center">
+					<div className={"col-" + halfWidth}></div>
+					<div className={"col-" + textWidth + " banner-text"}>
+						<h1 className="title" style={textStyle} dangerouslySetInnerHTML={{__html: title}} />
+		  {description && <>}
+						   <br/><br/>
+			 <p style={textStyle} dangerouslySetInnerHTML={{__html: description}}/>
+		   </>}
 					</div>
 				</div>
 			</div>
-        );
-    }
+		</div>
+	);
 }
 
 Banner.propTypes = {
