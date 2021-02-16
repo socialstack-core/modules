0c70d9aa316f157f64fb4b719fb9985b4732791c
Luke Briggs <luke@kulestar.com>
2021-02-16 14:17:35 +0000

    Made substitute a stateless functional component

--- Substitute.js
+++ Substitute.js
@@ -2,17 +2,9 @@
 /**
  * This component will substitute itself with something known by the canvas by calling onSubstitute on the canvas itself.
  */
-export default class Substitute extends React.Component {
-	
-	constructor(props){
-		super(props);
-	}
-	
-	render(){
-		var canvas = this.props.__canvas;
-		return canvas && canvas.props.onSubstitute && canvas.props.onSubstitute(this.props.name) || null;
-	}
-	
+export default function Substitute (props) {
+	var canvas = props.__canvas;
+	return canvas && canvas.props.onSubstitute && canvas.props.onSubstitute(props.name) || null;
 }
 
 Substitute.propTypes={
