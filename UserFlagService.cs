c91d91fc9c1d2425e6f2499e2e816043306864f7
Michael Rogers <michaelr@conceptsoftworks.com>
2021-02-19 15:57:23 -0700

    Added return of UserFlagOption to UserFlag Load and List.

--- UserFlagService.cs
+++ UserFlagService.cs
@@ -14,6 +14,9 @@ namespace Api.UserFlags
 	/// </summary>
 	public partial class UserFlagService : AutoService<UserFlag>
     {
+
+		private UserFlagOptionService _ufo;
+
 		/// <summary>
 		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 		/// </summary>
@@ -38,6 +41,48 @@ namespace Api.UserFlags
 				}
 
 				return flag;
+			});
+
+			Events.UserFlag.AfterLoad.AddEventListener(async (Context context, UserFlag flag) =>
+			{
+				if (flag == null)
+                {
+					return flag;
+                }
+				
+				// Let's grab the option if it has one.
+				if(_ufo == null)
+                {
+					_ufo = Services.Get<UserFlagOptionService>();
+                }
+
+				// Grab the user flag option
+				var flagOption = await _ufo.Get(context, flag.UserFlagOptionId);
+				flag.UserFlagOption = flagOption;
+
+				return flag;
+			});
+
+			Events.UserFlag.AfterList.AddEventListener(async (Context context, List<UserFlag> userFlags) =>
+			{
+				if(userFlags == null)
+                {
+					return userFlags;
+                }
+
+				// Let's grab the option if it has one.
+				if (_ufo == null)
+				{
+					_ufo = Services.Get<UserFlagOptionService>();
+				}
+
+				foreach (var flag in userFlags)
+                {
+					var flagOption = await _ufo.Get(context, flag.UserFlagOptionId);
+					flag.UserFlagOption = flagOption;
+				}
+
+				return userFlags;
 			});
 
 			
