0ec5c450144900efd945575d8fb617e9ab0d1233
Luke Briggs <luke@kulestar.com>
2021-02-23 20:07:34 +0000

    Huge strip back of PageRouter - the server is now in control of all requests. A new non server reliant router for native apps will be available soon.

--- PageRouter.js
+++ PageRouter.js
@@ -1,15 +1,10 @@
 import webRequest from 'UI/Functions/WebRequest';
 import Canvas from 'UI/Canvas';
-import Content from 'UI/Content';
 
 export default class PageRouter extends React.Component{
 	
 	go(url){
-		if(global.storedToken){
-			global.history.pushState({}, "", '#' + url);
-		}else{
-			global.history.pushState({}, "", url);
-		}
+		global.history.pushState({}, "", global.storedToken ? '#' + url : url);
 		document.body.parentNode.scrollTop=0;
 		this.props.onNavigate && this.props.onNavigate(url);
 	}
@@ -20,162 +15,8 @@ export default class PageRouter extends React.Component{
 		context.pageRouter = this;
 		this.onLinkClick = this.onLinkClick.bind(this);
 		this.onPopState = this.onPopState.bind(this);
-		this.state = global.pageRouterData ? this.loadPageData(global.pageRouterData) : {tokens:{}};
-	}
-	
-	makeRequest(){
-		return webRequest("page/list").then(response => {
-			var pages = response.json.results;
-			this.setState(this.loadPageData(pages));
-		}).catch(err => {
-			// No additional pages are available.
-			console.log(err);
-		})
-	}
-
-	loadPageData(pages){
-		// Build route map and ID maps next.
-		var contentMap = {};
-		var adminContentMap = {};
-		var rootPage = {children: {}, tokenNames: []};
-		
-		pages.forEach(page => {
-			
-			var url = page.url;
-			
-			if(!url){
-				return;
-			}
-
-			// Does our url contain '{' ?
-			if(url.indexOf("{") != -1) {
-				// We need to handle it as a content type. Let's get the type and piecesAfter value. 
-				var urlExploded = url.split("/");
-
-				var contentType = null;
-				var parameter = null;
-				var piecesAfter = 0;
-
-				// Let's loop backwards through our exploded url
-				for (var i = urlExploded.length -1; i >= 0; i--) {
-					var piece = urlExploded[i];
-
-					// Is our piece the content?
-					if(piece.charAt(0) == "{" && piece.charAt(piece.length-1) == "}") {
-						// yes, shed the curlies.
-						contentAndParam = piece.slice(1, -1);
-
-						// Let's split on the "." now.
-						contentAndParamExplode = contentAndParam.split(".");
-						
-						// do we have two pieces? If not, this is not right.
-						if(contentAndParamExplode.length == 2) {
-							// good to go. 
-							// what is the content type and param?
-							contentType = contentAndParamExplode[0].toLowerCase();
-							parameter = contentAndParamExplode[1].toLowerCase();
-							
-							//Nothing else needed in this loop now.
-							break;
-						}
-					}
-
-					piecesAfter++;
-				}
-
-				// Did we get a content type and param out of this?
-				if(contentType && parameter) {
-					
-					// What's it's scope?
-					if(url.includes("/en-admin/")) {
-						// The scope is admin - do we have an existing entry for this content?
-						if(!adminContentMap[contentType] || adminContentMap[contentType].piecesAfter > piecesAfter) {
-							adminContentMap[contentType] = {
-								parameter: parameter,
-								piecesAfter: piecesAfter,
-								page: page,
-								url: url
-							};
-						}
-					} else {
-						// For now, we will assume the scope is the UI otherwise.
-						if(!contentMap[contentType] || contentMap[contentType].piecesAfter > piecesAfter) {
-							contentMap[contentType] = {
-								parameter: parameter,
-								piecesAfter: piecesAfter,
-								page: page,
-								url: url
-							};
-						} 
-					}
-				}
-			}
-
-			
-			if(url.length && url[0] == '/'){
-				url = url.substring(1);
-			}
-			
-			if(url.length && url[url.length-1] == '/'){
-				url = url.substring(0, url.length-1);
-			}
-			
-			page.url = url;
-			
-			// URL parts:
-			
-			var pg = rootPage;
-			
-			if(url.length){
-				var parts = url.split('/');
-				
-				for(var i=0;i<parts.length;i++){
-					
-					var part = parts[i];
-					var token;
-					if (part.length)
-					{
-						if (part[0] == ':')
-						{
-							token = part.substring(1);
-						}
-						else if (part[0] == '{')
-						{
-							token = (part[part.length - 1] == '}') ? part.substring(1, part.length - 1) : part.substring(1);
-						}
-					}
-
-					if(token){
-						// Anything. Treat these tokens as *:
-						part = '*';
-					}
-
-					var next = pg.children[part];
-					
-					if(!next){
-						pg.children[part] = next = {children: {}, tokenNames: []};
-						if(token){
-							next.tokenNames.push(token);
-						}
-					}
-					
-					pg = next;
-				}
-			}
-			
-			pg.page = page;
-		});
-		
-		var pageInfo = this.getPage(rootPage, this.props.url);
-		this.trigger(pageInfo);
-		
-		return {
-			pages,
-			rootPage,
-			contentMap,
-			adminContentMap,
-			...pageInfo
-		};
+		this.state = context.pgState || global.pgState;
+		this.trigger(this.state.page);
 	}
 	
 	onLinkClick(e){
@@ -199,7 +40,6 @@ export default class PageRouter extends React.Component{
 					if(href.indexOf(':') != -1 || (href[0] == '/' && (href.length>1 && href[1] == '/'))){
 						return;
 					}
-					
 					if(targetIsExternPage == isOnExternPage){
 						e.preventDefault();
 						this.go(cur.pathname + cur.search);
@@ -217,21 +57,8 @@ export default class PageRouter extends React.Component{
 	}
 	
 	componentDidMount(){
-		var {loadingUser} = this.context.app.state;
-		
-		if(loadingUser){
-			loadingUser.then(res => {
-				this.role = res && res.json.role ? res.json.role.id : undefined;
-			})
-		}
-		
 		global.addEventListener("popstate", this.onPopState);
-		
 		document.addEventListener("click", this.onLinkClick);
-		
-		if(!this.state.pages){
-			this.makeRequest();
-		}
 	}
 	
 	trigger(pgInfo){
@@ -248,150 +75,9 @@ export default class PageRouter extends React.Component{
 		}
 	}
 	
-	getRole(){
-		var { role } = this.context.app.state;
-		return role ? role.id : undefined;
-	}
-	
-	componentWillReceiveProps(props){
-		var role = this.getRole();
-		if(this.role!==undefined && role!==undefined && this.role != role){
-			this.role = role;
-			// User role changed (they logged in) - get page list again, then change page:
-			this.makeRequest().then(() => this.setupPage(props));
-		}else{
-			// Only setup if page actually changed.
-			if(props.url != this.props.url){
-				this.setupPage(props);
-			}
-		}
-	}
-	
-	setupPage(props){
-		// page and tokens
-		this.getPageLazy(this.state.rootPage, props.url).then((pageInfo) => {
-			this.trigger(pageInfo);
-			this.setState(pageInfo);
-		});
-	}
-	
 	componentWillUnmount(){
 		global.removeEventListener("popstate", this.onPopState);
 		document.removeEventListener("click", this.onLinkClick);
-	}
-	
-	getPageLazy(rootPage, url){
-		// The same as getPage only this one also resolves redirect: PageId.
-		return new Promise((success, reject) => {
-			var pageAndState = this.getPage(rootPage, url);
-			
-			if(!pageAndState || !pageAndState.page){
-				// Redirect to 404:
-				pageAndState = this.getPage(rootPage, this.props.notFound || '/404');
-			}
-			
-			var {page} = pageAndState;
-			
-			if(!page || !page.id){
-				return success(pageAndState);
-			}
-			
-			if(page.createdUtc == undefined){
-				// Page not loaded yet - get it now:
-				Content.get("page", page.id).then(response => {
-					for(var field in response){
-						page[field] = response[field];
-					}
-					
-					var url = page.url;
-					
-					if(url.length && url[0] == '/'){
-						url = url.substring(1);
-					}
-					
-					if(url.length && url[url.length-1] == '/'){
-						url = url.substring(0, url.length-1);
-					}
-					
-					page.url = url;
-					success(pageAndState);
-				});
-			}else{
-				success(pageAndState);
-			}
-		});
-	}
-	
-	getPage(rootPage, url){
-		url = url.split('?')[0].trim();
-		
-		if(url[0] == '/'){
-			url=url.substring(1);
-		}
-
-		if(url[url.length-1] == '/'){
-			url=url.substring(0,url.length-1);
-		}
-		
-		var prefix = global.urlPrefix;
-		
-		if(prefix){
-			if(prefix[0] == '/'){
-				prefix = prefix.substring(1);
-			}
-			
-			if(url.substring(0,prefix.length).toLowerCase() == prefix.toLowerCase()){
-				url = url.substring(prefix.length);
-				
-				if(url[0] == '/'){
-					url=url.substring(1);
-				}
-			}
-		}
-		
-		if(global.mapUrl){
-			url = global.mapUrl(url);
-		}
-		
-		var curNode = rootPage;
-		
-		if(!curNode){
-			return null;
-		}
-		
-		var tokens = {};
-		
-		if(url.length){
-			var parts = url.split('/');
-
-			for(var i=0;i<parts.length;i++){
-				var nextNode = curNode.children[parts[i]];
-
-				if(!nextNode){
-					nextNode = curNode.children['*'];
-					
-					if(nextNode && nextNode.tokenNames.length){
-						// handles e.g. :token or {token} in the URL - sets the named token into the URL ctx.
-						// Note that one URL segment can be known by multiple token names.
-						var tNames= nextNode.tokenNames;
-						
-						for(var e=0;e<tNames.length;e++){
-							tokens[tNames[e]] = parts[i];
-						}
-					}
-				}
-				
-				if(!nextNode){
-					return null;
-				}
-				curNode = nextNode;
-			}
-		}
-
-		return {
-			page: curNode.page,
-			tokens
-		};
 	}
 	
 	render(){
@@ -401,7 +87,11 @@ export default class PageRouter extends React.Component{
 		</Canvas> : null;
 	}
 	
-	componentDidUpdate(){
+	componentDidUpdate(oldProps){
+		if(oldProps.url != this.props.url){
+			webRequest("page/state", {url:this.props.url, version: 1}).then(res => this.setState(res.json));
+		}
+		
 		var {page} = this.state;
 		if(page && page.title){
 			document.title = page.title;
