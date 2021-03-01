7c16bdb5b8e5e60fc7678290dff9823d57daeeff
Luke Briggs <luke@kulestar.com>
2021-03-01 03:46:48 +0000

    Added support for main.js from the new build system

--- GetBuildDate.js
+++ GetBuildDate.js
@@ -4,26 +4,35 @@ var buildDate = null;
 * Reads the build date whenever it's available.
 * It originates from the main.generated?v=X number (where X is actually a UTC unix timestamp in ms)
 */
-module.exports = () => {
+export default () => {
 	if(!buildDate){
 		var scr = global.document.scripts;
 		for(var i=0;i<scr.length;i++){
 			var src = scr[i].src;
 			
-			if(!src || src.indexOf("generated.js") == -1){
+			if(!src || (src.indexOf("/main.js?") == -1 && src.indexOf("/main.generated.js?") == -1)){
 				continue;
 			}
 			
-			var versionParts = src.split('?v=');
-			if(versionParts.length == 2){
-				var timestamp = parseInt(versionParts[1]);
-				var date = new Date(timestamp);
-				buildDate = {
-					date,
-					dateString: date.toUTCString(),
-					timestamp
-				};
+			var versionParts = src.split('?');
+			var vp = versionParts[1].split('&');
+			var map = {};
+			for(var n=0;n<vp.length;n++){
+				var entry = vp[n].split('=');
+				if(entry.length == 2){
+					map[entry[0]] = entry[1];
+				}
 			}
+			
+			var timestamp = parseInt(map['v']);
+			var date = new Date(timestamp);
+			buildDate = {
+				date,
+				hash: map['h'],
+				dateString: date.toUTCString(),
+				timestamp
+			};
+			break;
 		}
 	}
 	
