3411af24d6c41b7f6c118d1d5310e1d089ceac24
Conan Ablewhite <conan.ablewhite@4-roads.com>
2021-03-01 03:53:16 +0000

    horizontalAlignment option added to Row component

--- Row.js
+++ Row.js
@@ -4,9 +4,23 @@ import omit from 'UI/Functions/Omit';
  * A responsive row.
  */
 
-export default function Row (props) {
+export default function Row(props) {
+	var rowClass = "row ";
+
+	if (props.noGutters) {
+		rowClass += "no-gutters ";
+	}
+
+	if (props.horizontalAlignment) {
+		rowClass += "justify-content-" + props.horizontalAlignment + " ";
+	}
+
+	if (props.className) {
+		rowClass += props.className;
+	}
+
 	return <div 
-		className={"row " + (props.noGutters ? "no-gutters" : "") + " " + (props.className || '')}
+		className={rowClass}
 		{...(omit(props, ['className', 'noGutters', 'children', '__canvas']))}
 	>
 		{props.children}
@@ -15,6 +29,14 @@ export default function Row (props) {
 
 Row.propTypes={
 	noGutters: 'boolean',
+	horizontalAlignment: [
+		{ name: 'No preference', value: '' },
+		{ name: 'Align left', value: 'start' },
+		{ name: 'Align centre', value: 'center' },
+		{ name: 'Align right', value: 'end' },
+		{ name: 'Distribute space around', value: 'around' },
+		{ name: 'Distribute space between', value: 'between' }
+	],
 	children: {default: [{module: "UI/Column", content: "Column 1"}, {module: "UI/Column", content: "Column 2"}]}
 };
 
