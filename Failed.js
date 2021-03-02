4c37883ca48f6f8d99177167e036a77b373c6ae4
Michael Rogers <michaelr@conceptsoftworks.com>
2021-03-02 11:50:14 -0700

    Localization changes.

--- Failed.js
+++ Failed.js
@@ -2,7 +2,7 @@ export default function Failed () {
 	return(
 		<div className="alert alert-danger" role="alert" style = {{textAlign: "center"}}>
 			<i className="fad fa-wifi-slash" />
-			<p>The service is currently unavailable. This may be because your device is currently offline.</p>
+			<p>{`The service is currently unavailable. This may be because your device is currently offline.`}</p>
 		</div>
 	);
 }
