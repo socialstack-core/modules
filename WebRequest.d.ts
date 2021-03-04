5e89c3334f68d0a7daf3f3dbcc2efe479f447136
Andi Emma Davies <andiemmadavies@outlook.com>
2021-03-04 23:00:15 +0000

    Add TypeScript type definitions file

---
+++ WebRequest.d.ts
@@ -0,0 +1,9 @@
+﻿export default function webRequest(
+    origUrl: string,
+    data?: FormData | unknown,
+    opts?: object & {
+        blob?: unknown;
+        method?: string;
+        locale?: unknown;
+    }
+): Promise<unknown>;
