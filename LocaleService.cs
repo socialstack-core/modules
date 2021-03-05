d7a936451012ffe2f7d96ce298b6539a34790775
Luke Briggs <luke@kulestar.com>
2021-03-05 16:33:23 +0000

    Added handling of Accept-Language, enabled by default.

--- LocaleService.cs
+++ LocaleService.cs
@@ -7,6 +7,9 @@ using Api.Contexts;
 using Api.DatabaseDiff;
 using System;
 using Api.Startup;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using System.Linq;
 
 namespace Api.Translate
 {
@@ -44,7 +47,100 @@ namespace Api.Translate
 
 				}
 			});
+
+			var cfg = GetConfig<LocaleServiceConfig>();
+
+			Events.ContextAfterAnonymous.AddEventListener(async (Context context, Context result, HttpRequest request) =>
+			{
+				if (cfg.HandleAcceptLanguageHeader && result != null)
+				{
+					// Identify most suitable locale from the accept-lang header.
+					StringValues acceptLangs;
+
+					// Could also handle Accept-Language here. For now we use a custom header called Locale (an ID).
+					if (request.Headers.TryGetValue("Accept-Language", out acceptLangs) && !string.IsNullOrEmpty(acceptLangs))
+					{
+						// Locale header is set:
+						var acceptLanguageHeader = acceptLangs.FirstOrDefault();
+
+						var langsUpTo = acceptLanguageHeader.IndexOf(';');
+						var index = 0;
+
+						while (index < langsUpTo)
+						{
+							var next = acceptLanguageHeader.IndexOf(',', index + 1);
+
+							if (next > langsUpTo)
+							{
+								break;
+							}
+
+							if (next == -1 || next > langsUpTo)
+							{
+								next = langsUpTo;
+							}
+
+							string langCode = acceptLanguageHeader.Substring(index, next - index);
+
+							var localeId = await GetId(langCode);
+
+							if (localeId.HasValue)
+							{
+								context.LocaleId = localeId.Value;
+								break;
+							}
+
+							index = next + 1;
+						}
+
+					}
+				}
+
+				return result;
+			});
+
 		}
+
+		/// <summary>
+		/// Gets locale ID by its case insensitive code. Can contain hyphens, such as "en-gb".
+		/// </summary>
+		/// <param name="localeCode"></param>
+		/// <returns>null if not found.</returns>
+		public async ValueTask<int?> GetId(string localeCode)
+		{
+			if (_codeMap == null)
+			{
+				var all = await List(new Context(), new Filter<Locale>());
+
+				var map = new Dictionary<string, int>();
+
+				foreach (var locale in all)
+				{
+					if (string.IsNullOrEmpty(locale.Code))
+					{
+						continue;
+					}
+
+					// Primary locale code (may have an aliases field in the future):
+					var mainCode = locale.Code.Trim().ToLower();
+					map[mainCode] = locale.Id;
+				}
+
+				_codeMap = map;
+			}
+
+			if (!_codeMap.TryGetValue(localeCode, out int result))
+			{
+				return null;
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// A mapping of locale code -> ID. Uses IDs such that it does not need locale specific variations.
+		/// </summary>
+		private Dictionary<string, int> _codeMap;
 
 		/// <summary>
 		/// The name of the cookie when locale is stored.
---
+++ LocaleServiceConfig.cs
@@ -0,0 +1,18 @@
+
+using Api.Configuration;
+
+namespace Api.Translate
+{
+	/// <summary>
+	/// Config for locale service.
+	/// </summary>
+	public class LocaleServiceConfig : Config
+	{
+		/// <summary>
+		/// True (default) if the accept-lang header will be used to 
+		/// establish default language when a user is seen for the first time.
+		/// </summary>
+		public bool HandleAcceptLanguageHeader {get; set;} = true;
+	}
+	
+}
