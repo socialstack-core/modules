a7f8dc954482d9efdfdcd457cf92ac2692d072f4
Luke Briggs <luke@kulestar.com>
2021-03-16 12:31:55 +0000

    Added comments

--- Column.d.ts
+++ Column.d.ts
@@ -12,12 +12,27 @@ interface ColumnProps {
 	offsetSm?: "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12";
 	offsetMd?: "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12";
 	offsetLg?: "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12";
+	
+	/**
+		An amount, in bootstrap grid columns, to offset your column by.
+	*/
 	offsetXl?: "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12";
+	
+	/**
+		Optionally add a specific type of margin to your column. See the bootstrap docs for what the available options do.
+	*/
 	margin?: "" | "ml-auto" | "mr-auto" | "mx-auto";
+	
+	/**
+		Set this to true if you don't want gutters on your column.
+	*/
 	noGutters?: boolean;
 
 }
 
+/**
+	A column for a flexbox based bootstrap grid. Use inside a Row.
+*/
 export default function Column(
     props: React.PropsWithChildren<ColumnProps>
 ): React.ReactElement;
