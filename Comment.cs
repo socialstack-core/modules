d7cd7cb929c9547701c67c0f7ff954d7dd8c8287
Michael Rogers <michaelr@conceptsoftworks.com>
2021-03-16 16:57:14 -0700

    BodyJson is now Body since it wasn't being used like a json object.

--- Comment.cs
+++ Comment.cs
@@ -66,7 +66,7 @@ namespace Api.Comments
 		/// and be formatted in complex ways.
 		/// </summary>
 		// [DatabaseField(Length = 2000)]
-		public string BodyJson;
+		public string Body;
 		/// <summary>
 		/// Reactions to this comment (typically upvote/ downvote).
 		/// </summary>
