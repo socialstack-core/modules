ca230f8bbb174ae8d384ebfebb151dcfcafc6cd3
Luke Briggs <luke@kulestar.com>
2021-03-17 14:27:06 +0000

    Added general use email install mechanism

--- AutoService.cs
+++ AutoService.cs
@@ -1180,6 +1180,47 @@ public partial class AutoService
 	}
 
 	/// <summary>
+	/// Installs one or more email templates.
+	/// Schedules the install to happen either immediately if services have not yet started (async) or after services have started.
+	/// </summary>
+	/// <param name="templates"></param>
+	public void InstallEmails(params Api.Emails.EmailTemplate[] templates)
+	{
+		if (Services.Started)
+		{
+			InstallEmailsInternal(templates);
+		}
+		else
+		{
+			// Must happen after services start otherwise the email template service isn't necessarily ready yet.
+			Events.ServicesAfterStart.AddEventListener((Context ctx, object src) =>
+			{
+				InstallEmailsInternal(templates);
+				return new ValueTask<object>(src);
+			});
+		}
+	}
+
+	private void InstallEmailsInternal(Api.Emails.EmailTemplate[] templates)
+	{
+		var emailService = Services.Get<Api.Emails.EmailTemplateService>();
+
+		if (emailService == null)
+		{
+			return;
+		}
+
+		Task.Run(async () =>
+		{
+			foreach (var template in templates)
+			{
+				await emailService.InstallNow(template);
+			}
+		});
+
+	}
+	
+	/// <summary>
 	/// Defines a new IHave* interface.
 	/// It's added to content types to declare they have e.g. an array of tags, categories etc.
 	/// This one specifically is where the mapping type is also the array entries.
