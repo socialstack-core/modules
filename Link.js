2beed890f39db9e67d1c74dabd2a9c066275f9eb
Luke Briggs <luke@kulestar.com>
2021-03-22 22:54:59 +0000

    Simplified and added support for the RTE

--- Link.js
+++ Link.js
@@ -1,57 +1,47 @@
 import omit from 'UI/Functions/Omit';
 
-export default class Link extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+export default function Link (props) {
+	var attribs = omit(props, ['children', 'href', '_rte']);
+	attribs.alt = attribs.alt || attribs.title;
 	
-    render() {
-		var attribs = omit(this.props, ['text', 'url', 'children']);
-		attribs.alt = attribs.alt || attribs.title;
+	var children = props.children || props.text;
+	var url = (props.url || props.href);
+	
+	if(url){
+		// if url contains :// it must be as-is (which happens anyway).
+		if(url[0] == '/'){
+			if(url.length>1 && url[1] == '/'){
+				// as-is
+			}else{
+				var prefix = window.urlPrefix || '';
 
-		var url = (this.props.url || this.props.href);
-		
-		if(url){
-			// if url contains :// it must be as-is (which happens anyway).
-			if(url[0] == '/'){
-				if(url.length>1 && url[1] == '/'){
-					// as-is
-				}else{
-					var prefix = window.urlPrefix || '';
+				if(prefix) {
+					if(prefix[0] != '/'){
+						// must return an absolute url
+						prefix = '/' + prefix;
+					 }
 
-					if(prefix) {
-						if(prefix[0] != '/'){
-							// must return an absolute url
-							prefix = '/' + prefix;
-						 }
-
-						if(prefix[prefix.length - 1] == "/") {
-							url = url.substring(1);
-						}
-			
-						url = prefix + url;
+					if(prefix[prefix.length - 1] == "/") {
+						url = url.substring(1);
 					}
+		
+					url = prefix + url;
 				}
 			}
 		}
-
-		if(this.props.text){
-			return <a href={url} 
-				dangerouslySetInnerHTML={{__html: (this.props.text)}}
-				{...attribs}
-			/>;
-		}else{
-			return <a href={url}
-				{...attribs}
-				>
-					{this.props.children}
-			</a>;
-		}
-    }
+	}
+	
+	return <a href={url}
+		{...attribs}
+		>
+			{children}
+	</a>;
 }
 
+Link.editable = true;
+
 Link.propTypes = {
-	text: 'string',
 	title: 'string',
-	url: 'string'
+	href: 'string',
+	children: 'jsx'
 };
