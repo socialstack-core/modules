980990cd24519599be5d8979aa7006364db72235
Luke Briggs <luke@kulestar.com>
2021-03-26 22:16:06 +0000

    Renamed to Content

--- StaffMember.cs
+++ StaffMember.cs
@@ -10,7 +10,7 @@ namespace Api.StaffMembers
 	/// <summary>
 	/// A Staff Member that isn't a user - used for e.g. staff lists.
 	/// </summary>
-	public partial class StaffMember : RevisionRow
+	public partial class StaffMember : VersionedContent<int>
 	{
 		/// <summary>
 		/// The first name of the Staff Member (and any middle names).
