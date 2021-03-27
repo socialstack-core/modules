8b155b38734c358b97fed5b4910c86fbbb31edd1
Michael Rogers <michaelr@conceptsoftworks.com>
2021-03-26 19:59:02 -0700

    Entity changes

--- Connection.cs
+++ Connection.cs
@@ -9,7 +9,7 @@ namespace Api.Connections
 	/// <summary>
 	/// A user following (or subscribed to) some other user.
 	/// </summary>
-	public partial class Connection : RevisionRow
+	public partial class Connection : VersionedContent<int>
 	{
 		/// <summary>
 		/// The user id this (creator) user is subscribed to.
