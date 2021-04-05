ba50f562410005f29bbf7dc1bf535711cb72fbda
Luke Briggs <luke@kulestar.com>
2021-04-05 20:03:54 +0100

    Default ID type is uint

--- Template.cs
+++ Template.cs
@@ -9,7 +9,7 @@ namespace Api.Templates
 	/// <summary>
 	/// A template.
 	/// </summary>
-	public partial class Template : VersionedContent<int>
+	public partial class Template : VersionedContent<uint>
 	{
 		/// <summary>
 		/// A key used to identify a template by its purpose.
