7d0b7d5ea41c84db2ea35a9d9d6656e987617b1f
Luke Briggs <luke@kulestar.com>
2021-04-06 05:16:37 +0100

    Changed to uint

--- Follower.cs
+++ Follower.cs
@@ -9,12 +9,12 @@ namespace Api.Followers
 	/// <summary>
 	/// A user following (or subscribed to) some other user.
 	/// </summary>
-	public partial class Follower : RevisionRow
+	public partial class Follower : VersionedContent<uint>
 	{
 		/// <summary>
 		/// The user id this (creator) user is subscribed to.
 		/// </summary>
-		public int SubscribedToId;
+		public uint SubscribedToId;
 	}
 
 }
