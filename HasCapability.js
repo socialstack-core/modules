ebb0dbcd80bbadf99828c8b4f8a89dff1e05ff64
Luke Briggs <luke@kulestar.com>
2021-04-07 05:16:15 +0100

    Changed global state

--- HasCapability.js
+++ HasCapability.js
@@ -1,46 +1,21 @@
 import hasCapability from 'UI/Functions/HasCapability';
+import { useSession } from 'UI/Session';
 
 /*
 Displays its content only if the named capability is actually granted.
 */
-export default class HasCapability extends React.Component {
+export default props => {
 	
-	constructor(props){
-		super(props);
-		
-		this.state={
-			name: props.called,
-			granted: false
-		};
-	}
+	const [granted, setGranted] = React.useState(false);
+	const { session } = useSession();
 	
-	componentDidMount(){
-		if(props.called){
-			hasCapability(props.called, this.context).then(granted => this.setState({granted}));
-		}
-	}
+	React.useEffect(() => {
+		hasCapability(props.called, session).then(grant => {
+			grant != granted && setGranted(grant);
+		});
+	});
 	
-	componentWillReceiveProps(props){
-		if(props.called == this.state.name){
-			return;
-		}
-		
-		this.setState({name: props.called});
-		hasCapability(props.called).then(granted => this.setState({granted}));
-	}
-	
-	render(){
-		var g = this.state.granted;
-		this.props.invert && (g=!g);
-		return g ? this.props.children : null;
-	}
-	
+	var g = granted;
+	props.invert && (g=!g);
+	return g ? props.children : null;
 }
-
-HasCapability.propTypes = {
-	called: 'string',
-	invert: 'bool',
-	children: true
-};
-
-HasCapability.icon = 'times-circle';
