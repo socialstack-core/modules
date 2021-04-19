13907d2a73137c34a407b2e7569048f37574ffeb
Luke Briggs <luke@kulestar.com>
2021-04-19 04:07:38 +0100

    Addition of includes handling

--- WebRequest.js
+++ WebRequest.js
@@ -2,6 +2,62 @@
 import store from 'UI/Functions/Store';
 import contentChange from 'UI/Functions/ContentChange';
 
+function expandIncludes(response){
+	
+	var {result, results, includes} = response;
+	
+	if(includes){
+		for(var i=includes.length-1;i>=0;i--){
+			var inc = includes[i];
+			var targetValues = inc.on === undefined ? (results || [result]) : includes[inc.on].values;
+			
+			if(inc.src){
+				// Source field in the original object has the ID. Use that:
+				var byIdMap = {};
+				inc.values.forEach(v => byIdMap[v.id] = v);
+				targetValues.forEach(val => {
+					val[inc.field] = byIdMap[val[inc.src]];
+				});
+			}else if(typeof inc.map == 'string'){
+				// Each object in values has a field of the given name, which contains an ID that maps to an object in _targetValues_.
+				var targetIdMap = {};
+				targetValues.forEach(v => {
+					targetIdMap[v.id] = v;
+					v[inc.field] = [];
+				});
+				inc.values.forEach(v => {
+					var target = targetIdMap[v[inc.map]];
+					if(target){
+						target[inc.field].push(v);
+					}
+				});
+			}else if(inc.map){
+				// It's an array of tuples. The first is a source ID, second is target ID.
+				var targetIdMap = {};
+				targetValues.forEach(v => {
+					targetIdMap[v.id] = v;
+					v[inc.field] = [];
+				});
+				var srcIdMap = {};
+				inc.values.forEach(v => srcIdMap[v.id] = v);
+				for(var n=0;n<inc.map.length;n+=3){
+					var id = inc.map[n]; // the mapping entry ID.
+					var a = srcIdMap[inc.map[n+1]];
+					var b = targetIdMap[inc.map[n+2]];
+					
+					if(a && b){
+						// todo: only push a if id not already in set.
+						// where should id be stored though? (it's not the same thing as a.id - it's the actual mapping row's id).
+						b[inc.field].push(a);
+					}
+				}
+			}
+		}
+	}
+	
+	return response.result ? response.result : response;
+}
+
 export default function webRequest(origUrl, data, opts) {
 	var url = (global.apiHost || '') + '/v1/' + origUrl;
 	
@@ -43,9 +99,9 @@ export default function webRequest(origUrl, data, opts) {
                     reject(json || { error: 'invalid response' });
 					return;
                 }
-
+				
 				// Run success now:
-				response.json = json;
+				response.json = expandIncludes(json);
 				
 				// If we're not blocked from doing so with the options
 				// and this was either a DELETE or POST request which returned an entity
@@ -113,7 +169,13 @@ function _fetch(url, data, opts) {
 		});
 	}
 	
-	return fetch(url, {
+	var includes = opts && opts.includes;
+	
+	if(Array.isArray(includes)){
+		includes = includes.map(x=>x.trim()).join(',');
+	}
+	
+	return fetch(url + (includes ? '?includes=' + includes : ''), {
 		method: opts && opts.method ? opts.method : 'post',
 		headers: {
 			'Accept': 'application/json',
