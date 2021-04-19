d6dce2b108dd1af9b3792f3ef80eab9d5619e819
Luke Briggs <luke@kulestar.com>
2021-04-19 18:01:34 +0100

    Mapping entities can now be used directly, opening up the mapping API for easier usage

--- MappingEntity.cs
+++ MappingEntity.cs
@@ -48,7 +48,7 @@ namespace Api.Database
 	/// If it's e.g. a list of tags on a user, it has a UserId field and a TagId field, which are linked up to SourceId and TargetId.
 	/// These are fully automated and used via the include system.
 	/// </summary>
-	public abstract class Mapping<S,T> : Content<uint> 
+	public class Mapping<S,T> : Content<uint> 
 		where S : struct
 		where T : struct
 	{
