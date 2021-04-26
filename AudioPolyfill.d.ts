1fc531282f5cb320fbf755815443fbc98475c31a
Andi Emma Davies <andiemmadavies@outlook.com>
2021-04-26 12:00:20 +0100

    Add TypeScript type declarations file

---
+++ AudioPolyfill.d.ts
@@ -0,0 +1,17 @@
+﻿interface MediaRecorderProps { onData?(data: unknown): void; }
+
+declare class MediaRecorder {
+    constructor(stream: MediaStream, props?: MediaRecorderProps);
+
+    writeHeader(): void;
+    start(timeslice: number): void | undefined;
+    addData(e: { length: number; data: unknown[]; }): void;
+    getWav(): Uint8Array;
+    stop(): void;
+    onstopped(): void;
+    mimeType: string;
+    isTypeSupported(mimeType: string): boolean;
+    notSupported: boolean;
+}
+
+export default MediaRecorder;
