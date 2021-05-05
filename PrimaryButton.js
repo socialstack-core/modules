a73e6f9e5da81a66ca24a1520c23ba2b6abe030a
Luke Briggs <luke@kulestar.com>
2021-05-06 00:51:45 +0100

    Now supports both context and custom data passed via primary object

--- PrimaryButton.js
+++ PrimaryButton.js
@@ -1,49 +1,47 @@
 import applyTokens from 'Email/Functions/ApplyTokens';
-import {SessionConsumer} from 'UI/Session';
-
+import Content from 'UI/Content';
+import { useSession } from 'UI/Session';
 
 /*
 * A large, centered button.
 * Target can include {context.tokens} where 
 */
 
-export default class PrimaryButton extends React.Component {
+export default function PrimaryButton (props) {
 	
-	render(){
-		return <SessionConsumer>
-			{session => this.renderIntl(session)}
-		</SessionConsumer>
-	}
-
-	renderIntl(session){
-		var { origin } = global.location;
-		var target = this.props.target;
-		if(!target){target = '';}
-		
-		// Apply {tokens}:
-		target = applyTokens(target, session);
-		
-		if(target.indexOf('http') != 0){
-			if(target.length && target[0] == '/'){
-				target = origin + target;
-			}else{
-				target = origin + '/' + target;
+	var {session} = useSession();
+	
+	return <Content primary>
+		{content => {
+			
+			var { origin } = global.location;
+			var target = props.target;
+			if(!target){target = '';}
+			
+			// Apply {tokens}:
+			target = applyTokens(target, session, content);
+			
+			if(target.indexOf('http') != 0){
+				if(target.length && target[0] == '/'){
+					target = origin + target;
+				}else{
+					target = origin + '/' + target;
+				}
 			}
-		}
-		
-		return <table role="presentation" style="margin: auto;" cellspacing="0" cellpadding="0" border="0" align="center">
-				<tbody>
-					<tr>
-						<td class="button-td button-td-primary" style="border-radius: 4px; background: #222222;">
-							 <a class="button-a button-a-primary" href={target} style="background: #222222; border: 1px solid #000000; font-family: sans-serif; font-size: 15px; line-height: 15px; text-decoration: none; padding: 13px 17px; color: #ffffff; display: block; border-radius: 4px;">
-								{this.props.label || 'Click here'}
-							 </a>
-						</td>
-					</tr>
-				</tbody>
-		</table>;
-		
-	}
+			
+			return <table role="presentation" style="margin: auto;" cellspacing="0" cellpadding="0" border="0" align="center">
+					<tbody>
+						<tr>
+							<td class="button-td button-td-primary" style="border-radius: 4px; background: #222222;">
+								 <a class="button-a button-a-primary" href={target} style="background: #222222; border: 1px solid #000000; font-family: sans-serif; font-size: 15px; line-height: 15px; text-decoration: none; padding: 13px 17px; color: #ffffff; display: block; border-radius: 4px;">
+									{props.label || 'Click here'}
+								 </a>
+							</td>
+						</tr>
+					</tbody>
+			</table>;
+		}}
+	</Content>;
 	
 }
 
