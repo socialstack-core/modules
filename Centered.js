a4877293ac11e5bcf08ccb2231514fa313375b87
Luke Briggs <luke@kulestar.com>
2021-05-17 12:02:30 +0100

    Made compatible with regular react

--- Centered.js
+++ Centered.js
@@ -1,10 +1,10 @@
 export default class Centered extends React.Component {
 	
 	render(){
-		return <table border="0" cellpadding="0" cellspacing="0" class="content" style="width: 100%; border-collapse: separate; border-spacing: 0;">
+		return <table border="0" cellpadding="0" cellspacing="0" class="content" style={{width: "100%", borderCollapse: "separate", borderSpacing: "0"}}>
 			<tbody>
 				<tr>
-					<td class="text-content" style="font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; color: #333333; font-size: 15px; font-weight: 400; line-height: 1.4; padding: 15px 5px;" align="center">
+					<td class="text-content" style={{fontFamily: "&quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif", color: "#333333", fontSize: "15px", fontWeight: "400", lineHeight: "1.4", padding: "15px 5px"}} align="center">
 						{this.props.children}
 					</td>
 				</tr>
