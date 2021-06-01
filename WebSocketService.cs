4563ec79f08d657fe9a7749f9048c4099e6d8838
Luke Briggs <luke@kulestar.com>
2021-06-01 16:14:13 +0100

    All basic services now AutoServices

--- WebSocketService.cs
+++ WebSocketService.cs
@@ -23,7 +23,7 @@ namespace Api.WebSockets
 	/// Handles creations of galleries - containers for image uploads.
 	/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 	/// </summary>
-	public partial class WebSocketService
+	public partial class WebSocketService : AutoService
     {
 
 		private readonly ContextService _contextService;
