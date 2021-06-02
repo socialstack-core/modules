d4489ad376665263f90ab80d38b4ea803e0f7658
Luke Briggs <luke@kulestar.com>
2021-06-03 00:40:00 +0100

    Multiple ListAs attributes can now be used

--- ListAsAttribute.cs
+++ ListAsAttribute.cs
@@ -6,7 +6,7 @@ namespace Api.Startup
 	/// Add [ListAs("fieldName")] attributes to your type to declare a virtual field with the given name on all other types. The field contains a list of your type.
 	/// If the target type has a name of YourContentTypeId, the list will be based on that. Otherwise, a mapping type will be internally created and used.
 	/// </summary>
-	[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+	[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
 	internal sealed class ListAsAttribute : Attribute
 	{
 		/// <summary>
