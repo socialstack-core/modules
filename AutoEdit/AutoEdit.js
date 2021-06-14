f4233db014c78a32be49bba44fb353ab24da585b
Luke Briggs <luke@kulestar.com>
2021-06-14 12:46:02 +0100

    Directly added urlToken mapping support

--- AutoEdit/AutoEdit.js
+++ AutoEdit/AutoEdit.js
@@ -1,5 +1,6 @@
 import Tile from 'Admin/Tile';
 import Loop from 'UI/Loop';
+import {TokenResolver} from 'UI/Token';
 import AutoForm from 'Admin/AutoForm';
 import Default from 'Admin/Layouts/Default';
 import omit from 'UI/Functions/Omit';
@@ -12,19 +13,28 @@ export default class AutoEdit extends React.Component {
 	}
 	
 	render(){
-		return (
-			<Default>
-				<AutoForm {...(omit(this.props, ['children']))} />
-				{this.props.children}
-			</Default>	
-		);
+		var id = this.props.id;
+		
+		// legacy support - mapping old urlTokens:
+		if(typeof id != 'string' && id.type == 'urlToken'){
+			id = '${url.' + id.name + '}';
+		}
+		
+		return <TokenResolver value={id}>
+			{id => 
+				<Default>
+					<AutoForm {...(omit(this.props, ['children']))} id={id} />
+					{this.props.children}
+				</Default>	
+			}
+		</TokenResolver>;
 	}
 	
 }
 
 AutoEdit.propTypes = {
 	children: true,
-	id: 'string',
+	id: 'token',
 	endpoint: 'string',
 	deletePage: 'string'
 };
