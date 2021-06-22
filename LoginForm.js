076f673258d6eceec1a03cf6480d7da81b3e470f
Luke Briggs <luke@kulestar.com>
2021-06-22 22:15:40 +0100

    Added general use MFA mechanism into this loginform, migrated from the admin specific one

--- LoginForm.js
+++ LoginForm.js
@@ -2,6 +2,7 @@ import Input from 'UI/Input';
 import Form from 'UI/Form';
 import Row from 'UI/Row';
 import Col from 'UI/Column';
+import Canvas from 'UI/Canvas';
 import Spacer from 'UI/Spacer';
 import Alert from 'UI/Alert';
 import { useSession, useRouter } from 'UI/Session';
@@ -10,11 +11,12 @@ import { useSession, useRouter } from 'UI/Session';
  * Frontend login form.
  */
 
-export default function LoginForm (props) {
-	const {emailOnly} = props;
+export default props => {
 	const { setSession } = useSession();
 	const { setPage } = useRouter();
-	const [failed,setFailed] = React.useState(false);
+	const [ failed, setFailed ] = React.useState(false);
+	const [ moreRequired, setMoreRequired ] = React.useState(null);
+	const {emailOnly} = props;
 	
 	var validate = ['Required'];
 
@@ -26,41 +28,51 @@ export default function LoginForm (props) {
 		<Form className="login-form"
 			action = "user/login" 
 			onSuccess={response => {
+				if(response.moreDetailRequired){
+					// More required - e.g. a 2FA screen.
+					// The value of this is canvas compatible JSON.
+					setMoreRequired(response.moreDetailRequired);
+					return;
+				}
+				
 				setSession(response);
 				if(!props.noRedirect){
-					setPage("/");
+					setPage(props.redirectTo || '/');
 				}
 				props.onLogin && props.onLogin(response);
 			}}
 			onValues={v => {
-				setFailed(false)
+				setFailed(false);
 				return v;
 			}}
-			onFailed={()=>{
-				setFailed(true)
-			}}
+			onFailed={e=>setFailed(e)}
 			>
-			<Input label = {emailOnly ? "Email" : "Email or username"}  name="emailOrUsername" placeholder={emailOnly ? "Email" : "Email or username"} validate={validate} />
-			<Input label = "Password" name="password" placeholder="Password" type="password" />
-			<Row>
-				<Col size="6">
-					<label className="checkbox-label">
-						<input name="remember" type="checkbox" /> Remember me
-					</label>
-				</Col>
-				<Col size="6" className="text-right">
-					<a href="/forgot">I forgot my password</a>
-				</Col>
-			</Row>
-			<Spacer height="20" />
-			<Input type="submit" label="Login"/>
-			{failed && (
-				<Alert type="fail">
-					Those login details weren't right - please try again.
-				</Alert>
+			{moreRequired && (
+				<Canvas>{moreRequired}</Canvas>
 			)}
-			<div className="form-group">
-				Don't have an account? <a href="/register">Register here</a>
+			<div style={{display: moreRequired ? 'none' : 'initial'}}>
+				<Input label = {emailOnly ? `Email` : `Email or username`}  name="emailOrUsername" placeholder={emailOnly ? `Email` : `Email or username`} validate={validate} />
+				<Input label = {`Password`} name="password" placeholder={`Password`} type="password" />
+				<Row>
+					<Col size="6">
+						<label className="checkbox-label">
+							<input name="remember" type="checkbox" /> {`Remember me`}
+						</label>
+					</Col>
+					<Col size="6" className="text-right">
+						<a href="/forgot">{`I forgot my password`}</a>
+					</Col>
+				</Row>
+				<Spacer height="20" />
+				<Input type="submit" label="Login"/>
+				{failed && (
+					<Alert type="fail">
+						{`Those login details weren't right - please try again.`}
+					</Alert>
+				)}
+				<div className="form-group">
+					{`Don't have an account?`} <a href={props.registerUrl || "/register"}>{`Register here`}</a>
+				</div>
 			</div>
 		</Form>
 	);
