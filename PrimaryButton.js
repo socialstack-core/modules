07bfec2d182aec90d3f67a8a59aab0adca681b2e
Luke Briggs <luke@kulestar.com>
2021-06-27 16:15:00 +0100

    Upgraded to latest token structure, making them easier to edit and simpler to use in components

--- PrimaryButton.js
+++ PrimaryButton.js
@@ -1,51 +1,39 @@
-import applyTokens from 'Email/Functions/ApplyTokens';
-import Content from 'UI/Content';
-import { useSession } from 'UI/Session';
+import { TokenResolver } from 'UI/Token';
 
 /*
 * A large, centered button.
-* Target can include {context.tokens} where 
 */
 
 export default function PrimaryButton (props) {
-	
-	var {session} = useSession();
-	
-	return <Content primary>
-		{content => {
-			
-			var { origin } = global.location;
-			var target = props.target;
-			if(!target){target = '';}
-			
-			// Apply {tokens}:
-			target = applyTokens(target, session, content);
-			
-			if(target.indexOf('http') != 0){
-				if(target.length && target[0] == '/'){
-					target = origin + target;
-				}else{
-					target = origin + '/' + target;
-				}
-			}
-			
-			return <table role="presentation" style={{margin: "auto"}} cellspacing="0" cellpadding="0" border="0" align="center">
-					<tbody>
-						<tr>
-							<td class="button-td button-td-primary" style={{borderRadius: "4px", background: "#222222"}}>
-								 <a class="button-a button-a-primary" href={target} style={{background: "#222222", border: "1px solid #000000", fontFamily: "sans-serif", fontSize: "15px", lineHeight: "15px", textDecoration: "none", padding: "13px 17px", color: "#ffffff", display: "block", borderRadius: "4px"}}>
-									{props.label || 'Click here'}
-								 </a>
-							</td>
-						</tr>
-					</tbody>
-			</table>;
-		}}
-	</Content>;
-	
+	return <TokenResolver value={props.target}>
+		{target => <table role="presentation" cellspacing="0" style={{margin: "auto"}} cellpadding="0" border="0" align="center">
+			<tbody>
+				<tr>
+					<td class="button-td button-td-primary" style={{borderRadius: "4px", background: "#222222"}}>
+						 <a class="button-a button-a-primary" href={target} style={
+							 {
+								 background: "#222222",
+								 border: "1px solid #000000",
+								 fontFamily: "sans-serif",
+								 fontSize: "15px",
+								 lineHeight: "15px",
+								 textDecoration: "none",
+								 padding: "13px 17px",
+								 color: "#ffffff",
+								 display: "block",
+								 borderRadius: "4px"
+							}
+						}>
+							{props.label || `Click here`}
+						</a>
+					</td>
+				</tr>
+			</tbody>
+		</table>}
+	</TokenResolver>;
 }
 
 PrimaryButton.propTypes = {
-	label: 'string',
-	target: 'string'
+	label: {type: 'jsx', default: 'Click here'},
+	target: 'token'
 };
