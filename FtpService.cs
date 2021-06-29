1362b3701e77c415a009e91dba2334e893d20be9
Luke Briggs <luke@kulestar.com>
2021-06-29 12:47:37 +0100

    Initial commit

---
+++ FtpService.cs
@@ -0,0 +1,16 @@
+using Api.Contexts;
+
+
+namespace Api.Ftp
+{
+	
+	/// <summary>
+	/// A helper service for handling SFTP servers.
+	/// </summary>
+	public partial class FtpService : AutoService
+	{
+		
+		
+	}
+	
+}
