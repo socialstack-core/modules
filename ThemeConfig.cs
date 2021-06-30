94ef5c2a268f00148f92721584fd83a1236abc53
Luke Briggs <luke@kulestar.com>
2021-06-30 14:19:48 +0100

    Arranged the theme config to match the new multiple configs structure

--- ThemeConfig.cs
+++ ThemeConfig.cs
@@ -12,124 +12,83 @@ namespace Api.Themes
 	public partial class ThemeConfig : Config
 	{
 		/// <summary>
-		/// The base light palette. This is usually used as the source of colours for other variables.
+		/// Primary palette
 		/// </summary>
-		public ThemePalette LightPalette { get; set; } = new ThemePalette(
-			background: "#ffffff",
-			foreground: "#000000"
-			);
+		public ThemePalette Primary { get; set; }
 
 		/// <summary>
-		/// The base dark palette. This is usually used as the source of colours for other variables.
+		/// Secondary palette
 		/// </summary>
-		public ThemePalette DarkPalette { get; set; } = new ThemePalette(
-			background: "#000000",
-			foreground: "#ffffff"
-			);
+		public ThemePalette Secondary { get; set; }
 
+		/// <summary>
+		/// Tertiary palette
+		/// </summary>
+		public ThemePalette Tertiary { get; set; }
+
+		/// <summary>
+		/// Success palette
+		/// </summary>
+		public ThemePalette Success { get; set; }
+
+		/// <summary>
+		/// Info palette
+		/// </summary>
+		public ThemePalette Info { get; set; }
+
+		/// <summary>
+		/// Warning palette
+		/// </summary>
+		public ThemePalette Warning { get; set; }
+
+		/// <summary>
+		/// Danger palette
+		/// </summary>
+		public ThemePalette Danger { get; set; }
 	}
 
 	/// <summary>
-	/// Palette within the theme config. Very closely based on Bootstrap 5 color palette options.
+	/// A particular palette within a theme. 
+	/// Note that the default (rest) styles are directly on the palette, because it is also a ThemeUnit.
 	/// </summary>
-	public partial class ThemePalette
+	public partial class ThemePalette : ThemeUnit
 	{
 		/// <summary>
-		/// 
+		/// Hover mode
 		/// </summary>
-		/// <param name="background"></param>
-		/// <param name="foreground"></param>
-		/// <param name="primary"></param>
-		/// <param name="secondary"></param>
-		/// <param name="tertiary"></param>
-		/// <param name="success"></param>
-		/// <param name="info"></param>
-		/// <param name="warning"></param>
-		/// <param name="danger"></param>
-		/// <param name="light"></param>
-		/// <param name="dark"></param>
-		public ThemePalette(
-			string background, 
-			string foreground, 
-			string primary = "#0d6efd",
-			string secondary = "#6c757d",
-			string tertiary = "#6c757d",
-			string success = "#198754",
-			string info = "#0dcaf0",
-			string warning = "#ffc107",
-			string danger = "#dc3545",
-			string light = "#f8f9fa",
-			string dark = "#212529"
-			) {
-			Background = background;
-			Foreground = foreground;
-
-			Primary = primary;
-			Secondary = secondary;
-			Tertiary = tertiary;
-			Success = success;
-			Info = info;
-			Warning = warning;
-			Danger = danger;
-			Light = light;
-			Dark = dark;
-		}
+		public ThemeUnit Hover { get; set; }
 
 		/// <summary>
-		/// Background col
+		/// Focus mode
 		/// </summary>
-		public string Background { get; set; } = "#fff";
+		public ThemeUnit Focus { get; set; }
 
 		/// <summary>
-		/// Foreground col
+		/// Active mode
 		/// </summary>
-		public string Foreground { get; set; } = "#000";
-
-		/// <summary>
-		/// Primary col
-		/// </summary>
-		public string Primary { get; set; } = "#0d6efd";
-
-		/// <summary>
-		/// Secondary col
-		/// </summary>
-		public string Secondary { get; set; } = "#6c757d";
-
-		/// <summary>
-		/// Tertiary col
-		/// </summary>
-		public string Tertiary { get; set; } = "#6c757d";
-
-		/// <summary>
-		/// Success col
-		/// </summary>
-		public string Success { get; set; } = "#198754";
-
-		/// <summary>
-		/// Info col
-		/// </summary>
-		public string Info { get; set; } = "#0dcaf0";
-
-		/// <summary>
-		/// Warning col
-		/// </summary>
-		public string Warning { get; set; } = "#ffc107";
-
-		/// <summary>
-		/// Danger col
-		/// </summary>
-		public string Danger { get; set; } = "#dc3545";
-
-		/// <summary>
-		/// Light col
-		/// </summary>
-		public string Light { get; set; } = "#f8f9fa";
-
-		/// <summary>
-		/// Dark col
-		/// </summary>
-		public string Dark { get; set; } = "#212529";
-
+		public ThemeUnit Active { get; set; }
 	}
 
+	/// <summary>
+	/// A particular unit within the theme config. This declares a set of common css properties that can be edited within other theme segments.
+	/// </summary>
+	public partial class ThemeUnit
+	{
+		/// <summary>
+		/// Bg color
+		/// </summary>
+		public string Background { get; set; }
+		/// <summary>
+		/// Text color
+		/// </summary>
+		public string Color { get; set; }
+		/// <summary>
+		/// Border color
+		/// </summary>
+		public string Border { get; set; }
+		/// <summary>
+		/// Shadow color
+		/// </summary>
+		public string Shadow { get; set; }
+	}
 }
