71d16dc292803a2135d4e79a7715edabef2bb21a
Luke Briggs <luke@kulestar.com>
2021-08-12 00:54:52 +0100

    Added simple partial realtime behaviour to the stdout dash

--- Stdout/Stdout.js
+++ Stdout/Stdout.js
@@ -1,4 +1,5 @@
 import webRequest from 'UI/Functions/WebRequest';
+import Tile from 'Admin/Tile';
 import Alert from 'UI/Alert';
 import Html from 'UI/Html';
 
@@ -10,12 +11,28 @@ export default function StdOut() {
 	var [log, setLog] = React.useState();
 	
 	React.useEffect(() => {
-		webRequest('monitoring/stdout').then(response => {
-			setLog(response.json.log.replace(/\r/gi, '').replace(/\n/gi, '<br/>'));
-		});
+		var update = () => {
+			webRequest('monitoring/stdout').then(response => {
+				setLog(response.json.log.replace(/\r/gi, '').replace(/\n/gi, '<br/>'));
+				setTimeout(() => {
+					window.scroll(0,window.scrollMaxY);
+				}, 100);
+			});
+		};
+		
+		update();
+		
+		setInterval(update, 3000);
 	},[]);
 	
-	return <div className="dashboards-stdout">
-		<Html>{log}</Html>
+	return <div>
+		<Tile>
+			<Alert type='info'>
+				The following log is partially realtime. It will poll the current server for its current log fragment every 3 seconds.
+			</Alert>
+		</Tile>
+		<div className="dashboards-stdout">
+			<Html>{log}</Html>
+		</div>
 	</div>;
 }
