e474621d61667663ab4b05d1e40132346a7c9939
Luke Briggs <luke@kulestar.com>
2021-08-25 03:46:21 +0100

    Now supports props like className again

--- Html.js
+++ Html.js
@@ -1,9 +1,11 @@
+import omit from 'UI/Functions/Omit';
+
 /**
  * This component displays html. Only ever use this with trusted text.
 */
 
 export default function Html (props) {
-   return <span dangerouslySetInnerHTML={{__html: props.html || props.children}} />;
+   return <span dangerouslySetInnerHTML={{__html: props.html || props.children}} {...omit(props, ['html', 'children'])} />;
 }
 
 Html.propTypes={
