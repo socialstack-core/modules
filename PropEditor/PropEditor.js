c432e10c6c6457b5b80a7fe1a1589d6030c1dd33
Michael Rogers <michaelr@conceptsoftworks.com>
2021-09-13 16:12:19 -0700

    Commented out code block causing collectLinkTypes to trigger.

--- PropEditor/PropEditor.js
+++ PropEditor/PropEditor.js
@@ -194,6 +194,7 @@ export default class PropEditor extends React.Component {
 			var inputContent = undefined;
 			var propType = fieldInfo.propType;
 			
+			{/*
 			if(fieldInfo.value && fieldInfo.value.type && fieldInfo.value.type != 'module'){
 				// It's a linked field.
 				// Show its options instead.
@@ -234,7 +235,7 @@ export default class PropEditor extends React.Component {
 					</div>
 				);
 				return;
-			}
+			}*/}
 			
 			var extraProps = {};
 			
