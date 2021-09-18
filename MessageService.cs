3790e423d679275d72c8d4b21e3290f23f3ba2d7
Luke Briggs <luke@kulestar.com>
2021-09-18 13:51:37 +0100

    Added email based support

--- MessageService.cs
+++ MessageService.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using Api.Permissions;
 using Api.Contexts;
 using Api.Eventing;
+using Api.Startup;
+using Api.Emails;
 
 namespace Api.Messages
 {
@@ -13,12 +15,60 @@ namespace Api.Messages
 	/// </summary>
 	public partial class MessageService : AutoService<Message>
     {
+		private MessageServiceConfig _config;
+		private readonly EmailTemplateService _emailService;
+
+
 		/// <summary>
 		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 		/// </summary>
-		public MessageService() : base(Events.Message)
+		public MessageService(EmailTemplateService emailService) : base(Events.Message)
         {
+			_emailService = emailService;
+			_config = GetConfig<MessageServiceConfig>();
 		}
+		
+		/// <summary>
+		/// Sends a support email
+		/// </summary>
+		public async ValueTask SendSupportEmail(Context context, SupportEmailRequest supportEmailRequest)
+		{
+
+			if (string.IsNullOrEmpty(_config.SupportEmailAddress))
+			{
+				throw new PublicException("Support email is unavailable.", "not_configured");
+			}
+
+			string replyTo;
+
+			if (!string.IsNullOrEmpty(supportEmailRequest.EmailAddress))
+			{
+				replyTo = supportEmailRequest.EmailAddress;
+			}
+			else if (context.User != null && !string.IsNullOrEmpty(context.User.Email))
+			{
+				replyTo = context.User.Email;
+			}
+			else
+			{
+				throw new PublicException("No email address provided.", "no_reply_to");
+			}
+
+			var additionalHeaders = new Dictionary<string, string>() {
+				{ "Reply-To", replyTo }
+			};
+
+			await _emailService.Send(
+				_config.SupportEmailAddress,
+				"New support request", 
+				"Their email address<br>" + supportEmailRequest.EmailAddress + "<br><br>Their message<br>" + supportEmailRequest.Body + "<br><br>---<br><br>Replies to this email will be sent to " + supportEmailRequest.EmailAddress + ".",
+				null, 
+				null,
+				null,
+				additionalHeaders
+			);
+		}
+		
 	}
     
 }
