0508bb151a40a2925b603fa0a667e41ce2fd8b70
Luke Briggs <luke@kulestar.com>
2021-09-26 02:35:32 +0100

    Upgraded to use new config system

--- FileTypeBlockerConfig.cs
+++ FileTypeBlockerConfig.cs
@@ -1,3 +1,4 @@
+using Api.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,18 +9,91 @@ namespace Api.FileTypeBlocker
 	/// <summary>
 	/// The appsettings.json config block for the file type blocker.
 	/// </summary>
-    public class FileTypeBlockerConfig
+    public class FileTypeBlockerConfig : Config
     {
 		/// <summary>
 		/// Filetypes to reject uploading. If neither this or whitelist is specified then a default set is used instead.
 		/// </summary>
-		public string[] Blacklist {get; set;}
-		
+		public string[] Blacklist {get; set;} = new string[]{
+			"exe",
+			"msi",
+			"pif",
+			"application",
+			"app",
+			"com",
+			"scr",
+			"hta",
+			"cpl",
+			"msc",
+			"jar",
+			"js",
+			"jse",
+			"html",
+			"css",
+			"bat",
+			"cmd",
+			"sh",
+			"php",
+			"ps1",
+			"ps1xml",
+			"ps2",
+			"scf",
+			"lnk",
+			"inf",
+			"reg",
+			"ps2xml",
+			"psc1",
+			"psc2",
+			"htaccess",
+			"aspx",
+			"asp",
+			"zip",
+			"rar",
+			"tar",
+			"gz",
+			"img",
+			"iso",
+			"bin"
+		};
+
 		/// <summary>
 		/// Filetypes to only permit. If neither this or whitelist is specified then a default set is used instead.
 		/// </summary>
-		public string[] Whitelist {get; set;}
-		
+		public string[] Whitelist {get; set;} = new string[]{
+			"apng",
+			"avif",
+			"png",
+			"jpg",
+			"jpeg",
+			"tiff",
+			"tif",
+			"bmp",
+			"heif",
+			"heic",
+			"gif",
+			"webp",
+			"webm",
+			"mp3",
+			"pdf",
+			"svg",
+			"svgz",
+			"jp2",
+			"j2k",
+			"jpf",
+			"jpx",
+			"jpm",
+			"mj2",
+			"wav",
+			"mp4",
+			"webm",
+			"mpeg",
+			"mpg",
+			"mp2",
+			"avi",
+			"mov",
+			"mkv"
+		};
+
 		/// <summary>
 		/// True if it should use the whitelist technique, but with the default set.
 		/// </summary>
--- FileTypeBlockerService.cs
+++ FileTypeBlockerService.cs
@@ -7,99 +7,17 @@ using Api.Eventing;
 using Api.Configuration;
 using Microsoft.Extensions.Configuration;
 using Api.Uploader;
+using Api.Startup;
 
 namespace Api.FileTypeBlocker
 {
 	/// <summary>
 	/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 	/// </summary>
-	public partial class FileTypeBlockerService
+	public partial class FileTypeBlockerService : AutoService
     {
 		private FileTypeBlockerConfig _configuration;
 
-		/// <summary>
-		/// Default whitelist
-		/// </summary>
-		public readonly string[] WhitelistDefaults = new string[]{
-		    "apng",
-		    "avif",
-			"png",
-			"jpg",
-			"jpeg",
-			"tiff",
-			"tif",
-			"bmp",
-			"heif",
-			"heic",
-			"gif",
-			"webp",
-			"webm",
-			"mp3",
-			"pdf",
-			"svg",
-			"svgz",
-			"jp2",
-			"j2k",
-			"jpf",
-			"jpx",
-			"jpm",
-			"mj2",
-			"wav",
-			"mp4",
-			"webm",
-			"mpeg",
-			"mpg",
-			"mp2",
-			"avi",
-			"mov",
-			"mkv"
-		};
-		
-		/// <summary>
-		/// Default blacklist
-		/// </summary>
-		public readonly string[] BlacklistDefaults = new string[]{
-			"exe",
-			"msi",
-			"pif",
-			"application",
-			"app",
-			"com",
-			"scr",
-			"hta",
-			"cpl",
-			"msc",
-			"jar",
-			"js",
-			"jse",
-			"html",
-			"css",
-			"bat",
-			"cmd",
-			"sh",
-			"php",
-			"ps1",
-			"ps1xml",
-			"ps2",
-			"scf",
-			"lnk",
-			"inf",
-			"reg",
-			"ps2xml",
-			"psc1",
-			"psc2",
-			"htaccess",
-			"aspx",
-			"asp",
-			"zip",
-			"rar",
-			"tar",
-			"gz",
-			"img",
-			"iso",
-			"bin"
-		};
-		
 		/// <summary>
 		/// The constructed filter set.
 		/// </summary>
@@ -109,44 +27,47 @@ namespace Api.FileTypeBlocker
 		/// True if it's using a whitelist technique.
 		/// </summary>
 		private bool UseWhitelist;
-		
+
 		/// <summary>
 		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 		/// </summary>
 		public FileTypeBlockerService()
-        {
-			_configuration = AppSettings.GetSection("FileTypeBlocker").Get<FileTypeBlockerConfig>();
-			
-			var useWhitelist = false;
-			var blacklist = BlacklistDefaults;
-			var whitelist = WhitelistDefaults;
-			
-			if(_configuration != null)
-			{
-				if(_configuration.Blacklist != null)
-				{
-					blacklist = _configuration.Blacklist;
-				}
-				
-				if(_configuration.Whitelist != null)
-				{
-					whitelist = _configuration.Whitelist;
-				}
-				
-				useWhitelist = _configuration.UseWhitelist;
-			}
-			
-			var filterSet = useWhitelist ? whitelist : blacklist;
+		{
+			_configuration = GetConfig<FileTypeBlockerConfig>();
+
+			_configuration.OnChange += () => {
+				UpdateList();
+				return new ValueTask();
+			};
+
+			UpdateList();
+		}
+
+		private void UpdateList()
+		{
 			FilterSet = new Dictionary<string, bool>();
-			
-			foreach(var ext in filterSet)
+			UseWhitelist = false;
+
+			if (_configuration == null)
 			{
-				// Add:
-				FilterSet[ext.Trim().ToLower()] = true;
+				// Do nothing.
+				return;
 			}
-			
-			UseWhitelist = useWhitelist;
-			
+
+			UseWhitelist = _configuration.UseWhitelist;
+
+			var filterSet = UseWhitelist ? _configuration.Whitelist : _configuration.Blacklist;
+
+			if (filterSet != null)
+			{
+
+				foreach (var ext in filterSet)
+				{
+					// Add:
+					FilterSet[ext.Trim().ToLower()] = true;
+				}
+			}
+
 			Events.Upload.BeforeCreate.AddEventListener((Context context, Upload upload) => {
 				
 				if(upload == null)
@@ -160,7 +81,7 @@ namespace Api.FileTypeBlocker
 				if(FilterSet.ContainsKey(upload.FileType) != UseWhitelist)
 				{
 					// Reject:
-					upload = null;
+					throw new PublicException("'" + upload.FileType + "' files can't be uploaded here.", "file_type");
 				}
 				
 				return new ValueTask<Upload>(upload);
