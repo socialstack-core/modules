00a65179cf1fb092593843d362bb43fcd1456c29
Conan Ablewhite <conan.ablewhite@4-roads.com>
2021-10-05 10:33:55 +0100

    removed nested <span>s from <p> tags

--- Text.js
+++ Text.js
@@ -7,17 +7,19 @@ import omit from 'UI/Functions/Omit';
  * <Text group='header' key='hello_world'>Hello world</Text>
  */
 export default function Text (props) {
-	var text = <span 
-		dangerouslySetInnerHTML={{__html: (props.text || props.children)}}
-		{...omit(props, ['text', 'children', 'paragraph'])}
-	/>;
-	
-	return props.paragraph ? <p>{text}</p> : text;
+	var className = this.props.className || undefined;
+	var Tag = props.paragraph ? "p" : "span";
+
+	return <Tag className={className}
+				dangerouslySetInnerHTML={{__html: (props.text || props.children)}}
+				{...omit(props, ['text', 'children', 'paragraph'])}>
+		</Tag>;
 }
 
 Text.propTypes = {
 	paragraph: 'boolean'
 };
+
 Text.icon = 'align-justify';
 
 Text.rendererPropTypes = {
