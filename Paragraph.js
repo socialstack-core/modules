03d6ebb31cc98b1fdbbed7bf6585135dea55f452
Conan Ablewhite <conan.ablewhite@4-roads.com>
2021-10-06 01:47:53 +0100

    bold / animate options added

--- Paragraph.js
+++ Paragraph.js
@@ -1,10 +1,25 @@
 import omit from 'UI/Functions/Omit';
 
 export default function Paragraph (props) {
-	return <p data-aos="fade-up" {...(omit(props, ['children']))}>{props.children}</p>;
+	const { bold, animate } = props;
+
+	var className = props.className || undefined;
+	var animation = animate ? "fade-up" : undefined;
+
+	return <p data-aos={animation} className={className} {...(omit(props, ['children']))}>
+			{bold ? <strong>{props.children}</strong> : <>{props.children}</>}
+		</p>;
 }
 
 Paragraph.propTypes = {
-	children: true
+	children: true,
+	bold: 'boolean',
+	animate: 'boolean'
 };
+
+Paragraph.defaultProps = {
+	bold: false,
+	animate: true
+};
+
 Paragraph.icon = 'align-left';
