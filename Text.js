bbbdfd6683781a27dc859cae601494ee938ad5ea
Conan Ablewhite <conan.ablewhite@4-roads.com>
2021-10-08 03:37:39 +0100

    className / animation support added

--- Text.js
+++ Text.js
@@ -7,10 +7,37 @@ import omit from 'UI/Functions/Omit';
  * <Text group='header' key='hello_world'>Hello world</Text>
  */
 export default function Text (props) {
-	const { paragraph, bold, animate } = props;
+	const { paragraph, bold, className, animation, animationDirection } = props;
+	const omitProps = ['text', 'children', 'paragraph', 'bold', 'className', 'animation', 'animationDirection'];
 
-	var className = props.className || undefined;
-	var animation = animate ? "fade-up" : undefined;
+	var anim = animation ? animation : undefined;
+
+	// ref: https://github.com/michalsnik/aos
+	// TODO: disable horizontal anims on mobile to prevent triggering horizontal scrolling issues
+	switch (anim) {
+		case 'fade':
+		case 'zoom-in':
+		case 'zoom-out':
+
+			if (animationDirection) {
+				anim += "-" + animationDirection;
+			}
+
+			break;
+	
+		case 'flip':
+		case 'slide':
+
+			// default static flip / slide animations to "up" variants
+			if (animationDirection) {
+				anim += "-" + animationDirection;
+			} else {
+				anim += "-up";
+			}
+
+		break;
+	}
+
 	var Tag = paragraph ? "p" : "span";
 
 	if (bold && !paragraph) {
@@ -18,29 +45,48 @@ export default function Text (props) {
 	}
 
 	if (bold && paragraph) {
-		return <p className={className} data-aos={animation}
-				{...omit(props, ['text', 'children', 'paragraph', 'bold', 'animate'])}>
+		return <p className={className} data-aos={anim} {...omit(props, omitProps)}>
 				<strong dangerouslySetInnerHTML={{__html: (props.text || props.children)}}>
 				</strong>
 			</p>;
 	}
 
-	return <Tag className={className} data-aos={animation}
-				dangerouslySetInnerHTML={{__html: (props.text || props.children)}}
-				{...omit(props, ['text', 'children', 'paragraph', 'bold', 'animate'])}>
+	return <Tag className={className} data-aos={anim} dangerouslySetInnerHTML={{__html: (props.text || props.children)}} {...omit(props, omitProps)}>
 		</Tag>;
 }
 
 Text.propTypes = {
 	paragraph: 'boolean',
 	bold: 'boolean',
-	animate: 'boolean'
+	className: 'string',
+	animation: [
+		{ name: 'None', value: null },
+		{ name: 'Fade', value: 'fade' },
+		{ name: 'Flip', value: 'flip' },
+		{ name: 'Slide', value: 'slide' },
+		{ name: 'Zoom in', value: 'zoom-in' },
+		{ name: 'Zoom out', value: 'zoom-out' }
+	],
+
+	// NB - currently unsupported:
+	//fade-up-right
+	//fade-up-left
+	//fade-down-right
+	//fade-down-left
+	animationDirection: [
+		{ name: 'Static', value: null },
+		{ name: 'Up', value: 'up' },
+		{ name: 'Down', value: 'down' },
+		{ name: 'Left', value: 'left' },
+		{ name: 'Right', value: 'right' },
+	]
 };
 
 Text.defaultProps = {
 	paragraph: false,
 	bold: false,
-	animate: true
+	animation: 'none',
+	animationDirection: 'static'
 };
 
 Text.icon = 'align-justify';
@@ -49,5 +95,5 @@ Text.rendererPropTypes = {
 	text: 'string',
 	paragraph: 'boolean',
 	bold: 'boolean',
-	animate: 'boolean'
+	className: 'string'
 };
