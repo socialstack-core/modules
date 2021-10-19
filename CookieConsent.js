96d7ebcbc596180387862c7d83a236295d8a962f
Luke Briggs <luke@kulestar.com>
2021-10-19 03:34:15 +0100

    Made the consent banner themeable

--- CookieConsent.js
+++ CookieConsent.js
@@ -25,7 +25,7 @@ export default class CookieConsent extends React.Component {
 		super(props);
 		this.state={mode:1};
 		
-		this.cookies = [
+		this.cookies = props.cookies || [
 			{title: 'Logging In', names: ['user'], necessary: true, description: 'This one lets us log you in.'},
 			{title: 'Consent', names: ['consent'], necessary: true, description: 'This helps us remember which type of cookies you\'re ok with.'},
 			{title: 'Statistics', names: ['_ga', '_gid'], description: 'This is created by Google Analytics. We\'d like to use Google Analytics to figure out how many people are using our website and what parts of it they\'re using so we can make them even better. It does, however, potentially give Google the ability to track you on the web in general.'}
@@ -33,20 +33,19 @@ export default class CookieConsent extends React.Component {
 	}
 	
 	renderDetails(){
-		return <Modal isExtraLarge className="consent-info" visible={this.state.details} onClose={() => this.setState({details: false})}>
-			<h1>Cookies - what are they?</h1>
+		return <Modal title={`Cookies - what are they?`} isExtraLarge className="consent-info" visible={this.state.details} onClose={() => this.setState({details: false})}>
 			<p>
-				Cookies are little bits of data we store on your computer or mobile device to remember things. They're great for things like letting you log in - we'll remember who you are next time you come back - but they're also good for tracking where you go on the internet which can be quite invasive, so that's why these prompts exist on lots of websites.
+				{`Cookies are little bits of data we store on your computer or mobile device to remember things. They're great for things like letting you log in - we'll remember who you are next time you come back - but they're also good for tracking where you go on the internet which can be quite invasive, so that's why these prompts exist on lots of websites.`}
 			</p>
-			<h1>How we use them</h1>
+			<h3>{`How we use them`}</h3>
 			<p>
-				We use cookies for the following things:
+				{`We use cookies for the following things:`}
 			</p>
 			<ul>
 				{this.cookies.map(cookie => {
 					
 					return <li>
-						<h2>{cookie.title + ' - ' + (cookie.necessary ? 'Necessary' : 'Not Necessary')}</h2>
+						<h4>{cookie.title + ' - ' + (cookie.necessary ? `Necessary` : `Not Necessary`)}</h4>
 						<p>
 							Cookie name(s): {cookie.names.join(', ')}
 						</p>
@@ -73,10 +72,10 @@ export default class CookieConsent extends React.Component {
 			return null;
 		}
 		
-		return <div className="cookie-consent">
+		return <div className="cookie-consent" data-theme={this.props.theme || "cookie-consent-theme"}>
 			<Container>
 				<h2>
-					We'd like to use cookies
+					{`We'd like to use cookies`}
 				</h2>
 				<p>
 					We use <a href='#' className="show-details" onClick={() => this.setState({
@@ -84,8 +83,8 @@ export default class CookieConsent extends React.Component {
 					})}>cookies</a> to give you a great online experience. Please let us know if you agree to them.
 				</p>
 				<div>
-					<button className="btn btn-secondary necessary-only" onClick={() => this.set('necessary')}>Use necessary cookies only</button>
-					<button className="btn btn-primary" onClick={() => this.set('all')}>I agree</button>
+					<button className="btn btn-secondary necessary-only" onClick={() => this.set('necessary')}>{`Use necessary cookies only`}</button>
+					<button className="btn btn-primary" onClick={() => this.set('all')}>{`I agree`}</button>
 				</div>
 			</Container>
 			{this.renderDetails()}
