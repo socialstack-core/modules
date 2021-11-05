740f00b3d293f1ce00f064020cfbe15f2b6573e2
Conan Ablewhite <conan.ablewhite@4-roads.com>
2021-11-05 14:07:57 +0000

    try/catch {} wrapper added; origin added to new URL() call

--- UrlTools.js
+++ UrlTools.js
@@ -1,13 +1,19 @@
 function getParam(param) {
-	// hitachivantara.virtualrack.com/products?category=1
-	var url = new URL(window.location.href);
-	var val = url && url.searchParams ? url.searchParams.get(param) : undefined;
 
-	// check if we're using hash routing (e.g. within a Cordova app)
-	// /apps/com.hitachivantara.virtualrack/asdasdj898u8345/index.html?category=1#/products
-	if (!val) {
-		var hash = window.location.hash && window.location.hash.length ? new URL(window.location.hash) : undefined;
-		val = hash && hash.searchParams ? hash.searchParams.get(param) : undefined;
+	try {
+		// example.com/products?myparam=1
+		var url = new URL(window.location.href);
+		var val = url && url.searchParams ? url.searchParams.get(param) : undefined;
+
+		// check if we're using hash routing (e.g. within a Cordova app)
+		// /apps/com.example/asdasdj898u8345/index.html?myparam=1#/products
+		if (!val) {
+			var hash = window.location.hash && window.location.hash.length ? new URL(window.location.hash, window.location.origin) : undefined;
+			val = hash && hash.searchParams ? hash.searchParams.get(param) : undefined;
+		}
+	}
+	catch {
+		val = undefined;
 	}
 
 	return val;
