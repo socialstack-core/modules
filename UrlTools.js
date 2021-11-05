d069a5dc7c35cce6d7cee9226c0fdbaf4dd32192
Conan Ablewhite <conan.ablewhite@4-roads.com>
2021-11-05 15:41:54 +0000

    hash routing param check updated

--- UrlTools.js
+++ UrlTools.js
@@ -1,19 +1,39 @@
 function getParam(param) {
+	// example.com/products?myparam=1
+	var url = new URL(window.location.href);
+	var val = url && url.searchParams ? url.searchParams.get(param) : undefined;
 
-	try {
-		// example.com/products?myparam=1
-		var url = new URL(window.location.href);
-		var val = url && url.searchParams ? url.searchParams.get(param) : undefined;
+	// check if we're using hash routing (e.g. within a Cordova app)
+	// e.g. file:///android_asset/www/index.en.html#/products?myparam=1
+	if (!val) {
+		var urlParts = window.location.href.split("?");
 
-		// check if we're using hash routing (e.g. within a Cordova app)
-		// /apps/com.example/asdasdj898u8345/index.html?myparam=1#/products
-		if (!val) {
-			var hash = window.location.hash && window.location.hash.length ? new URL(window.location.hash, window.location.origin) : undefined;
-			val = hash && hash.searchParams ? hash.searchParams.get(param) : undefined;
+		if (urlParts.length > 1) {
+			var searchParams = urlParts[urlParts.length - 1];
+
+			// strip any appended hash
+			// e.g. myparam=1#/products
+			var hashCheck = searchParams.split("#");
+
+			if (hashCheck.length > 1) {
+				searchParams = hashCheck[0];
+			}
+
+			// split into individual params
+			searchParams = searchParams.split("&");
+
+			// split each param into key/value pair
+			for (var i = 0; i < searchParams.length; i++) {
+				var kv = searchParams[i].split("=");
+
+				if (kv.length == 2 && kv[0] == param) {
+					val = kv[1];
+				}
+
+			}
+
 		}
-	}
-	catch {
-		val = undefined;
+
 	}
 
 	return val;
