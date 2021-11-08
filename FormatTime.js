bb547eeabe65d8fa6ffd5b8cd0f754b3f08bcaa3
kurt.hodges <kurt.hodges@4-roads.com>
2021-11-08 15:53:00 +0000

    Made ordinal_suffix_of an exported function

--- FormatTime.js
+++ FormatTime.js
@@ -5,7 +5,7 @@ import * as dateTools from 'UI/Functions/DateTools';
 */
 const longMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-function ordinal_suffix_of(i) {
+export function ordinal_suffix_of(i) {
     var j = i % 10,
         k = i % 100;
     if (j == 1 && k != 11) {
