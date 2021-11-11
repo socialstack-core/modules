9335b4c6bd2998ae38c6559678c70227831d9ce9
Luke Briggs <luke@kulestar.com>
2021-11-11 00:33:12 +0000

    Added ability to get the public URL

--- Upload.cs
+++ Upload.cs
@@ -153,6 +153,15 @@ namespace Api.Uploader
 		}
 
 		/// <summary>
+		/// Gets the absolute URL of this upload (unsigned).
+		/// </summary>
+		/// <returns></returns>
+		public string GetUrl(string variant = "original")
+		{
+			return Services.Get<FrontendCodeService>().GetContentUrl() + (IsPrivate ? "/content-private/" : "/content/") + GetRelativePath(variant);
+		}
+		
+		/// <summary>
 		/// Gets a transcode callback URL. This allows trustless file manipulation.
 		/// </summary>
 		/// <returns></returns>
