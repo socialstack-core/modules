6c4d86ee553c2aa2344fec3de1b3218052e3a4bb
Luke Briggs <luke@kulestar.com>
2021-12-01 13:17:03 +0000

    dotnet 5 backwards compatibility

--- WebSocketServiceConfig.cs
+++ WebSocketServiceConfig.cs
@@ -1,14 +1,16 @@
 using Api.Configuration;
-namespace Api.WebSockets;
 
 
-/// <summary>
-/// Config for WS service.
-/// </summary>
-public class WebSocketServiceConfig : Config
+namespace Api.WebSockets
 {
 	/// <summary>
-	/// True if all clients should be available via websocketservice.AllClients.
+	/// Config for WS service.
 	/// </summary>
-	public bool? TrackAllClients {get; set;}
+	public class WebSocketServiceConfig : Config
+	{
+		/// <summary>
+		/// True if all clients should be available via websocketservice.AllClients.
+		/// </summary>
+		public bool? TrackAllClients {get; set;}
+	}
 }
