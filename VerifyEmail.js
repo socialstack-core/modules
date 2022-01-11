8667856608d0d12871b92d485aec177dafac3767
kurt.hodges <kurt.hodges@4-roads.com>
2022-01-11 16:26:02 +0000

    Added theme support

--- VerifyEmail.js
+++ VerifyEmail.js
@@ -13,61 +13,63 @@ export default function VerifyEmail(props) {
 	var token = useTokens('${url.token}');
 	var userId = useTokens('${url.userid}');
 
-	return (			
-		<Form
-			action = {"user/verify/" + userId + "/" + token}
-			method='POST'
-			onSuccess={response => {
-				setSuccess(true);
-				onSuccess && onSuccess();
-				if(!noRelocation) { 
-					window.location.href = "/";
-				}
-			}}
-			className="verify-email-form"
-			onFailed={e=>{
-				if (!e.message) {
-					e.message = "Something went wrong. Please make sure the passwords match and try again.";
-				}
-				setFailed(e);
-			}}
-			onValues={v=>{
-				if (v.password !== v.passwordRepeat) {
-					return Promise.reject(new Error('The passwords do not match.'));
-				}
+	return (
+		<div className="verify-email-wrapper" data-theme={props['data-theme']}>
+			<Form
+				action = {"user/verify/" + userId + "/" + token}
+				method='POST'
+				onSuccess={response => {
+					setSuccess(true);
+					onSuccess && onSuccess();
+					if(!noRelocation) { 
+						window.location.href = "/";
+					}
+				}}
+				className="verify-email-form"
+				onFailed={e=>{
+					if (!e.message) {
+						e.message = "Something went wrong. Please make sure the passwords match and try again.";
+					}
+					setFailed(e);
+				}}
+				onValues={v=>{
+					if (v.password !== v.passwordRepeat) {
+						return Promise.reject(new Error('The passwords do not match.'));
+					}
 
-				return v;
-			}}
-		>
-			<h2 className="verify-header">Create a password</h2>
+					return v;
+				}}
+			>
+				<h2 className="verify-header">Create a password</h2>
 
-			<div className="verify-description">
-				<p>
-					In order to complete registration, you will need to create a password.
-				</p>
-			</div>
-
-			<div className="verify-input-group">
-				<Input className="verify-input input-grey" name="password" type="password" placeholder="Create a password" validate={['Required']} onInput/>
-				<Input className="verify-input input-grey" name="passwordRepeat" type="password" placeholder="Confirm password" validate={['Required']} onInput/>
-				<Input className="remember-me" name="rememberMe" type="checkbox" label={"Remember me"} />
-			</div>
-			{failed && (
-				<Alert type="fail">
-					{failed.message ? failed.message : failed == "VALIDATION" && "Please verify all values are correct."}
-				</Alert>
-			)}
-			{success ? (
-				<Alert type="success">
-					Account created!
-				</Alert>
-			) : (
-				<div>
-					<Spacer height="20"/>
-					<Input className="btn btn-primary verify-input" type="submit" label="Next" />
+				<div className="verify-description">
+					<p>
+						In order to complete registration, you will need to create a password.
+					</p>
 				</div>
-			)}
-		</Form>
+
+				<div className="verify-input-group">
+					<Input className="verify-input input-grey" name="password" type="password" placeholder="Create a password" validate={['Required']} onInput/>
+					<Input className="verify-input input-grey" name="passwordRepeat" type="password" placeholder="Confirm password" validate={['Required']} onInput/>
+					<Input className="remember-me" name="rememberMe" type="checkbox" label={"Remember me"} />
+				</div>
+				{failed && (
+					<Alert type="fail">
+						{failed.message ? failed.message : failed == "VALIDATION" && "Please verify all values are correct."}
+					</Alert>
+				)}
+				{success ? (
+					<Alert type="success">
+						Account created!
+					</Alert>
+				) : (
+					<div>
+						<Spacer height="20"/>
+						<Input className="btn btn-primary verify-input" type="submit" label="Next" />
+					</div>
+				)}
+			</Form>
+		</div>	
 	);
 }
 
