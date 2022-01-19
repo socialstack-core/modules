f70de6727243320e04ffad7a88001c8367d7156b
RobN <robert.nash@4-roads.com>
2022-01-19 23:44:20 +0000

    added support for default parameters

--- ParseQueryString.js
+++ ParseQueryString.js
@@ -1,18 +1,19 @@
 /**
  * Breaks a querystring into its key/value pairs.
- * @param str
+ * @param queryString
+ * @param defaultParams
  */
-export default function parseQueryString(str) {
-	if (!str || !str.length) {
-		return {};
+export default function parseQueryString(queryString = null, defaultParams = {}) {
+	if (!queryString || !queryString.length) {
+		queryString = window.location.search.replace("?", '')
 	}
 
-	if (str[0] == '?') {
-		str = str.substring(1);
+	if (queryString[0] == '?') {
+		queryString = queryString.substring(1);
 	}
 
-	var pieces = str.split('&');
-	var result = {};
+	var pieces = queryString.split('&');
+	var result = defaultParams;
 	for (var i = 0; i < pieces.length; i++) {
 		var entry = pieces[i].split('=');
 		result[entry[0]] = decodeURI(entry[1]);
