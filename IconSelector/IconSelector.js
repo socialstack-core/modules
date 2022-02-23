d3cfc3acf647836a45c9f96abc7e3cf10ddf0261
Luke Briggs <luke@kulestar.com>
2022-02-23 14:49:24 +0000

    Can now display custom icon sets

--- IconSelector/IconSelector.js
+++ IconSelector/IconSelector.js
@@ -11,25 +11,54 @@ import webRequest from 'UI/Functions/WebRequest';
 import getRef from 'UI/Functions/GetRef';
 
 let icons = [];
+let iconStyles = [];
+let iconSets = [];
 
 export default class IconSelector extends React.Component {
     constructor(props){
 		super(props);
         this.search = this.search.bind(this);
 		this.state = {
-			icons,
 			selectIcon: false,
             selectedIcon: null,
-            styleFilter: "all",
             debounce: new Debounce(this.search)
 		};
 		
 	}
 	
 	componentDidMount(){
-		if(!this.state.icons.length){
-			webRequest(getRef(faIconsRef, {url: true})).then(response => {
-				icons = response.json;
+		if(!icons.length){
+			
+			var styles = [{name: 'All', key: 'all'},{name:'Regular', key: 'regular', prefix: 'far'}, {name:'Solid', key: 'solid', prefix: 'fas'}, {name: 'Brands', key: 'brands', prefix: 'fab'}];
+			var sets = [{name: 'All', key: 'all'}, {name: 'Default (FontAwesome)', key: 'default'}];
+			
+			var proms = [webRequest(getRef(faIconsRef, {url: true}))];
+			
+			if(global.customIcons){
+				global.customIcons.forEach(ci => {
+					proms.push(webRequest(getRef(ci.listRef, {url: true})).then(response=>{
+						response.json.forEach(icon => {
+							if(ci.prefix){
+								icon.prefix = ci.prefix;
+							}
+							
+							icon.set = ci.customSetName || 'custom';
+						});
+						
+						sets.push({name: ci.customSetName || 'custom', key: ci.customSetName || 'custom'});
+						
+						return response;
+					}));
+					
+				});
+				
+			}
+			
+			Promise.all(proms).then(responses => {
+				icons = [];
+				responses.forEach(r => icons=icons.concat(r.json));
+				iconStyles = styles;
+				iconSets = sets;
 				this.setState({icons});
 			});
 		}
@@ -57,10 +86,18 @@ export default class IconSelector extends React.Component {
                         this.setState({styleFilter: e.target.value})
                     }}
                 >
-                    <option value = {"all"}>All</option>
-                    <option value = {"regular"}>Regular</option>
-                    <option value = {"solid"}>Solid</option>
-                    <option value = {"brands"}>Brands</option>
+					{iconStyles.map(s => <option value = {s.key}>{s.name}</option>)}
+                </Input>
+                <label>
+                    Set
+                </label>
+                <Input type ="select"
+                    name = "set"
+                    onChange = {(e) => {
+                        this.setState({setFilter: e.target.value})
+                    }}
+                >
+					{iconSets.map(s => <option value = {s.key}>{s.name}</option>)}
                 </Input>
             </Col>
             <Col size = {6}>
@@ -75,8 +112,14 @@ export default class IconSelector extends React.Component {
     }
 
     render(){
-        var {selectIcon, value, styleFilter, searchFilter} = this.state;
-
+        var {selectIcon, value, styleFilter, setFilter, searchFilter} = this.state;
+		
+		var prefixForStyle = {};
+		
+		iconStyles.forEach(s => {
+			prefixForStyle[s.key] = s.prefix;
+		});
+		
         return <div className = "icon-selector">
             <Modal
                 visible = {this.props.visible}
@@ -94,18 +137,35 @@ export default class IconSelector extends React.Component {
                         {icon => {
                             if(icon.name.includes(searchFilter) ||icon.name.replace(/-/g, " ").includes(searchFilter) || !searchFilter) {
                                 return icon.styles.map(style => {
-                                    if(styleFilter == "all" || styleFilter == style) {
-                                        return <Col className="icon-tile" size = {3} onClick= {() => {
-                                            var newIcon = "fa"+style[0]+":fa-" + icon.name;
-                                            
-                                            this.setState({value: newIcon});
-                                            this.props.onSelected && this.props.onSelected(newIcon)
-                                            this.closeModal && this.closeModal();
-                                        }}>
-                                            <i className={"fa"+style[0]+" fa-" + icon.name} />
-                                            <p>{icon.name.replace(/-/g, " ")} ({style})</p>
-                                        </Col>
-                                    }
+									
+									if(styleFilter && styleFilter != "all"){
+										if(styleFilter != style){
+											return null;
+										}
+									}
+									
+									if(setFilter && setFilter != "all"){
+										if(setFilter == 'default'){
+											if(icon.set){
+												return null;
+											}
+										}else if(setFilter != icon.set){
+											return null;
+										}
+									}
+									
+									var prefix = prefixForStyle[style];
+									
+									return <Col className="icon-tile" size = {3} onClick= {() => {
+										var newIcon = prefix+":"+(icon.prefix||"fa")+"-" + icon.name;
+										
+										this.setState({value: newIcon});
+										this.props.onSelected && this.props.onSelected(newIcon)
+										this.closeModal && this.closeModal();
+									}}>
+										<i className={prefix+" "+(icon.prefix||"fa")+"-" + icon.name} />
+										<p>{icon.name.replace(/-/g, " ")} ({style})</p>
+									</Col>
                                 }) 
                             }
                         }}
