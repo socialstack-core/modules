3ae6bd4b407b220db3a4db865796bfd36049b2c4
Luke Briggs <luke@kulestar.com>
2022-04-12 20:03:32 +0100

    Relocated password reset endpoints

--- UserController.cs
+++ UserController.cs
@@ -6,9 +6,6 @@ using Api.Eventing;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Api.Startup;
-using Api.PasswordResetRequests;
-using Microsoft.Extensions.Primitives;
-using System.Linq;
 using Microsoft.AspNetCore.Http;
 
 namespace Api.Users
@@ -153,38 +150,6 @@ namespace Api.Users
         }
 
 		/// <summary>
-		/// POST /v1/user/sendverifyemail/
-		/// Sends the user a new token to verify their email.
-		/// </summary>
-		[HttpPost("sendverifyemail")]
-		public async ValueTask ResendVerificationEmail([FromBody] UserPasswordForgot body)
-		{
-			var context = await Request.GetContext();
-
-			var user = context.User;
-
-			if (!string.IsNullOrEmpty(body.Email))
-            {
-				user = await (_service as UserService).Where("Email=?", DataOptions.IgnorePermissions).Bind(body.Email == null ? null : body.Email.Trim()).Last(context);
-			}
-
-			if (user == null)
-			{
-				throw new PublicException("Incorrect user email. Either the account does not exist or the attempt was unsuccessful.", "user_not_found");
-			}
-
-			var result = await (_service as UserService).SendVerificationEmail(context, user);
-
-			if (string.IsNullOrEmpty(result))
-			{
-				throw new PublicException("The attempt was unsuccessful, please try again later", "user_verify_failed");
-			}
-
-			// output the context:
-			await OutputContext(context);
-        }
-
-		/// <summary>
 		/// Impersonate a user by their ID. This is a hard cookie switch. You will loose all admin functionality to make the impersonation as accurate as possible.
 		/// </summary>
 		[HttpGet("{id}/impersonate")]
@@ -276,37 +241,6 @@ namespace Api.Users
 			// Note that this will also generate a new token:
 			await OutputContext(context);
 		}
-
-		/// <summary>
-		/// POST /v1/user/verify/{userid}/{token}
-		/// Attempts to verify the users email. If a password is supplied, the users password is also set.
-		/// </summary>
-		[HttpPost("verify/{userid}/{token}")]
-		public async ValueTask VerifyUser(uint userid, string token, [FromBody] NewPassword newPassword)
-        {
-			var context = await Request.GetContext();
-
-			var user = await (_service as UserService).Where("Id=?", DataOptions.IgnorePermissions).Bind(userid).Last(context);
-
-			if (user == null)
-			{
-				throw new PublicException("Incorrect user. Either the account does not exist or the attempt was unsuccessful.", "user_not_found");
-			}
-
-			var isToeknValid = !string.IsNullOrEmpty(user.EmailVerifyToken) && user.EmailVerifyToken.ToUpper() == token.ToUpper();
-
-			if (!isToeknValid)
-            {
-				throw new PublicException("Incorrect token. Either the token is invalid or the attempt was unsuccessful.", "invalid_token");
-            }
-
-			var result = await (_service as UserService).VerifyEmail(context, user, newPassword?.Password);
-
-			context.User = result;
-
-			// output the context:
-			await OutputContext(context);
-        }
 
     }
 }
