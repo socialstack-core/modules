5aa0932fd83a91af52b1afd2431b75804ada9aba
Karl Barber <karl.barber@4-roads.com>
2022-04-22 09:28:15 +0100

    #misc - added hints to locales

--- Locale.cs
+++ Locale.cs
@@ -1,4 +1,5 @@
-﻿using Api.Users;
+﻿using Api.AutoForms;
+using Api.Users;
 
 
 namespace Api.Translate
@@ -11,16 +12,19 @@ namespace Api.Translate
 		/// The name.
 		/// </summary>
 		[Localized]
+		[Data("hint", "The name of the locale")]
 		public string Name;
 
 		/// <summary>
 		/// Usually a 5 letter locale code e.g. "en_GB". May also be just 2 e.g. "fr".
 		/// </summary>
+		[Data("hint", "The primary locale code, Usually a 5 letter locale code e.g. 'en_GB'. May also be just 2 e.g. 'fr' or client specific such as 'en-en'")]
 		public string Code;
-		
+
 		/// <summary>
 		/// List of comma seperated aliases for mapping request headers or custom client codes
 		/// </summary>
+		[Data("hint", "List of comma seperated aliases for mapping request headers or custom client codes")]
 		public string Aliases;
     }
 
