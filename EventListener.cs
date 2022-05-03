6930e0e1850ae3564b62c050e920ec6bbaf9e18c
Luke Briggs <luke@kulestar.com>
2022-05-03 02:24:21 +0100

    Further decoupling from websocket system

--- EventListener.cs
+++ EventListener.cs
@@ -348,31 +348,3 @@ namespace Api.ContentSync
 
 
 }
-
-public partial class AutoService<T, ID>
-{
-	/// <summary>
-	/// The network room set for the given svc.
-	/// Note that this is null for mapping services - use MappingNetworkRooms on those instead, as they are indexed by SourceId.
-	/// </summary>
-	public NetworkRoomSet<T, ID, ID> StandardNetworkRooms;
-}
-
-
-
-namespace Api.Startup
-{
-	/// <summary>
-	/// The AutoService for mapping types.
-	/// </summary>
-	public partial class MappingService<SRC_ID, TARG_ID>
-	{
-
-		/// <summary>
-		/// The network room set specifically for mappings.
-		/// This is indexed by SourceId.
-		/// </summary>
-		public NetworkRoomSet<Mapping<SRC_ID, TARG_ID>, uint, SRC_ID> MappingNetworkRooms;
-
-	}
-}
