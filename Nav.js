97b9a9e89484712527ddd3c267ce1bc10bccc541
Conan Ablewhite <conan.ablewhite@4-roads.com>
2022-05-26 17:19:33 +0100

    Initial commit

---
+++ Nav.js
@@ -0,0 +1,16 @@
+// TODO
+export default function Nav(props) {
+	
+	return (
+		<div className="nav">
+		</div>
+	);
+}
+
+Nav.propTypes = {
+};
+
+Nav.defaultProps = {
+}
+
+Nav.icon='align-center';
