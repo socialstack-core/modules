16c44e30725841dc9b80865269d073862e335b34
Conan Ablewhite <conan.ablewhite@4-roads.com>
2022-05-26 22:48:46 +0100

    Alert styling moved to submodule; additional options added to Alert component

--- Alert.js
+++ Alert.js
@@ -1,53 +1,131 @@
-/*
-	A bubble like block message. Not a popup or toast message (although it can be used as either via nesting).
-*/
+import { useState } from 'react';
+import CloseButton from 'UI/CloseButton';
 
-export default Alert = props => {
-	let alertClass = "";
-	let alertIcon = "";
+const ALERT_PREFIX = 'alert';
+const DEFAULT_VARIANT = 'info';
 
-	switch (props.type) {
-		case "info":
-			alertClass = 'alert alert-info';
-			alertIcon = 'fas fa-info-circle';
-			break;
+const supportedVariants = [
+	'primary',
+	'secondary',
+	'success',
+	'danger',
+	'warning',
+	'info',
+	'light',
+	'dark'
+];
 
-		case "fail":
-		case "failure":
-		case "failed":
-		case "error":
-		case "danger":
-			alertClass = 'alert alert-danger';
-			alertIcon = 'fas fa-times-circle';
-			break;
+/**
+ * Bootstrap Alert component
+ * @param {any} children			- content
+ * @param {string} variant			- determines appearance (primary / secondary etc.)
+ * @param {string} type				- deprecated (use 'variant' in preference to this)
+ * @param {boolean} showIcon		- set true to display icon
+ * @param {string} icon				- set to override default icon classname
+ * @param {boolean} isDismissable	- set true to display close button
+ */
+export default function Alert(props) {
+	const { children, variant, type, showIcon, icon, isDismissable } = props;
+	const [showAlert, setShowAlert] = useState(true);
 
-		case "warn":
-		case "warning":
-			alertClass = 'alert alert-warning';
-			alertIcon = 'fas fa-exclamation-triangle';
-			break;
+	var alertVariant, iconClass;
 
-		default:
-		case "success":
-		case "successful":
-		case "ok":
-		case "good":
-			alertClass = 'alert alert-success';
-			alertIcon = 'fas fa-check-circle';
-			break;
+	if (type) {
+		alertVariant = type.toLowerCase();
+	} else {
+		alertVariant = variant.toLowerCase();
+    }
+
+	if (!alertVariant || !supportedVariants.includes(alertVariant)) {
+		alertVariant = DEFAULT_VARIANT;
 	}
 
-	return (
-		<div className={alertClass}>
-			<i className={alertIcon} />
-			{props.children}
-		</div>
-	);
+	// resolve aliases / default icon class
+	switch (alertVariant) {
+		//case 'primary':
+		//break;
+
+		//case 'secondary':
+		//break;
+
+		case 'success':
+		case 'successful':
+		case 'ok':
+		case 'good':
+			alertVariant = 'success';
+			iconClass = 'fal fa-check-circle';
+			break;
+
+		case 'danger':
+		case 'fail':
+		case 'failure':
+		case 'failed':
+		case 'error':
+			alertVariant = 'danger';
+			iconClass = 'fal fa-times-circle';
+			break;
+
+		case 'warning':
+		case 'warn':
+			alertVariant = 'warning';
+			iconClass = 'fal fa-exclamation-triangle';
+			break;
+
+		case 'info':
+		case 'information':
+		case 'note':
+			alertVariant = 'info';
+			iconClass = 'fal fa-info-circle';
+			break;
+
+		//case 'light':
+		//break;
+
+		//case 'dark':
+		//break;
+	}
+
+	if (icon) {
+		iconClass = icon;
+	}
+
+	if (iconClass) {
+		iconClass = ALERT_PREFIX + '__icon ' + iconClass;
+	}
+
+	var alertClass = [ALERT_PREFIX];
+	alertClass.push(ALERT_PREFIX + '-' + alertVariant);
+
+	if (isDismissable) {
+		alertClass.push(ALERT_PREFIX + '-dismissable');
+	}
+
+	return (<>
+		{showAlert && <>
+			<div className={alertClass.join(' ')} role="alert">
+				{showIcon && iconClass && <i className={iconClass}></i>}
+				{children}
+				{isDismissable && <>
+					{/*<button type="button" className="btn-close" onClick={() => setShowAlert(false)} aria-label="Close"></button>*/}
+					<CloseButton callback={setShowAlert(false)} />
+				</>}
+			</div>
+		</>}
+	</>);
 }
 
 Alert.propTypes = {
-    children: {default: 'My New Alert'},
-	type: ['error', 'warning', 'success', 'info']
+	variant: ['error', 'warning', 'success', 'info'],
+	isDismissable: 'bool',
+	showIcon: 'bool',
+	icon: 'string'
 };
 
-Alert.icon='exclamation-circle';
+Alert.defaultProps = {
+	children: 'My New Alert',
+	variant: DEFAULT_VARIANT,
+	isDismissable: false,
+	showIcon: true
+}
+
+Alert.icon = 'exclamation-circle';
