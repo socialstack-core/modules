06a55bcf0b94bed37093503ddcd5f0c1409ec7cd
Conan Ablewhite <conan.ablewhite@4-roads.com>
2022-05-26 17:19:43 +0100

    Initial commit

---
+++ NavBar.js
@@ -0,0 +1,16 @@
+// TODO
+export default function NavBar(props) {
+
+	return (
+		<div className="navbar">
+		</div>
+	);
+}
+
+NavBar.propTypes = {
+};
+
+NavBar.defaultProps = {
+}
+
+NavBar.icon = 'align-center';
