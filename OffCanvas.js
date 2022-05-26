9ba86d9906097c4901afd918cd15a1e0db201404
Conan Ablewhite <conan.ablewhite@4-roads.com>
2022-05-26 17:19:52 +0100

    Initial commit

---
+++ OffCanvas.js
@@ -0,0 +1,16 @@
+// TODO
+export default function OffCanvas(props) {
+
+	return (
+		<div className="offcanvas">
+		</div>
+	);
+}
+
+OffCanvas.propTypes = {
+};
+
+OffCanvas.defaultProps = {
+}
+
+OffCanvas.icon = 'align-center';
