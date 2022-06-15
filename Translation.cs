5268c91c28d63a2eea5abb986ed632d7b10265e7
Karl Barber <karl.barber@4-roads.com>
2022-06-15 12:42:20 +0100

    #misc - added hints for translations

--- Translation.cs
+++ Translation.cs
@@ -1,4 +1,5 @@
 using System;
+using Api.AutoForms;
 using Api.Database;
 using Api.Translate;
 using Api.Users;
@@ -16,13 +17,16 @@ namespace Api.Translate
         /// E.g. "UI/AboutUs" - the exact JS module name that this translation is for.
         /// </summary>
         [DatabaseField(Length = 200)]
+        [Data("hint", "The ux module exposing the value (where it exposed within the site)")]
 		public string Module;
-		
+
 		/// <summary>The original text string.</summary>
+		[Data("hint", "The baseline/original text to find within the component (normally no need to change this)")]
 		public string Original;
 		
 		/// <summary>The translation.</summary>
 		[Localized]
+        [Data("hint", "The translated text to replace the original value (site restart required if changed !)")]
 		public string Translated;
 		
 	}
