f00980e387bc930a972dd475ea5802aa0a5f0c95
Conan Ablewhite <conan.ablewhite@4-roads.com>
2022-06-24 11:32:16 +0100

    back to login button now outlined; strings made localisable

--- ForgotPassword.js
+++ ForgotPassword.js
@@ -1,6 +1,5 @@
 ﻿import Input from 'UI/Input';
 import Form from 'UI/Form';
-import Row from 'UI/Row';
 
 /*
 * Provide an email address in order to email a reset link to.
@@ -12,16 +11,16 @@ export default class ForgotPassword extends React.Component {
 				{
 					this.state.success ? (
 						<div>
-							{this.props.successMessage || 'Your request has been submitted - if an account exists with this email address it will receive an email shortly.'}
+						{this.props.successMessage || `Your request has been submitted - if an account exists with this email address it will receive an email shortly.`}
 						</div>
 					) : [
 						<p>
-							{this.props.prompt || 'Please provide your email address and we\'ll email you a reset link.'}
+							{this.props.prompt || `Please provide your email address and we'll email you a reset link.`}
 						</p>,
 						<Form
-							failedMessage={this.props.failedMessage || "We weren't able to send the link. Please try again later."}
-							submitLabel={this.props.submitLabel || "Send me a link"}
-							loadingMessage={this.props.loadingMessage || "Sending.."}
+							failedMessage={this.props.failedMessage || `We weren't able to send the link. Please try again later.`}
+							submitLabel={this.props.submitLabel || `Send me a link`}
+							loadingMessage={this.props.loadingMessage || `Sending..`}
 							action="passwordresetrequest"
 							onSuccess={response => {
 								this.setState({
@@ -30,11 +29,13 @@ export default class ForgotPassword extends React.Component {
 								this.props.onSuccess && this.props.onSuccess();
 							}}
 						>
-							<Input name="email" placeholder="Email address" validate="Required" />
+							<Input name="email" placeholder={`Email address`} validate="Required" />
 						</Form>
 				]}
 				{this.props.children || <div className="form-group">	
-					<a className = "btn btn-primary" href={this.props.loginLink || "/login"}>Back to login</a>		
+					<a className="btn btn-outline-primary" href={this.props.loginLink || "/login"}>
+						{`Back to login`}
+					</a>
 				</div>}
 		</div>;
 	}
