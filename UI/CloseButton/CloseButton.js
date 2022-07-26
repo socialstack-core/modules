4bf7c9a50e670570ce645dbf3dd2eab3934d23ad
Luke Briggs <luke@kulestar.com>
2022-07-26 12:00:30 +0100

    CloseButton now supports isSmall prop and classnames

--- UI/CloseButton/CloseButton.js
+++ UI/CloseButton/CloseButton.js
@@ -4,17 +4,27 @@ const CLOSE_PREFIX = 'btn-close';
  * Bootstrap CloseButton component
  * @param {boolean} isDisabled	- set true to disable button
  * @param {boolean} isLight		- set true to display light version
+ * @param {boolean} isSmall		- set true to reduce size
  * @param {string} label		- accessibility label
+ * @param {string} className	- optional additional classname(s)
  * @param {function} callback	- onClick handler
  */
 export default function CloseButton(props) {
-	var { isDisabled, isLight, label, callback } = props;
+	var { isDisabled, isLight, isSmall, label, callback, className } = props;
 
 	var btnCloseClass = [CLOSE_PREFIX];
 
 	if (isLight) {
 		btnCloseClass.push(CLOSE_PREFIX + '-white');
 	}
+
+	if (isSmall) {
+		btnCloseClass.push(CLOSE_PREFIX + '--sm');
+    }
+
+	if (className) {
+		btnCloseClass.push(className);
+    }
 
 	label = label || `Close`;
 
