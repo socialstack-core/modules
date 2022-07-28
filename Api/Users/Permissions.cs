d57956860983b24a632065c3c682eab2ad784cdd
Luke Briggs <luke@kulestar.com>
2022-07-28 20:15:50 +0100

    As SS core is being used by less open social projects, the default user permissions are now favouring safety first.

--- Api/Users/Permissions.cs
+++ Api/Users/Permissions.cs
@@ -20,6 +20,15 @@ namespace Api.Users
 			// Hook the default role setup. It's done like this so it can be removed by a plugin if wanted.
 			Events.CapabilityOnSetup.AddEventListener((Context context, object source) =>
 			{
+				// Remove public viewing (as it's enabled by default):
+				Roles.Guest.Revoke("user_load", "user_list");
+				Roles.Public.Revoke("user_load", "user_list");
+				Roles.Member.Revoke("user_load", "user_list");
+				
+				Roles.Guest.If("IsSelf()").ThenGrant("user_load", "user_list");
+				Roles.Public.If("IsSelf()").ThenGrant("user_load", "user_list");
+				Roles.Member.If("IsSelf()").ThenGrant("user_load", "user_list");
+
 				// Public - the role used by anonymous users.
 				return new ValueTask<object>(source);
 			});
