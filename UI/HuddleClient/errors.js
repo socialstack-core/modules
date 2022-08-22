09f3b6759653466d2948c8d736a4a7a9d76e578e
Luke Briggs <luke@kulestar.com>
2022-08-22 17:22:21 +0100

    Added error messages for the HuddleClient

---
+++ UI/HuddleClient/errors.js
@@ -0,0 +1,9 @@
+export default {
+	'unspecified': `Unable to load meeting due to an unspecified error. Please try again later.`,
+	'huddle_not_found': `This meeting wasn't found.`,
+	'ended': `This meeting has ended.`,
+	'no_seats': `The meeting subscription has ran out of capacity. Please check with the site administrator.`,
+	'already_recording': `The meeting is already being recorded.`,
+	'subscription_required': `A Huddle subscription is required for this action.`,
+	'no_recording_info': `This site doesn't have storage configured for recording yet. Please ask the site administrator to check their Huddle subscription configuration.`
+};
