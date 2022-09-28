239ef68c509896e7e651969f3c7a1c2524e17355
Luke Briggs <luke@kulestar.com>
2022-09-28 16:16:58 +0100

    UI/Token made much more robust - it can now accept a wider variety of inputs and when all fail it displays some help text

--- UI/Token/Token.js
+++ UI/Token/Token.js
@@ -2,74 +2,83 @@ import Content from 'UI/Content';
 import { useSession, useRouter } from 'UI/Session';
 import { useContent } from 'UI/Content';
 
-var modes = {'content': 1, 'session': 1, 'url': 1, 'customdata': 1, 'primary':1, 'theme':1};
+var modes = { 'content': 1, 'session': 1, 'url': 1, 'customdata': 1, 'primary': 1, 'theme': 1 };
 
-export function TokenResolver(props){
+export function TokenResolver(props) {
 	return props.children(useTokens(props.value));
 }
 
-export function useTokens(str){
-	var {session} = useSession();
+export function useTokens(str) {
+	var { session } = useSession();
 	var localContent = useContent();
-	var {pageState} = useRouter();
-	
-	return (str || '').toString().replace(/\$\{(\w|\.)+\}/g, function(textToken) {
+	var { pageState } = useRouter();
+
+	return handleString(str, session, localContent, pageState);
+}
+
+function handleString(str, session, localContent, pageState) {
+	return (str || '').toString().replace(/\$\{(\w|\.)+\}/g, function (textToken) {
 		var fields = textToken.substring(2, textToken.length - 1).split('.');
-		
+
 		var mode = '';
 		var first = fields[0].toLowerCase();
-		if(modes[first]){
+		if (modes[first]) {
 			fields.shift();
 			mode = first;
 		}
-		
-		return resolveValue(mode,fields,session, localContent, pageState);
+
+		return resolveValue(mode, fields, session, localContent, pageState);
 	});
 }
 
-export function resolveValue(mode, fields, session, localContent, pageState){
+export function resolveValue(mode, fields, session, localContent, pageState) {
 	var token;
-	if(mode == "content"){
+
+	if (mode) {
+		mode = mode.toLowerCase();
+	}
+
+	if (mode == "content") {
 		token = localContent ? localContent.content : null;
-	}else if(mode == "url"){
-		if(!pageState || !pageState.tokenNames){
+	} else if (mode == "url") {
+		if (!pageState || !pageState.tokenNames) {
 			return '';
 		}
 		var index = pageState.tokenNames.indexOf(fields.join('.'));
 		return (index == null || index == -1) ? '' : pageState.tokens[index];
-	}else if(mode == "theme"){
+	} else if (mode == "theme") {
 		return 'var(--' + fields.join('-') + ')';
-	}else if(mode == "customdata" || mode == "primary"){
+	} else if (mode == "customdata" || mode == "primary") {
 		// Used by emails mostly. Passes through via primary object.
-		if(!pageState || !pageState.po){
+		if (!pageState || !pageState.po) {
 			return '';
 		}
 		token = pageState.po;
-	}else{
+	} else {
 		token = session;
 	}
-	
-	if(!token){
+
+	if (!token) {
 		return '';
 	}
-	
+
 	var fields = fields;
-	
-	if(Array.isArray(fields) && fields.length){
-		try{
-			for(var i=0;i<fields.length;i++){
+
+	if (Array.isArray(fields) && fields.length) {
+		try {
+			for (var i = 0; i < fields.length; i++) {
 				token = token[fields[i]];
-				if(token === undefined || token === null){
+				if (token === undefined || token === null) {
 					return '';
 				}
 			}
-		}catch(e){
+		} catch (e) {
 			console.log(e);
 			token = null;
 		}
-		
+
 		return token;
-	}else if(typeof fields == 'string'){
+	} else if (typeof fields == 'string') {
 		return token[fields];
 	}
 }
@@ -79,18 +88,33 @@ export function resolveValue(mode, fields, session, localContent, pageState){
 * Available values either come from the primary type on the page, or the global state. The RTE establishes the options though.
 */
 
-export default function Token (props) {
+export default function Token(props) {
 	// If editor, display the thing and its children:
-	var {session} = useSession();
+	var { session } = useSession();
 	var localContent = useContent();
-	var {pageState} = useRouter();
-	
-	if(props._rte){
+	var { pageState } = useRouter();
+
+	if (props._rte) {
 		return <span className="context-token" ref={props.rootRef}>
 			{props.children}
 		</span>;
 	}
 	
+	if (!props.mode) {
+		// Resolve from child string if there is one.
+		var str = props.s || props.children;
+	
+		if(Array.isArray(str)){
+			str = str.length ? str[0] : null;
+		}
+		
+		if (typeof str == 'string') {
+			return handleString(str.indexOf('$') == -1 ? '${' + str + '}' : str, session, localContent, pageState);
+		}
+
+		return '{Incorrect token, see wiki}';
+	}
+
 	// Resolved value. No wrapper - just plain value.
 	return resolveValue(props.mode, props.fields, session, localContent, pageState);
 }
@@ -100,44 +124,63 @@ Token.editable = {
 	onLoad: nodeInfo => {
 		// Convert mode and fields to children root
 		const data = nodeInfo.d || {};
-		
-		let str = data.mode || '';
-		let fieldStr = data.fields ? data.fields.join('.') : '';
-		
-		if(str && fieldStr){
-			str += '.' + fieldStr;
-		}else{
-			str += fieldStr;
+
+		if (data && data.mode) {
+			let str = data.mode || '';
+			let fieldStr = data.fields ? data.fields.join('.') : '';
+
+			if (str && fieldStr) {
+				str += '.' + fieldStr;
+			} else {
+				str += fieldStr;
+			}
+
+			if (!str) {
+				str = 'unnamed token';
+			}
+
+			nodeInfo.r = {
+				children: { s: str }
+			};
 		}
-		
-		if(!str){
-			str='unnamed token';
-		}
-		
-		nodeInfo.r = {
-			children: {s: str}
-		};
-		
-		nodeInfo.d = null;
 	},
 	onSave: nodeInfo => {
 		// Ensure children root is pure text:
-		var childRoot = nodeInfo.c && typeof nodeInfo.c === 'string';
-		
-		if(nodeInfo.c && typeof nodeInfo.c === 'string'){
+		var childRoot;
+
+		if (nodeInfo.r && nodeInfo.r.children) {
+			childRoot = nodeInfo.r.children;
+		} else if (nodeInfo.c) {
+			childRoot = nodeInfo.c;
+		}
+
+		if (childRoot) {
+			if (Array.isArray(childRoot)) {
+				childRoot = childRoot.length ? childRoot[0] : null;
+			}
+
+			if (childRoot.s) {
+				childRoot = childRoot.s;
+			}
+		}
+
+		if (childRoot && typeof childRoot === 'string') {
 			// good! The root is a pure text node.
-			var pieces = nodeInfo.c.split('.');
-			var first = pieces[0].toLowerCase();
-			
+			var pieces = childRoot.split('.');
+			var first = pieces[0].trim();
+
 			nodeInfo.d = {};
-			
-			if(modes[first]){
+
+			if (modes[first]) {
 				nodeInfo.d.mode = first;
 				pieces.shift();
 			}
-			
+
+			for (var i = 0; i < pieces.length; i++) {
+				pieces[i] = pieces[i].trim();
+			}
+
 			nodeInfo.d.fields = pieces;
-			delete nodeInfo.c;
 		}
 	}
 };
