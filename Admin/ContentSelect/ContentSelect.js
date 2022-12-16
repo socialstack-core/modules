77bd130df778bd173a13377c807fefdff0db30e0
kurt-hodges-4-roads <kurt.hodges@4-roads.com>
2022-12-16 13:15:04 +0000

    Added buttons to create and edit related content without needing the leave the page. Todo: add props to enable/disable these buttons.

--- Admin/ContentSelect/ContentSelect.js
+++ Admin/ContentSelect/ContentSelect.js
@@ -2,6 +2,8 @@ import webRequest from 'UI/Functions/WebRequest';
 import omit from 'UI/Functions/Omit';
 import Input from 'UI/Input';
 import Search from 'UI/Search';
+import AutoForm from 'Admin/AutoForm';
+import Modal from 'UI/Modal';
 
 /**
  * Dropdown to select a piece of content.
@@ -10,14 +12,31 @@ export default class ContentSelect extends React.Component {
 	
 	constructor(props){
 		super(props);
+
 		this.state = {
-			selected: null
+			searchSelected: null,
+			selected: this.getDefaultValue(props),
+			showCreateOrEditModal: false
 		};
 		this.load(props, true);
 	}
 	
 	componentWillReceiveProps(props){
 		this.load(props);
+	}
+
+	getDefaultValue(props) {
+		var value = null;
+
+		if (props.defaultValue) {
+			if (props.defaultValue.id) {
+				value = props.defaultValue.id;
+			} else {
+				value = props.defaultValue;
+			}
+		}
+
+		return value;
 	}
 	
 	load(props, first){
@@ -26,12 +45,12 @@ export default class ContentSelect extends React.Component {
 			if(value){
 				webRequest(props.contentType.toLowerCase() + '/' + value).then(response => {
 					this.setState({
-						selected: response ? response.json : null
+						searchSelected: response ? response.json : null
 					});
 				});
 			}else if(!first){
 				this.setState({
-					selected: null
+					searchSelected: null
 				});
 			}
 		}else{
@@ -40,19 +59,34 @@ export default class ContentSelect extends React.Component {
 				if (!props.hideDefaultValue) {
 					all.unshift(null);
 				}
-				this.setState({all});
+				var selected = this.state.selected ? this.state.selected : this.getDefaultValue(props);
+				this.setState({all, selected: selected });
 			});
 		}
+	}
+
+	onChange(e) {
+		if (this.props.onChange) {
+			this.props.onChange(e);
+		}
+
+		var value = null;
+
+		if (e && e.target && e.target.value && e.target.value > 0) {
+			value = e.target.value;
+		}
+
+		this.setState({ selected: value });
 	}
 		
 	render(){
 		if(this.props.search){
 			
-			var {selected} = this.state;
+			var {searchSelected} = this.state;
 			var title = '';
 			
-			if(selected){
-				title = selected.title || selected.firstName || selected.username || selected.name || selected.url;
+			if(searchSelected){
+				title = searchSelected.title || searchSelected.firstName || searchSelected.username || searchSelected.name || searchSelected.url;
 			}
 			
 			var value = this.props.defaultValue || this.props.value;
@@ -70,7 +104,7 @@ export default class ContentSelect extends React.Component {
 									return v;
 								}
 								
-								return this.state.selected ? this.state.selected.id : '0';
+								return this.state.searchSelected ? this.state.searchSelected.id : '0';
 							}
 						}
 					}
@@ -78,12 +112,12 @@ export default class ContentSelect extends React.Component {
 				name={this.props.name} />
 				<Search for={this.props.contentType} field={this.props.search} limit={5} placeholder={"Search for a " + this.props.contentType + ".."} onFind={entry => {
 					this.setState({
-						selected: entry
+						searchSelected: entry
 					});
 				}}/>
 				{
 					<div className="selected-content">
-						{selected ? title : (
+						{searchSelected ? title : (
 							value ? 'Item #' + value : 'None selected'
 						)}
 					</div>
@@ -101,22 +135,80 @@ export default class ContentSelect extends React.Component {
 			window.matchMedia('(max-height: 752px) and (pointer: coarse) and (orientation: landscape)').matches) {
 			noSelection = mobileNoSelection;
 		}
-		
-		return (<Input {...omit(this.props, ['contentType'])} type="select">{
-			all ? all.map(content => {
-				if(!content){
-					return <option value={'0'}>{noSelection}</option>;
-				}
-				
-				var title = content.title || content.firstName || content.username || content.name || content.url;
-				
-				return <option value={content.id}>{
-					'#' + content.id + (title ? ' - ' + title : '(no identified title)')
-				}</option>;
-			}) : [
-			]
-		}</Input>);
-		
+
+		return (<div className="content-select">
+			<Input {...omit(this.props, ['contentType'])} type="select" defaultValue={this.state.selected ? this.state.selected : null} onChange={e => { this.onChange(e); }}>{
+				all ? all.map(content => {
+					if(!content){
+						return <option value={'0'}>{noSelection}</option>;
+					}
+					
+					var title = content.title || content.firstName || content.username || content.name || content.url;
+					
+					return <option value={content.id}>{
+						'#' + content.id + (title ? ' - ' + title : '(no identified title)')
+					}</option>;
+				}) : [
+				]
+			}</Input>
+			
+			<button 
+				className="btn btn-secondary btn-content-select-action btn-add-content"
+				onClick={e => {
+					e.preventDefault();
+					this.setState({ showCreateOrEditModal: true });
+				}}
+			>
+				{"New " + this.props.label}
+			</button>
+
+			{this.state.selected &&
+				<button 
+					className="btn btn-secondary btn-content-select-action btn-edit-content"
+					onClick={e => {
+						e.preventDefault();
+						this.setState({ showCreateOrEditModal: true, entityToEditId: value });
+					}}
+				>
+					{"Edit " + this.props.label}
+				</button>
+			}
+			
+			{this.state.selected &&
+				<a
+					className="btn btn-secondary btn-content-select-action btn-visit-content"
+					href={"/en-admin/" + this.props.contentType.toLowerCase() + "/" + this.state.selected}
+				>
+					{"Visit " + this.props.label}
+				</a>
+			}
+
+			{this.state.showCreateOrEditModal &&
+				<Modal
+					title={"Create New " + this.props.label}
+					onClose={() => {
+						this.setState({ showCreateOrEditModal: false, entityToEditId: null });
+					}}
+					visible
+					isExtraLarge
+				>
+					<AutoForm 
+						endpoint={this.props.contentType.toLowerCase()} 
+						singular={this.props.label} 
+						plural={this.props.label + "s"}
+						id={this.state.selected ? this.state.selected : null}
+						onActionComplete={entity => {
+							this.setState({ showCreateOrEditModal: false, entityToEditId: null, selected: entity });
+							this.load(this.props);
+
+							/* 
+								Todo: Does the user expect the parent entity to be saved upon completing this action? 
+								Currently you still need to press 'Save and Publish' on the parent form to save the value of this field.
+							*/
+						}} />
+				</Modal>
+			}
+		</div>);
 	}
 	
 }
