1c51b1abdec28ba354611724c90696ad3f7332f5
kurt-hodges-4-roads <kurt.hodges@4-roads.com>
2022-12-16 13:19:22 +0000

    Allow for AutoEdit and List to be passed entity vua url praram instead of prop

--- Admin/Layouts/AutoEdit/AutoEdit.js
+++ Admin/Layouts/AutoEdit/AutoEdit.js
@@ -4,6 +4,7 @@ import {TokenResolver} from 'UI/Token';
 import AutoForm from 'Admin/AutoForm';
 import Default from 'Admin/Layouts/Default';
 import omit from 'UI/Functions/Omit';
+import {useRouter} from 'UI/Session';
 
 
 export default class AutoEdit extends React.Component {
@@ -13,17 +14,33 @@ export default class AutoEdit extends React.Component {
 	}
 	
 	render(){
+		var { pageState } = useRouter();
 		var id = this.props.id;
+		var endpoint = this.props.endpoint;
+		var entityName = this.props.singular;
+		var entityNamePlural = this.props.plural;
 		
 		// legacy support - mapping old urlTokens:
 		if(typeof id != 'string' && id.type == 'urlToken'){
 			id = '${url.' + id.name + '}';
 		}
+
+		if (!endpoint && pageState.tokenNames) {
+			for(var i = 0; i < pageState.tokenNames.length; i++) {
+				if (pageState.tokenNames[i] == 'entity') {
+					endpoint = pageState.tokens[i];
+					entityName = endpoint.replace(/([A-Z])/g, ' $1').trim();
+					entityNamePlural = entityName + "s";
+				} else if (pageState.tokenNames[i] == 'id') {
+					id = pageState.tokens[i];
+				}
+			}
+		}
 		
 		return <TokenResolver value={id}>
 			{id => 
 				<Default>
-					<AutoForm {...(omit(this.props, ['children']))} id={id} />
+					<AutoForm {...(omit(this.props, ['children']))} id={id} endpoint={endpoint} singular={entityName} plural={entityNamePlural} />
 					{this.props.children}
 				</Default>	
 			}
--- Admin/Layouts/List/List.js
+++ Admin/Layouts/List/List.js
@@ -2,6 +2,7 @@ import Tile from 'Admin/Tile';
 import AutoList from 'Admin/AutoList';
 import Loop from 'UI/Loop';
 import Default from 'Admin/Layouts/Default';
+import {useRouter} from 'UI/Session';
 
 
 export default class List extends React.Component {
@@ -11,9 +12,24 @@ export default class List extends React.Component {
 	}
 	
 	render(){
+		var { pageState } = useRouter();
+		var endpoint = this.props.endpoint;
+		var entityName = this.props.singular;
+		var entityNamePlural = this.props.plural;
+
+		if (!endpoint && pageState.tokenNames) {
+			for(var i = 0; i < pageState.tokenNames.length; i++) {
+				if (pageState.tokenNames[i] == 'entity') {
+					endpoint = pageState.tokens[i];
+					entityName = endpoint.replace(/([A-Z])/g, ' $1').trim();
+					entityNamePlural = entityName + "s";
+				}
+			}
+		}
+
 		return (
 			<Default>
-				<AutoList endpoint={this.props.endpoint} {...this.props} title={'Edit or create ' + this.props.plural} create={!this.props.noCreate} searchFields={this.props.searchFields || ['title']} />
+				<AutoList endpoint={endpoint} {...this.props} singular={entityName} title={'Edit or create ' + entityNamePlural} create={!this.props.noCreate} searchFields={this.props.searchFields || ['title']} />
 				{this.props.children}
 			</Default>	
 		);
