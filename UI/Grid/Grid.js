71a87f1a138be27731a311795d87f29e7efc4d16
Conan Ablewhite <conan.ablewhite@4-roads.com>
2023-02-06 15:44:56 +0000

    UI/Grid: columns now use auto-width (.col) class by default (WIP)

--- UI/Grid/Grid.js
+++ UI/Grid/Grid.js
@@ -1,51 +1,78 @@
 import Row from 'UI/Row';
-import Column from 'UI/Column';
+
+const GRID_DEFAULT_NOGUTTERS = false;
+const GRID_DEFAULT_ROWS = 1;
+const GRID_DEFAULT_COLUMNS = 2;
+const GRID_DEFAULT_LAYOUT = 'auto';
 
 export default function Grid(props) {
+	var noGutters = (props.noGutters == undefined) ? GRID_DEFAULT_NOGUTTERS : props.noGutters;
+	var rows = props.rows || GRID_DEFAULT_ROWS;
+	var columns = props.columns || GRID_DEFAULT_COLUMNS;
+	var layout = props.layout || GRID_DEFAULT_LAYOUT;
 	
-	var rows = props.rows || 1;
-	var columns = props.columns || 3;
-	
-	if(rows <= 0 || columns <= 0){
+	if (rows <= 0 || columns <= 0){
 		return null;
 	}
 	
-	if(columns > 12){
+	if (columns > 12){
 		columns = 12;
 	}
-	
-	var colSize = Math.floor(12 / columns);
-	
+
 	var rowSet = [];
 	var i = 0;
-	
-	for(var r=0;r<rows;r++){
-		var colSet = [];
-		
-		for(var c=0;c<columns;c++){
-			var colContent = props['c' + (i++)];
-			
-			if(colContent === undefined){
-				colContent = null; // react requires null if this happens
+
+	if (layout == 'manual') {
+		// TODO
+	} else {
+		// auto layout
+		for (var r = 0; r < rows; r++) {
+			var colSet = [];
+
+			for (var c = 0; c < columns; c++) {
+				var colContent = props['c' + (i++)];
+
+				if (colContent === undefined) {
+					colContent = null; // react requires null if this happens
+				}
+
+				colSet.push(<>
+					<div className="col">
+						{colContent}
+					</div>
+				</>);
 			}
-			
-			colSet.push(<Column size={colSize}>{colContent}</Column>);
+
+			rowSet.push(<>
+				<Row noGutters={noGutters ? true : undefined}>
+					{colSet}
+				</Row>
+			</>);
 		}
-		
-		rowSet.push(<Row>{colSet}</Row>);
-	}
-	
+
+    }
+
 	return rowSet;
 }
 
 Grid.propTypes = {
 	rows: 'int',
-	columns: 'int'
+	//columns: 'int',
+	columns: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+	noGutters: 'boolean',
+	/*
+	layout: [
+		{ name: `Automatic`, value: 'auto' },
+		{ name: `Manual`, value: 'manual' }
+	]
+	 */
 };
 
 Grid.defaultProps = {
-	columns: 3,
-	rows: 1
+	rows: GRID_DEFAULT_ROWS,
+	columns: GRID_DEFAULT_COLUMNS,
+	noGutters: GRID_DEFAULT_NOGUTTERS,
+	//layout: GRID_DEFAULT_LAYOUT
 }
 
 Grid.onEditorUpdate = (node, rte) => {
@@ -53,17 +80,17 @@ Grid.onEditorUpdate = (node, rte) => {
 		return;
 	}
 	
-	var w = parseInt(node.props.columns) || 1;
-	var h = parseInt(node.props.rows) || 3;
+	var w = parseInt(node.props.columns) || GRID_DEFAULT_COLUMNS;
+	var h = parseInt(node.props.rows) || GRID_DEFAULT_ROWS;
 	
 	var cellCount = w * h;
-	
+
 	// Ensure there are cellCount roots:
 	if(!node.roots){
 		node.roots = {};
 	}
 	
-	for(var i=0;i<cellCount;i++){
+	for(var i=0; i < cellCount; i++){
 		if(!node.roots['c' + i]){
 			rte.addEmptyRoot(node, 'c' + i);
 		}
