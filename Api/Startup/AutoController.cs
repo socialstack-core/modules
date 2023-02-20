d057124c235994de59d524300a279c285cd47cd0
kurt-hodges-4-roads <kurt.hodges@4-roads.com>
2023-02-20 11:50:38 +0000

    Dispatch new EndpointListEntry event

--- Api/Startup/AutoController.cs
+++ Api/Startup/AutoController.cs
@@ -250,8 +250,8 @@ public partial class AutoController<T,ID> : ControllerBase
 			return await _service.GetResults(ctx, filt, async (Context ctx2, T result, int index, object src, object srcB) => {
 
 				var _onResult = src as Func<T, int, ValueTask>;
+				result = await _service.EventGroup.EndpointListEntry.Dispatch(context, result, Response);
 				await _onResult(result, index);
-
 			}, onResult, null);
 
 		}, writer, Response.Body, includes, filter.IncludeTotal);
