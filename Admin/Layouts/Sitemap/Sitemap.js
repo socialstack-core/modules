c9854edcdec161cb8fc517b9c47d76e713126dd9
Luke Briggs <luke@kulestar.com>
2023-03-15 22:07:54 +0000

    Admin/Layouts: Sitemap updated to avoid creating unreachable pages

--- Admin/Layouts/Sitemap/Sitemap.js
+++ Admin/Layouts/Sitemap/Sitemap.js
@@ -9,255 +9,271 @@ export default function Sitemap(props) {
 	const [ sitemap, setSitemap ] = useState(false);
 	const [ showConfirmModal, setShowConfirmModal ] = useState(false);
 	const { setPage } = useRouter();
-	var entityData = [];
-
-	function includePage(target, source, path) {
-		target.createdUtc = source.createdUtc;
-		target.description = source.description;
-		target.editedUtc = source.editedUtc;
-		target.id = source.id;
-		target.isDraft = source.isDraft;
-		target.preferIfLoggedIn = source.preferIfLoggedIn;
-		target.revision = source.revision;
-		target.revisionId = source.revisionId;
-		target.title = source.title;
-		target.url = path;
-		target.userId = source.userId;
-	}
 
 	function buildSitemap(results) {
-		const base = { children: [] };
+		var map = [];
+
+		var rootPage = {
+			children: {},
+			pages: [],
+			redirection: null,
+			urlTokenNames: null,
+			urlTokenNamesJson: null,
+			urlTokens: null,
+			wildcard: null
+		};
 
 		for (const node of results) {
-			const pageUrl = '/' + node.url.replace(/^\/|\/$/g, '');
-			const path = pageUrl.match(/\/[^\/]+/g);
-			let curr = base;
+			var pg = addPage(node.url, rootPage);
 
-			if (!path) {
-				// root
-				base.children.push({
-					children: []
-				});
-				includePage(curr.children[curr.children.length - 1], node, '/');
+			// skipped?
+			if (pg == null) {
+				continue;
+			}
+
+			if (pg.pages == null) {
+				pg.pages = [];
+			}
+
+			// if there are >1, ensure that the page at the start of the set is the one that is favoured (if any)
+			if (node.preferIfLoggedIn) {
+				pg.pages.unshift(node);
 			} else {
-				path.forEach((e, i) => {
-					const currPath = path.slice(0, i + 1).join("");
-					const child = curr.children.find(e => e.url === currPath);
-
-					if (child) {
-
-						// found an existing node with the same path
-						if (node.url == currPath) {
-
-							// if preferIfLoggedIn varies, treat these as two variations of the same page
-							if (node.preferIfLoggedIn != child.preferIfLoggedIn) {
-								var loginVariant = { children: [] };
-								includePage(loginVariant, node, currPath);
-								child.children.push(loginVariant);
-							} else {
-								// otherwise update existing link info
-								includePage(child, node, currPath);
-                            }
-
-                        }
-
-						curr = child;
-					}
-					else {
-						var grandchildren = [];
-
-						// check - is this step a valid URL?
-						if (!results.filter(page => '/' + page.url.replace(/^\/|\/$/g, '') == currPath).length) {
-							return;
-                        }
-
-						// check for params
-						var params = pageUrl.match(/{([^}]+)}/g);
-						var paramInfo = [];
-
-						if (params && params.length == 1) {
-							paramInfo = params[0].slice(1, -1).split(".");
-						}
-
-						// check - should have two elements (e.g. user / id)
-						if (paramInfo.length == 2) {
-							// has params - inject each valid URL
-							var pEntity = paramInfo[0];
-							var pKey = paramInfo[1];
-
-							entityData
-								.filter(data => data.type.toLowerCase() == pEntity)
-								.forEach(entity => {
-									var output = {
-										isPage: true,
-										id: entity[pKey],
-										url: currPath.replace("{" + pEntity + '.' + pKey + "}", entity[pKey]),
-										title: entity.name || entity.username || entity.email || entity.title || '',
-										description: entity.subTitle || entity.description || '',
-										children: []
-									};
-
-									// determine where to find data to display for this entity
-									var filteredNode = Object.entries(entity).filter(entry => {
-										const [key, value] = entry;
-										var lowerKey = key.toLowerCase();
-
-										return (typeof (entry[key]) == 'string') &&
-											lowerKey !== 'type' &&
-											lowerKey !== 'key' &&
-											!lowerKey.endsWith('ref') &&
-											!lowerKey.endsWith('json');
-									});
-
-									var basicInfo = filteredNode.length ? filteredNode[0][1] : `${pEntity} #${entity[pKey]}`;
-
-									if (output.title == '') {
-										output.title = basicInfo;
-									} else if (output.description == '' && basicInfo != output.title) {
-										output.description = basicInfo;
-									}
-
-									grandchildren.push(output);
-								});
-						}
-
-						curr.children.push({
-							children: grandchildren
-						});
-						curr = curr.children[curr.children.length - 1];
-						includePage(curr, node, currPath);
-					}
-				});
-
+				pg.pages.push(node);
 			}
+
+			map.push({
+				pageId: node.id,
+				url: node.url
+			});
 
 		}
 
-		base.children.forEach(node => {
-			if (node.children.length) {
-				node.children.sort((a, b) => (a.url > b.url) ? 1 : -1);
-			}
-		})
-
-		return base.children;
-    }
-
-	async function handleFetch(url) {
-		try{
-			const resp = await webRequest(url);
-			return resp && resp.json ? resp.json.results : undefined;
-		}catch(e){
-			console.warn(e);
-			return null;
-		}
+		return rootPage;
 	}
 
-	async function reduceFetch(acc, curr) {
-		const prev = await acc;
+	function addPage(url, rootPage) {
 
-		if (prev) {
-			entityData = [...entityData, ...prev]
+		if (!url || !url.length) {
+			return null;
 		}
 
-		return handleFetch(curr);
+		var tokenSet = [];
+
+		// strip leading and trailing slashes
+		if (url[0] == '/') {
+			url = url.substring(1);
+		}
+
+		if (url.length && url[url.length - 1] == '/') {
+			url = url.substring(0, url.length - 1);
+		}
+
+		// url parts
+		var pg = rootPage;
+
+		if (url.length) {
+			var parts = url.split('/');
+			var skip = false;
+
+			for (var i = 0; i < parts.length; i++) {
+				var part = parts[i];
+				var token = null;
+
+				if (part.length) {
+
+					// old-style tokens
+					if (part[0] == ':') {
+						token = part.substring(1);
+						tokenSet.push({
+							rawToken: token
+						});
+
+					} else if (part[0] == '{') {
+						// new-style tokens
+						token = (part[part.length - 1] == '}') ?
+							part.substring(1, part.length - 1) : part.substring(1);
+
+						var dotIndex = token.indexOf('.');
+
+						if (dotIndex != -1) {
+							var contentType = token.substring(0, dotIndex);
+							var fieldName = token.substring(dotIndex + 1);
+
+							/*
+							var type = Api.Database.ContentTypes.GetType(contentType);
+	
+							if (type == null) {
+								//Console.WriteLine("[WARN] Bad page URL using a type that doesn't exist. It was " + url + " using type " + contentType);
+								skip = true;
+								break;
+							}
+							*/
+
+							//var service = Api.Startup.Services.GetByContentType(type);
+
+							tokenSet.push({
+								//contentType: type,
+								contentType: contentType,
+								//contentTypeId = Api.Database.ContentTypes.GetId(type),
+								fieldName: fieldName,
+								//fieldOrProperty: null,
+								isId: fieldName.toLowerCase() == "id",
+								rawToken: token,
+								//service: service,
+								typeName: contentType
+							});
+
+						} else {
+							tokenSet.push({
+								rawToken: token
+							});
+						}
+
+					}
+
+				}
+
+				if (token != null) {
+					// Anything. Treat these tokens as *:
+					part = "*";
+				}
+
+				var next = Object.entries(pg.children).filter(page => page[0] == part);
+
+				if (!next.length) {
+					pg.children[part] = next = {
+						children: {},
+						pages: [],
+						redirection: null,
+						urlTokenNames: null,
+						urlTokenNamesJson: null,
+						urlTokens: null,
+						wildcard: null
+					};
+
+					if (token != null) {
+						// It's the wildcard one:
+						pg.wildcard = next;
+					}
+				} else {
+					next = next[0][1];
+				}
+
+				pg = next;
+			}
+
+			if (skip) {
+				return null;
+			}
+
+		}
+
+		pg.urlTokens = tokenSet;
+		pg.urlTokenNames = tokenSet.map(token => token.rawToken);
+
+		if (pg.urlTokenNames == null || !pg.urlTokenNames.length) {
+			pg.urlTokenNamesJson = "null";
+		}
+		else {
+			pg.urlTokenNamesJson = JSON.stringify(pg.urlTokenNames);
+		}
+
+		return pg;
 	}
 
 	useEffect(() => {
 		webRequest('page/list').then(resp => {
-			// check for URLs containing parameters (e.g. {user.id})
-			// (...new Set() ensures we strip out any duplicates)
-			var entityUrls = [...new Set(
-				resp.json.results.map(result => {
-					var params = result.url.match(/{([^}]+)}/g);
-					var paramInfo = [];
-
-					// TODO: en-admin/{entity}/{id}
-					if (params && params.length == 1) {
-						paramInfo = params[0].slice(1, -1).split(".");
-					}
-
-					var ignoreEntities = [
-						'page', // we're already listing pages
-						'upload' // no need to show every individual upload (that's a job for the Media page)
-					]
-
-					if (paramInfo.length == 2 && paramInfo[1] == 'id' && !ignoreEntities.includes(paramInfo[0])) {
-						return paramInfo[0] + '/list';
-					}
-				}).filter(entity => entity != undefined)
-			)];
-
-			entityData = [];
-
-			const pipeFetch = async entityUrls => entityUrls.reduce(reduceFetch, Promise.resolve(''));
-
-			pipeFetch(entityUrls).then(result => {
-
-				if (result) {
-					entityData = [...entityData, ...result]
-                }
-
-				var sm = buildSitemap(resp.json.results);
-				sm.sort((a, b) => (a.url > b.url) ? 1 : -1);
-				setSitemap(sm);
-			});
+			setSitemap(buildSitemap(resp.json.results));
 		});
 	}, []);
 
-	function renderNode(page) {
-		var hasParameter = page.url.match(/{([^}]+)}/g);
-		var editClick = function (e) {
-			e.stopPropagation();
-			setPage('/' + window.location.pathname.replace(/^\/+|\/+$/g, '') + '/' + page.id);
-		};
-
-		var removeClick = function (e) {
-			e.stopPropagation();
-			setShowConfirmModal(page.id);
-		}
-
-		var editButton = {
-			icon: 'fa fa-edit',
-			text: `Edit`,
-			showLabel: true,
-			variant: 'primary',
-			onClick: editClick
-		};
-
-		var launchButton = {
-			disabled: hasParameter,
-			icon: 'fa fa-external-link',
-			text: `Launch`,
-			showLabel: true,
-			variant: 'secondary',
-			//onClick: window.location.origin + page.url,
-			//target: '_blank'
-			onClick: function () {
-				setPage(page.url);
-			},
-			children: [
-				{
-					icon: 'fa fa-fw fa-trash',
-					text: `Remove`,
-					onClick: removeClick
-				}
-			]
-		};
-
-		var buttons = !page.isPage ? [editButton, launchButton] : [launchButton];
-		var largeIcon = page.url == '/' ? 'fa-home' : 'fa-file';
-
-		if (page.url.startsWith('/en-admin')) {
-			largeIcon = 'fa-cog';
-        }
+	function renderNode(node, isRoot) {
+		var hasParameter = node.urlTokens && node.urlTokens.length > 0;
 
 		return <>
-			<Collapsible compact expanderLeft title={page.url} subtitle={page.title} info={page.isPage ? undefined : `ID: #${page.id}`} buttons={buttons} className="sitemap-expander"
-				defaultClick={page.children.length || page.isPage ? undefined : editClick} icon={largeIcon}>
-				{page.children.length && page.children.map(child => {
-					return renderNode(child);
-				})}
-			</Collapsible>
+			{
+				node.pages.map(page => {
+					var pageUrl = '/' + window.location.pathname.replace(/^\/+|\/+$/g, '') + '/' + page.id;
+
+					var editClick = function (e) {
+						e.stopPropagation();
+						setPage(pageUrl);
+					};
+
+					var removeClick = function (e) {
+						e.stopPropagation();
+						setShowConfirmModal(page.id);
+					}
+
+					var editButton = {
+						icon: 'fa fa-edit',
+						text: `Edit`,
+						showLabel: true,
+						variant: 'primary',
+						//onClick: editClick
+						onClick: pageUrl
+					};
+
+					var launchButton = {
+						disabled: hasParameter,
+						icon: 'fa fa-external-link',
+						text: `Launch`,
+						showLabel: true,
+						variant: 'secondary',
+						//onClick: window.location.origin + page.url,
+						//target: '_blank'
+						onClick: function () {
+							setPage(page.url);
+						},
+						children: [
+							{
+								icon: 'fa fa-fw fa-trash',
+								text: `Remove`,
+								onClick: removeClick
+							}
+						]
+					};
+
+					var isPage = page.type == "Page";
+					var buttons = isPage ? [editButton, launchButton] : [launchButton];
+					var largeIcon = page.url == '/' ? 'fa-home' : 'fa-file';
+
+					if (page.url.startsWith('/en-admin')) {
+						largeIcon = 'fa-cog';
+					}
+
+					var hasChildren = Object.keys(node.children).length;
+
+					var jsx = <>
+						{page.preferIfLoggedIn && <>
+							<i className={'sitemap-expander--prefers-logged-in fa fa-fw fa-user-circle'} title={`Preferred if logged in`}></i>
+						</>}
+						<span className="info">
+							{!isPage ? undefined : `ID: #${page.id}`}
+						</span>
+					</>;
+
+					return <>
+						<Collapsible compact expanderLeft title={page.url} subtitle={page.title}
+							jsx={jsx} buttons={buttons}
+							open={isRoot} alwaysOpen={isRoot} noContent={!hasChildren} className="sitemap-expander"
+							defaultClick={hasChildren || !isPage ? undefined : editClick} icon={largeIcon}>
+							{renderNodeChildren(node)}
+						</Collapsible>
+					</>;
+				})
+			}
+		</>;
+	}
+
+	function renderNodeChildren(node) {
+		return <>
+			{
+				Object.entries(node.children).map(([key, value]) => {
+					return renderNode(node.children[key], false);
+				})
+			}
 		</>;
     }
 
@@ -305,9 +321,7 @@ export default function Sitemap(props) {
 								</p>
 							</ConfirmModal>
 						</>}
-						{sitemap && sitemap.map(page => {
-							return renderNode(page);
-						})}
+						{sitemap && renderNode(sitemap, true)}
 					</div>
 					{!this.props.noCreate && <>
 						<footer className="admin-page__footer">
