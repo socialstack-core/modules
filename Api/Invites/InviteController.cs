4099395f6ed9a0cc0d6a3b906f689b544ec90d37
Luke Briggs <luke@kulestar.com>
2023-03-17 00:13:55 +0000

    Api/Invites: Redeem returns both the invite and the context.

--- Api/Invites/InviteController.cs
+++ Api/Invites/InviteController.cs
@@ -1,6 +1,8 @@
 ﻿using Api.Contexts;
+using Api.Database;
 using Api.PasswordAuth;
 using Api.Permissions;
+using Api.SocketServerLibrary;
 using Api.Startup;
 using Api.Users;
 using Microsoft.AspNetCore.Mvc;
@@ -19,7 +21,7 @@ namespace Api.Invites
 		/// Attempts to redeem an invite by the given token
 		/// </summary>
 		[HttpPost("redeem")]
-		public async ValueTask Redeem([FromBody] InviteData inviteData)
+		public async ValueTask Redeem([FromBody] InviteData inviteData, [FromQuery] string includes = null)
 		{
 			var context = await Request.GetContext();
 			
@@ -31,9 +33,27 @@ namespace Api.Invites
 				Response.StatusCode = 404;
 				return;
 			}
+
+			// Special situation - must return both the context and the invite.
+			// The following code is a combination of both OutputContext and OutputJson.
+			var targetStream = Response.Body;
+
+			var writer = Writer.GetPooled();
+			writer.Start(null);
+			writer.WriteASCII("{\"context\":");
+
+			await Services.Get<ContextService>().ToJson(context, writer);
 			
-			// Context was updated:
-			await OutputContext(context);
+			// Copy to output:
+			writer.WriteASCII(",\"invite\":{\"result\":");
+
+			// Next, the invite:
+			await _service.ToJson(context, invite, writer, targetStream, includes, false);
+
+			writer.WriteASCII("}}");
+			await writer.CopyToAsync(targetStream);
+
+			writer.Release();
 		}
 	}
 	
