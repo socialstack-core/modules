4c48a644cfa86c42b93f83c79b8c757ffc8c9380
Luke Briggs <luke@kulestar.com>
2023-05-21 15:58:20 +0100

    UI/Spacer: Can now be hidden

--- UI/Spacer/Spacer.js
+++ UI/Spacer/Spacer.js
@@ -1,15 +1,45 @@
 /*
 Just an invisible space of a specified height. The default is 20px.
 */
+import { useEffect, useRef } from 'react';
 
-export default function Spacer (props) {
+export default function Spacer(props) {
+	let { height, mobileHeight, hidden } = props;
+
+	if (hidden) {
+		return;
+	}
+
+	const spacerRef = useRef(null);
+
+	height = parseInt(height, 10);
+	mobileHeight = parseInt(mobileHeight, 10);
+
+	if (isNaN(height)) {
+		height = 20;
+	}
+
+	if (isNaN(mobileHeight)) {
+		mobileHeight = height;
+	}
+
+	useEffect(() => {
+
+		if (spacerRef.current) {
+			spacerRef.current.style.setProperty('--spacer-mobile-height', `${mobileHeight}px`);
+		}
+
+	});
+
 	return <div className="spacer-container" data-theme={props['data-theme']}>
-		<div className="spacer" style={{height: props.height ? (props.height + 'px') : '20px'}}/>
+		<div className="spacer" style={{ height: `${height}px` }} ref={spacerRef} />
 	</div>;
 }
 
 Spacer.propTypes = {
-	height: 'int'
+	height: 'int',
+	mobileHeight: 'int',
+	hidden: 'bool'
 };
 
 Spacer.icon = 'sort';
