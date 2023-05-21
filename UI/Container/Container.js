f56d57033db080b4142ee4eadb4c6a6defa5ebd1
Luke Briggs <luke@kulestar.com>
2023-05-21 15:58:18 +0100

    UI/Container: Containers can now optionally be hidden

--- UI/Container/Container.js
+++ UI/Container/Container.js
@@ -4,16 +4,24 @@ import omit from 'UI/Functions/Omit';
  * A specific width container for content (maps directly to bootstrap 'container' by default).
  * Can also use type="sm", "md", "lg", "xl", "fluid" - see also: bootstrap's container names.
  */
-const container = (props) =>
-	<div className={"container" + (props.type ? "-" + props.type : '') + " " + (props.className || '')} {...omit(props, ['type', 'children', 'className'])}>
+export default function container(props) {
+	let { hidden } = props;
+
+	if (hidden) {
+		return;
+	}
+
+	return <div className={"container" + (props.type ? "-" + props.type : '') + " " + (props.className || '')} {...omit(props, ['type', 'children', 'className'])}>
 		{props.children}
 	</div>;
+}
 
-export default container;
 
 container.propTypes = {
 	className: 'string',
 	type: ['','sm','md','lg','xl'],
-	children: true
+	children: true,
+	hidden: 'bool'
 };
+
 container.icon = 'cube';
