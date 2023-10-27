eb2598773a29956fa9f9ace420458a920c034aea
Luke Briggs <luke@kulestar.com>
2023-10-27 22:31:04 +0100

    UI/Link: Added lowercase and some SSR handling

--- UI/Link/Link.js
+++ UI/Link/Link.js
@@ -1,13 +1,42 @@
 import omit from 'UI/Functions/Omit';
+import { useSession } from 'UI/Session';
+import { useState, useEffect } from 'react';
 
 export default function Link (props) {
-	var attribs = omit(props, ['children', 'href', '_rte']);
+	const [initialRender, setInitialRender] = useState(true);
+
+	useEffect(() => {
+		setInitialRender(false);
+	}, []);
+
+	const { session, setSession } = useSession();
+
+	var attribs = omit(props, ['children', 'href', '_rte' ,'hreflang']);
 	attribs.alt = attribs.alt || attribs.title;
 	attribs.ref = attribs.rootRef;
 	
 	var children = props.children || props.text;
 	var url = (props.url || props.href);
-	
+
+	function isExternalUrl(string) {
+
+		// SSR or initial client render detected - treat as an external link (don't change case)
+		if (initialRender || window.SERVER) {
+			return true;
+		}
+
+		var r = new RegExp('^(?:[a-z+]+:)?//', 'i');
+		var isExternal = r.test(string);
+
+		if (isExternal) {
+				// check domain
+				var url = new URL(string);
+				isExternal = url.origin != window.origin;
+			  }
+
+		return isExternal;
+	}
+
 	if(url){
 		// if url contains :// it must be as-is (which happens anyway).
 		if(url[0] == '/'){
@@ -30,12 +59,25 @@ export default function Link (props) {
 				}
 			}
 		}
+
+		// ensure internal links are lowercase unless doNotLower is passed to this component
+		if (!props.doNotLower && !isExternalUrl(url) && !url.includes("/content/")) {
+			url = url.toLowerCase();
+		}
+
+		if (url != "/") {
+
+			// strip any trailing slashes
+			while (url.endsWith("/")) {
+				url = url.slice(0, -1);
+			}
+
+		}
+
 	}
 	
-	return <a href={url}
-		{...attribs}
-		>
-			{children}
+	return <a href={url} {...attribs}>
+		{children}
 	</a>;
 }
 
