5108070c5b604a64ab859167978bc4536bb159ca
Conan Ablewhite <conan.ablewhite@4-roads.com>
2024-04-11 10:37:05 +0100

    UI/TwoFactorGoogleSetup: Google 2FA now uses UI/QrCode to produce scannable codes

--- UI/TwoFactorGoogleSetup/TwoFactorGoogleSetup.js
+++ UI/TwoFactorGoogleSetup/TwoFactorGoogleSetup.js
@@ -1,6 +1,7 @@
 import Loading from 'UI/Loading';
 import Form from 'UI/Form';
 import Input from 'UI/Input';
+import QrCode from 'UI/QrCode';
 import {SessionConsumer} from 'UI/Session';
 
 export default class TwoFactorGoogleSetup extends React.Component {
@@ -27,24 +28,25 @@ export default class TwoFactorGoogleSetup extends React.Component {
 				// Setup form required here:
 				return <div>
 					<h3>
-						Two factor authentication required
+						{`Two factor authentication required`}
 					</h3>
 					<p>
 						{this.props.introText}
 					</p>
-					<img src={this.props.setupUrl} />
+					<QrCode text={this.props.setupUrl} width={256} height={256} />
+					<br/>
 					<p>
-						When you've added it, enter the 6 digit pin to confirm:
+						{`When you've added it, enter the 6 digit pin to confirm:`}
 					</p>
 					<Input name="google2FAPin" validate={['Required']} />
 				</div>;
 			}else{
 				return <div>
 					<h3>
-						Two factor authentication
+						{`Two factor authentication`}
 					</h3>
 					<p>
-						Please provide the auth code from your device:
+						{`Please provide the auth code from your device:`}
 					</p>
 					<Input name="google2FAPin" validate={['Required']} />
 				</div>;
@@ -59,20 +61,21 @@ export default class TwoFactorGoogleSetup extends React.Component {
 		
 		return <div className="two-factor-google-setup">
 			<h2>
-				Two factor authentication setup
+				{`Two factor authentication setup`}
 			</h2>
 			<p>
 				{this.props.introText}
 			</p>
-			<img src={'/v1/user/setup2fa/newkey'} />
+			<QrCode text={this.props.setupUrl} width={256} height={256} />
+			<br />
 			<p>
-				When you've added it, enter the 6 digit pin to confirm:
+				{`When you've added it, enter the 6 digit pin to confirm:`}
 			</p>
 			<Form
 				action="user/setup2fa/confirm"
-				failureMessage="Unfortunately that pin was incorrect, so two factor authentication has not been enabled."
-				successMessage="Two factor authentication has been enabled"
-				submitLabel="Setup 2FA"
+				failureMessage={`Unfortunately that pin was incorrect, so two factor authentication has not been enabled.`}
+				successMessage={`Two factor authentication has been enabled`}
+				submitLabel={`Setup 2FA`}
 			>
 				<Input type="text" name="pin" />
 			</Form>
@@ -87,5 +90,5 @@ TwoFactorGoogleSetup.propTypes = {
 };
 
 TwoFactorGoogleSetup.defaultProps = {
-	introText: 'To setup two factor authentication, you\'ll need the Google Authenticator app on your phone. Once you\'ve installed that, press the add button and scan the QR code below:'
+	introText: `To setup two factor authentication, you'll need the Google Authenticator app on your phone. Once you've installed that, press the add button and scan the QR code below:`
 };
