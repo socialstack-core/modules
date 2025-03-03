ba98282356822e64b5cdcf99bc164fea631703ad
Luke <luke@kulora.com>
2025-03-03 15:09:17 +0000

    Added FileRef typescript def and static file types

---
+++ Project/TypeScript/globals.d.ts
@@ -0,0 +1,9 @@
+declare global {
+	/**
+	 * Represents a socialstack file ref as a string.
+	 * Use this type whenever you need to indicate that a string is specifically a ref.
+	 */
+	type FileRef = string;
+}
+
+export {};
