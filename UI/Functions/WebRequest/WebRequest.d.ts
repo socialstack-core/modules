37eefa874911eb9cc912cb6cdfc1085b40d5fc95
hudson1998x <jhudsonjejames@gmail.com>
2025-03-06 17:14:26 +0000

    UI/Functions/WebRequest: no unknowns or any's remaining in this d.ts file, This now follows the expected output

--- UI/Functions/WebRequest/WebRequest.d.ts
+++ UI/Functions/WebRequest/WebRequest.d.ts
@@ -1,14 +1,25 @@
-﻿interface WebRequestResponse<Result> extends Omit<Response, "json"> {
-    json: Result;
-    notificationKey: unknown;
+﻿export type ApiSuccess<Result> = {
+    json: 
+        { result: Result} |
+        { results: Result[] }
+}
+export type ApiFailure = {
+    json: {
+        errors: Record<string, string[]>,
+        type: string,
+        title: string,
+        status: number, 
+        traceId: string
+    }
 }
 
-export default function webRequest<Result, Data = undefined>(
-    origUrl: string,
-    data?: Data,
-    opts?: object & {
-        blob?: unknown;
-        method?: string;
-        locale?: unknown;
+export default function webRequest<Result, Payload>(
+    url: string, 
+    data?: Payload, 
+    opts?: object & { 
+        blob?: Blob,
+        method?: 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE' | 'OPTIONS',
+        locale?: string,
+        includes: string[]
     }
-): Promise<WebRequestResponse<Result>>;
+): Promise<ApiSuccess<Result> | ApiFailure>
