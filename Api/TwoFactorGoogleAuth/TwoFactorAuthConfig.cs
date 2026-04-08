9c3be43cbd66f05c201434f82f244f6ef8b7b9f4
Luke Briggs <luke@kulestar.com>
2026-04-08 17:01:45 +0100

    Added 2fa module from 2024

---
+++ Api/TwoFactorGoogleAuth/TwoFactorAuthConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Api.Configuration;
+using System.Threading.Tasks;
+
+
+namespace Api.TwoFactorGoogleAuth
+{
+	/// <summary>
+	/// Two factor auth cfg
+	/// </summary>
+    public partial class TwoFactorAuthConfig : Config
+    {
+		/// <summary>
+		/// Set true if 2fa is required on all accounts.
+		/// </summary>
+		public bool Required {get; set;} = false;
+		
+		/// <summary>
+		/// True if its required for any admin accessing accounts (any role with CanViewAdmin true).
+		/// </summary>
+		public bool RequiredForAdmin {get; set;} = true;
+    }
+	
+}
