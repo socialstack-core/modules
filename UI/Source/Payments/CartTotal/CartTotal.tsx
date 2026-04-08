7991afa5dd69bca0f1c4f14e1084d5ab17201ded
Luke Briggs <luke@kulestar.com>
2026-04-08 14:24:39 +0100

    Handling discounts and switch from modal to dialog

--- UI/Source/Payments/CartTotal/CartTotal.tsx
+++ UI/Source/Payments/CartTotal/CartTotal.tsx
@@ -1,15 +1,32 @@
 import { formatCurrency } from "UI/Functions/CurrencyTools";
 import Button from 'UI/Button';
-import Modal from 'UI/Modal';
+import ConfirmDialog from 'UI/Dialog/ConfirmDialog';
 import { useState } from 'react';
 import { useRouter } from 'UI/Router';
 import { useSession } from 'UI/Session';
+import { ShoppingCart } from 'Api/ShoppingCart';
 import getConfig from 'UI/Config';
+import Alert from "UI/Alert";
+import { useCart } from "UI/Payments/CartSession";
 
 /**
  * Props for the CartTotal component.
  */
 interface CartTotal {
+	/**
+	 * basket contents
+	 */
+	shoppingCart: ShoppingCart,
+
+	/** 
+	 * method to run when clicking "empty cart"
+	 */
+	emptyCart?: Function,
+
+	/**
+	 * set true to hide call to action buttons (used on checkout view)
+	 */
+	hideCTAs?: boolean
 }
 
 /**
@@ -17,7 +34,7 @@ interface CartTotal {
  * @param props React props.
  */
 const CartTotal: React.FC<CartTotalProps> = (props) => {
-	var { shoppingCart, emptyCart, lessTax } = props;
+	var { shoppingCart, emptyCart, hideCTAs } = props;
 	var [showEmptyCartPrompt, setShowEmptyCartPrompt] = useState(null);
 
 	var pricedCart = shoppingCart?.cartContents;
@@ -39,6 +56,8 @@ const CartTotal: React.FC<CartTotalProps> = (props) => {
 		guestCheckoutEnabled = false;
 	}
 
+	const { lessTax } = useCart();
+
 	//var itemSet = pricedCart.contents;
 	var currencyCode = pricedCart.currencyCode;
 	var hasAtLeastOneSubscription = pricedCart.hasSubscriptionProducts;
@@ -47,56 +66,93 @@ const CartTotal: React.FC<CartTotalProps> = (props) => {
 	let totalExcVat = pricedCart.totalLessTax;
 	let totalVat = totalIncVat - totalExcVat;
 
+	let discounts = [];
+	var cartContents = shoppingCart?.cartContents;
+
+	if (shoppingCart?.coupon && cartContents) {
+
+		if (shoppingCart.coupon.discountFixedAmount > 0) {
+			discounts.push(formatCurrency(shoppingCart.coupon.discountFixedAmount, { currencyCode: cartContents.currencyCode }));
+		}
+
+		if (shoppingCart.coupon.discountPercent > 0) {
+			discounts.push(new Intl.NumberFormat('default', {
+				style: 'percent',
+				minimumFractionDigits: 0,
+				maximumFractionDigits: 0,
+			}).format(shoppingCart.coupon.discountPercent / 100));
+		}
+
+	}
+
 	return <>
 		<footer className="shopping-cart__total">
-			<dl className="shopping-cart__total-rows">
-				<div class="shopping-cart__total-row shopping-cart__total-row--exc-vat">
-					<dt>
-						{`Total exc VAT`}
-					</dt>
-					<dd>
-						{formatCurrency(totalExcVat, { currencyCode })}
-					</dd>
-				</div>
 
-				<div class="shopping-cart__total-row shopping-cart__total-row--vat">
-					<dt>
-						{`VAT`}
-					</dt>
-					<dd>
-						{formatCurrency(totalVat, { currencyCode })}
-					</dd>
-				</div>
+			{/*
+				- Legal liability notice -
+				Be careful! Deliveries are not VAT free.
+				*Do not* change the order of these values.
+			*/}
+			<p className="shopping-cart__total-row">
+				<span>{lessTax ? `Total (ex VAT)` : `Total (inc VAT)`}</span>
+				{formatCurrency(lessTax ? shoppingCart.cartContents?.totalLessTax : shoppingCart.cartContents?.total, { currencyCode })}
+			</p>
 
-				<div class="shopping-cart__total-row shopping-cart__total-row--inc-vat">
-					<dt>
-						{`Total inc VAT`}
-					</dt>
-					<dd>
-						{formatCurrency(totalIncVat, { currencyCode })}
-					</dd>
-				</div>
-			</dl>
+			{discounts.length > 0 && <>
+				<p className="shopping-cart__total-row">
+					<span>{discounts.length == 1 ? `Includes discount` : `Includes discounts`}</span>
+					-{discounts.join(', -')}
+				</p>
+			</>}
 
-			<div className="shopping-cart__total-cta">
-				<Button variant="danger" outlined onClick={() => setShowEmptyCartPrompt(true)}>
-					<i className="fr fr-trash-alt" />
-					{`Empty basket`}
-				</Button>
+			{lessTax && (
+				<p className="shopping-cart__total-row">
+					<span>{`VAT`}</span>
+					{formatCurrency((shoppingCart.cartContents?.total ?? 0) - (shoppingCart.cartContents?.totalLessTax ?? 0), { currencyCode })}
+				</p>
+			)}
 
-				<Button variant="primary" onClick={() => setPage('/cart/checkout')}>
-					<i className="fr fr-credit-card" />
-					{`Checkout`}
-				</Button>
+			<p className="shopping-cart__total-row shopping-cart__total-row--grand">
+				<span>{`Total`}</span>
+				{formatCurrency(shoppingCart.cartContents?.total, { currencyCode })}
+			</p>
 
-				{guestCheckoutEnabled && 
-					<Button variant="primary" onClick={() => setPage('/guest/register')}>
+			{!lessTax && (
+				<p className="shopping-cart__total-row">
+					<span>{`VAT`}</span>
+					{formatCurrency((shoppingCart.cartContents?.total ?? 0) - (shoppingCart.cartContents?.totalLessTax ?? 0), { currencyCode })}
+				</p>
+			)}
+
+			{Boolean(shoppingCart?.cartContents?.errorCode) ? <Alert variant={'danger'}>{shoppingCart.cartContents.errorMessage}</Alert> : null}
+
+			{!hideCTAs && <>
+				<div className="shopping-cart__total-cta">
+					<Button variant="danger" outlined onClick={() => setShowEmptyCartPrompt(true)}>
+						<i className="fr fr-trash-alt" />
+						{`Empty Basket`}
+					</Button>
+
+					<Button variant="primary" onClick={() => {
+						setPage('/cart/checkout');
+					}}>
 						<i className="fr fr-credit-card" />
-						{`Checkout as Guest`}
-					</Button>					
-				}
+						{`Checkout`}
+					</Button>
 
-			</div>
+					{guestCheckoutEnabled &&
+						<Button variant="primary" onClick={() => {
+
+							setPage('/guest/register');
+
+						}}>
+							<i className="fr fr-credit-card" />
+							{`Checkout as Guest`}
+						</Button>
+					}
+
+				</div>
+			</>}
 
 			{/* TODO: reinstate subscription footnote?
 				{hasAtLeastOneSubscription && <small>
@@ -105,25 +161,19 @@ const CartTotal: React.FC<CartTotalProps> = (props) => {
 			*/}
 
 		</footer>
-		{
-			showEmptyCartPrompt && <>
-				<Modal visible isSmall className="empty-cart-modal" title={`Empty basket`} onClose={() => setShowEmptyCartPrompt(false)}>
-					<p>{`This will remove all selected products from your shopping basket.`}</p>
-					<p>{`Are you sure you wish to do this?`}</p>
-					<footer>
-						<Button variant="danger" outlined onClick={() => setShowEmptyCartPrompt(false)}>
-							{`Cancel`}
-						</Button>
-						<Button variant="danger" onClick={() => {
-							emptyCart();
-							setShowEmptyCartPrompt(false);
-						}}>
-							{`Empty`}
-						</Button>
-					</footer>
-				</Modal>
-			</>
-		}
+
+		{showEmptyCartPrompt && <>
+			<ConfirmDialog variant="danger" isOpen={showEmptyCartPrompt} title={`Empty Basket`} onClose={() => setShowEmptyCartPrompt(false)}
+				confirmText={`Empty`}
+				confirmCallback={() => {
+					emptyCart();
+					setShowEmptyCartPrompt(false);
+				}}>
+				<p>{`This will remove all selected products from your shopping basket.`}</p>
+				<p>{`Are you sure you wish to do this?`}</p>
+			</ConfirmDialog>
+		</>}
+
 	</>;
 }
 
