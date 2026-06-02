b3d7fabd61d00346a6fc70311f4fb4e0b33e8317
Luke Briggs <luke@kulestar.com>
2026-06-02 15:02:10 +0100

    Added explicit indices for the payments module (if present) to the audit events module.

---
+++ Api/AuditEvents/DatabaseIndices/Product.cs
@@ -0,0 +1,10 @@
+﻿using Api.Database;
+
+namespace Api.Payments
+{
+	[DatabaseIndex(Name= "Product_Audit_Creation_Date", Scope = "database", Unique=false, Fields = ["CreatedUtc"])]
+	public partial class Product
+	{
+	
+	}
+}
---
+++ Api/AuditEvents/DatabaseIndices/Purchase.cs
@@ -0,0 +1,10 @@
+﻿using Api.Database;
+
+namespace Api.Payments
+{
+	[DatabaseIndex(Name= "Purchase_Audit_Creation_Date", Scope = "database", Unique=false, Fields = ["CreatedUtc"])]
+	public partial class Purchase
+	{
+	
+	}
+}
