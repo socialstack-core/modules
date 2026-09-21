e8b00d3f0920dffc4020f8f8badd6184a237c570
Luke Briggs <luke@kulestar.com>
2026-09-21 14:31:10 +0100

    Added more structure to the base web template

--- UI/Source/Templates/BaseWebTemplate/BaseWebTemplate.tsx
+++ UI/Source/Templates/BaseWebTemplate/BaseWebTemplate.tsx
@@ -1,23 +1,33 @@
-
-interface SidebarTemplateProps {
-    header?: React.ReactNode,
-    body?: React.ReactNode,
-    footer?: React.ReactNode,
+interface BaseWebTemplateProps {
+	header?: React.ReactNode,
+	body?: React.ReactNode,
+	footer?: React.ReactNode,
 }
 
-const BaseWebTemplate: React.FC<SidebarTemplateProps> = (props : SidebarTemplateProps) => {
+const BaseWebTemplate: React.FC<BaseWebTemplateProps> = (props: BaseWebTemplateProps) => {
 
-     return (
-          <div id="wrapper">
-               {props.header && <header>{props.header}</header>}
-               <div id="content">
-                    {props.body && <main>{props.body}</main>}
-               </div>
-               {props.footer && <footer>{props.footer}</footer>}
-          </div>
-     )
+	return (
+		<div id="site-wrapper">
+			{/* site header / nav */}
+			{props.header && <>
+				<nav role="navigation" aria-label={`Main navigation`}>
+					{props.header}
+				</nav>
+			</>}
+
+			{/* main site content */}
+			<main role="main" id="site-content">
+				{props.body}
+			</main>
+
+			{/* site footer */}
+			{props.footer && <>
+				<footer role="contentinfo">
+					{props.footer}
+				</footer>
+			</>}
+		</div>
+	)
 };
-
-// BaseWebTemplate.propTypes = {}
 
 export default BaseWebTemplate;
